5c0c917a120dc292eeb416c83ed1011dc7b05103
Sagi <sagi.fogel@gmail.com>
2019-12-13 01:42:14 +0200

    some instances for Wander & Bazaar

--- src/main/scala/optics/internal/Bazaar.scala
+++ src/main/scala/optics/internal/Bazaar.scala
@@ -1,17 +1,19 @@
 package optics.internal
 
-import cats.Applicative
-import cats.arrow.{Profunctor, Strong}
+import cats.{Applicative, Monad}
+import cats.arrow.{ArrowChoice, Profunctor, Strong}
+
+import scala.Function.const
 
 /** [[Bazaar]] is used to characterize a Traversal */
-final case class Bazaar[P[_, _], F[_] : Applicative, A, B, S, T](x: P[A, F[B]] => S => F[T])
+final case class Bazaar[P[_, _], F[_]: Applicative, A, B, S, T](runBazaar: P[A, F[B]] => S => F[T])
 
 abstract class BazaarInstances {
   implicit def profunctorBazaar[P[_, _], F[_], G, H](implicit ev: Applicative[F]): Profunctor[Bazaar[P, F, G, H, *, *]] =
     new Profunctor[Bazaar[P, F, G, H, *, *]] {
       override def dimap[A, B, C, D](fab: Bazaar[P, F, G, H, A, B])(f: C => A)(g: B => D): Bazaar[P, F, G, H, C, D] = {
         Bazaar(pfgh => c => {
-          val fb = fab.x(pfgh)(f(c))
+          val fb = fab.runBazaar(pfgh)(f(c))
           ev.map(fb)(g)
         })
       }
@@ -20,25 +22,40 @@ abstract class BazaarInstances {
   implicit def strongBazaar[P[_, _], F[_], G, H](implicit ev: Applicative[F]): Strong[Bazaar[P, F, G, H, *, *]] =
     new Strong[Bazaar[P, F, G, H, *, *]] {
       override def first[A, B, C](fa: Bazaar[P, F, G, H, A, B]): Bazaar[P, F, G, H, (A, C), (B, C)] = {
-        Bazaar(pgfh => { case (a, c) =>
-          val afb = fa.x(pgfh)
-          ev.map(afb(a))((_, c))
+        Bazaar(pgfh => {
+          case (a, c) =>
+            val afb = fa.runBazaar(pgfh)
+            ev.map(afb(a))((_, c))
         })
       }
 
       override def second[A, B, C](fa: Bazaar[P, F, G, H, A, B]): Bazaar[P, F, G, H, (C, A), (C, B)] =
-        Bazaar(pgfh => { case (c, a) =>
-          val afb = fa.x(pgfh)
-          ev.map(afb(a))((c, _))
+        Bazaar(pgfh => {
+          case (c, a) =>
+            val afb = fa.runBazaar(pgfh)
+            ev.map(afb(a))((c, _))
         })
 
       override def dimap[A, B, C, D](fab: Bazaar[P, F, G, H, A, B])(f: C => A)(g: B => D): Bazaar[P, F, G, H, C, D] =
         profunctorBazaar[P, F, G, H].dimap(fab)(f)(g)
     }
-}
 
-object Bazaar {
-  def runBazaar[P[_, _], F[_] : Applicative, A, B, S, T](bazaar: Bazaar[P, F, A, B, S, T]): P[A, F[B]] => S => F[T] = {
-    bazaar.x
-  }
+  implicit def choiceBazaar[P[_, _], F[_], G, H](implicit ev: Monad[F]): ArrowChoice[Bazaar[P, F, G, H, *, *]] =
+    new ArrowChoice[Bazaar[P, F, G, H, *, *]] {
+      override def choose[A, B, C, D](f: Bazaar[P, F, G, H, A, C])(g: Bazaar[P, F, G, H, B, D]):
+        Bazaar[P, F, G, H, Either[A, B], Either[C, D]] = ???
+
+      override def lift[A, B](f: A => B): Bazaar[P, F, G, H, A, B] =
+        Bazaar[P, F, G, H, A, B](const(ev.pure[B] _ compose f))
+
+      override def compose[A, B, C](f: Bazaar[P, F, G, H, B, C], g: Bazaar[P, F, G, H, A, B]):
+        Bazaar[P, F, G, H, A, C] = Bazaar[P, F, G, H, A, C] { pgfh =>
+          a: A => {
+            val fb = g.runBazaar(pgfh)(a)
+            ev.flatMap(fb)(f.runBazaar(pgfh))
+          }
+      }
+
+      override def first[A, B, C](fa: Bazaar[P, F, G, H, A, B]): Bazaar[P, F, G, H, (A, C), (B, C)] = ???
+    }
 }
--- src/main/scala/optics/internal/Wander.scala
+++ src/main/scala/optics/internal/Wander.scala
@@ -1,19 +1,29 @@
 package optics.internal
 
 import cats.Applicative
-import cats.arrow.{Choice, Strong}
+import cats.arrow.{ArrowChoice, Strong}
+import optics.profunctor.Star
+
+trait Traversal[S, T, A, B] {
+  def apply[F[_]: Applicative](f: A => F[B]): S => F[T]
+}
 
 /** Class for profunctors that support polymorphic traversals */
 trait Wander[P[_, _]] {
-  def wander[F[_], S, T, A, B](f: (A => F[B]) => S => F[T])
-                              (pab: P[A, B])
-                              (implicit ev: Strong[P],
-                               ev2: Choice[P],
-                               ev3: Applicative[F]): P[S, T]
+  def wander[S, T, A, B](traversal: Traversal[S, T, A, B])
+                         (pab: P[A, B])
+                         (implicit ev: Strong[P],
+                         ev2: ArrowChoice[P]): P[S, T]
 }
 
-
 abstract class WanderInstances {
+  implicit def instance[F[_]: Applicative]: Wander[Star[F, *, *]] = new Wander[Star[F, *, *]] {
+    override def wander[S, T, A, B](traversal: Traversal[S, T, A, B])
+                                   (pab: Star[F, A, B])
+                                   (implicit ev: Strong[Star[F, *, *]],
+                                    ev2: ArrowChoice[Star[F, *, *]]): Star[F, S, T] =
+      Star(traversal(pab.runStar))
+  }
 }
 
 object Wander extends WanderInstances
