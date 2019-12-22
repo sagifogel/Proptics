9c325b21864a7c66f6a8685270750cedecf37236
Sagi <sagi.fogel@gmail.com>
2019-12-23 00:21:03 +0200

    implementaion of Setter methods

--- src/main/scala/optics/Setter.scala
+++ src/main/scala/optics/Setter.scala
@@ -1,5 +1,11 @@
 package optics
 
+import algebra.lattice.Heyting
+import cats.kernel.Semigroup
+import spire.algebra.{Field, Ring, Semiring}
+
+import scala.Function.const
+
 /**
  * A [[Setter]] is an [[Optic]] with a fixed type of a [[Function1]] as the type constructor
  *
@@ -8,5 +14,32 @@ package optics
  * @tparam A the target of a [[Setter]]
  * @tparam B the modified target of a [[Setter]]
  */
-abstract class Setter[S, T, A, B] extends Optic[* => *, S, T, A, B] {
+abstract class Setter[S, T, A, B] extends Optic[* => *, S, T, A, B] { self =>
+  def over(f: A => B, s: S): T = self(f)(s)
+
+  def set(b: B, s: S): T = over(const(b), s)
+
+}
+
+object Setter {
+  def addOver[S, T, A](setter: Setter[S, T, A, A])(a: A, s: S)(implicit ev: Semiring[A]): T =
+    setter.over(ev.additive.combine(_, a), s)
+
+  def mulOver[S, T, A](setter: Setter[S, T, A, A])(a: A, s: S)(implicit ev: Semiring[A]): T =
+    setter.over(ev.multiplicative.combine(_, a), s)
+
+  def subOver[S, T, A](setter: Setter[S, T, A, A])(a: A, s: S)(implicit ev: Ring[A]): T =
+    setter.over(ev.minus(_, a), s)
+
+  def divOver[S, T, A](setter: Setter[S, T, A, A])(a: A, s: S)(implicit ev: Field[A]): T =
+    setter.over(ev.div(_, a), s)
+
+  def disjOver[S, T, A](setter: Setter[S, T, A, A])(a: A, s: S)(implicit ev: Heyting[A]): T =
+    setter.over(ev.or(_, a), s)
+
+  def conjOver[S, T, A](setter: Setter[S, T, A, A])(a: A, s: S)(implicit ev: Heyting[A]): T =
+    setter.over(ev.and(_, a), s)
+
+  def appendOver[S, T, A](setter: Setter[S, T, A, A])(a: A, s: S)(implicit ev: Semigroup[A]): T =
+    setter.over(ev.combine(_, a), s)
 }
