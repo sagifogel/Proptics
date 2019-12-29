ea02072fb3e7047dcd85c80c1d398ae35a986c22
Sagi <sagi.fogel@gmail.com>
2019-12-29 12:35:22 +0200

    apply methods for AnIso

--- src/main/scala/optics/AnIso.scala
+++ src/main/scala/optics/AnIso.scala
@@ -1,5 +1,6 @@
 package optics
 
+import cats.arrow.Profunctor
 import optics.internal.Exchange
 
 /**
@@ -10,5 +11,24 @@ import optics.internal.Exchange
  * @tparam A the target of a [[AnIso]]
  * @tparam B the modified target of a [[AnIso]]
  */
-abstract class AnIso[S, T, A, B] extends Optic[Exchange[A, B, *, *], S, T, A, B] { self =>
+abstract class AnIso[S, T, A, B] extends Optic[Exchange[A, B, *, *], S, T, A, B] {
+  def withIso[R](f: (S => A) => (B => T) => R): R
+
+  def cloneIso[P[_, _]](implicit ev: Profunctor[P]): Iso[P, S, T, A, B] = withIso(Iso[P, S, T, A, B])
+}
+
+object AnIso {
+  private[optics] def apply[S, T, A, B](f: Exchange[A, B, A, B] => Exchange[A, B, S, T]): AnIso[S, T, A, B] = new AnIso[S, T, A, B] { self =>
+    override def withIso[R](f: (S => A) => (B => T) => R): R = {
+      val exchange = self(Exchange(identity, identity))
+
+      f(exchange.get)(exchange.inverseGet)
+    }
+
+    override def apply(pab: Exchange[A, B, A, B]): Exchange[A, B, S, T] = f(pab)
+  }
+
+  def apply[S, T, A, B](get: S => A)(inverseGet: B => T): AnIso[S, T, A, B] = {
+    AnIso((e: Exchange[A, B, A, B]) => Exchange(e.get compose get, inverseGet compose e.inverseGet))
+  }
 }
--- src/main/scala/optics/internal/Exchange.scala
+++ src/main/scala/optics/internal/Exchange.scala
@@ -4,18 +4,18 @@ import cats.Functor
 import cats.arrow.Profunctor
 
 /** The [[Exchange]] profunctor characterizes an [[optics.Iso]] */
-final case class Exchange[A, B, S, T](f: S => A, g: B => T)
+final case class Exchange[A, B, S, T](get: S => A, inverseGet: B => T)
 
 abstract class ExchangeInstances {
   implicit final def functorExchange[C, D, S]: Functor[Exchange[C, D, S, *]] = new Functor[Exchange[C, D, S, *]] {
     override def map[A, B](fa: Exchange[C, D, S, A])(f: A => B): Exchange[C, D, S, B] = {
-      Exchange(fa.f, f compose fa.g)
+      Exchange(fa.get, f compose fa.inverseGet)
     }
   }
 
   implicit final def profunctorExchange[E, F]: Profunctor[Exchange[E, F, *, *]] = new Profunctor[Exchange[E, F, *, *]] {
     override def dimap[A, B, C, D](fab: Exchange[E, F, A, B])(f: C => A)(g: B => D): Exchange[E, F, C, D] =
-      Exchange(fab.f compose f, g compose fab.g)
+      Exchange(fab.get compose f, g compose fab.inverseGet)
   }
 }
 
