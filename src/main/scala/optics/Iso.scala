2d72f2e8accdd3feb4c1b7ce0be14484a4e24f21
Sagi <sagi.fogel@gmail.com>
2019-12-29 12:32:35 +0200

    apply methods for Iso

--- src/main/scala/optics/Iso.scala
+++ src/main/scala/optics/Iso.scala
@@ -13,3 +13,13 @@ import cats.arrow.Profunctor
  */
 abstract class Iso[P[_, _]: Profunctor, S, T, A, B] extends Optic[P, S, T, A, B] { self =>
 }
+
+object Iso {
+  private[optics] def apply[P[_, _], S, T, A, B](f: P[A, B] => P[S, T])(implicit ev: Profunctor[P]): Iso[P, S, T, A, B] = new Iso[P, S, T, A, B] {
+    override def apply(pab: P[A, B]): P[S, T] = f(pab)
+  }
+
+  def apply[P[_, _], S, T, A, B](get: S => A)(inverseGet: B => T)(implicit ev: Profunctor[P]): Iso[P, S, T, A, B] = {
+    Iso(ev.dimap(_)(get)(inverseGet))
+  }
+}
