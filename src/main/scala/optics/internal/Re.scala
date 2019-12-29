078e8e62a6e8a83c86b048d79873c887c55a3661
Sagi <sagi.fogel@gmail.com>
2019-12-29 19:38:53 +0200

    addition of Re profunctor

---
+++ src/main/scala/optics/internal/Re.scala
@@ -0,0 +1,59 @@
+package optics.internal
+
+import cats.arrow.{Profunctor, Strong}
+import optics.profunctor.{Choice, Cochoice, Costrong}
+
+final case class Re[P[_, _], S, T, A, B](runRe: P[B, A] => P[S, T])
+
+abstract class ReInstances {
+  implicit def profunctorRe[P[_, _], S, T](implicit ev: Profunctor[P]): Profunctor[Re[P, S, T, *, *]] = new Profunctor[Re[P, S, T, *, *]] {
+    override def dimap[A, B, C, D](fab: Re[P, S, T, A, B])(f: C => A)(g: B => D): Re[P, S, T, C, D] =
+      Re(pdc => fab.runRe(ev.dimap(pdc)(g)(f)))
+  }
+
+  implicit def choiceRe[P[_, _], S, T](implicit ev: Choice[P]): Cochoice[Re[P, S, T, *, *]] = new Cochoice[Re[P, S, T, *, *]] {
+    override def unleft[A, B, C](p: Re[P, S, T, Either[A, C], Either[B, C]]): Re[P, S, T, A, B] =
+      Re(p.runRe compose ev.left)
+
+    override def unright[A, B, C](p: Re[P, S, T, Either[A, B], Either[A, C]]): Re[P, S, T, B, C] =
+      Re(p.runRe compose ev.right)
+
+    override def dimap[A, B, C, D](fab: Re[P, S, T, A, B])(f: C => A)(g: B => D): Re[P, S, T, C, D] =
+      profunctorRe[P, S, T].dimap(fab)(f)(g)
+  }
+
+  implicit def cochoiceRe[P[_, _], S, T](implicit ev: Cochoice[P]): Choice[Re[P, S, T, *, *]] = new Choice[Re[P, S, T, *, *]] {
+    override def left[A, B, C](pab: Re[P, S, T, A, B]): Re[P, S, T, Either[A, C], Either[B, C]] =
+      Re(pab.runRe compose ev.unleft)
+
+    override def right[A, B, C](pab: Re[P, S, T, B, C]): Re[P, S, T, Either[A, B], Either[A, C]] =
+      Re(pab.runRe compose ev.unright)
+
+    override def dimap[A, B, C, D](fab: Re[P, S, T, A, B])(f: C => A)(g: B => D): Re[P, S, T, C, D] =
+      profunctorRe[P, S, T].dimap(fab)(f)(g)
+  }
+
+  implicit def strongRe[P[_, _], S, T](implicit ev: Strong[P]): Costrong[Re[P, S, T, *, *]] = new Costrong[Re[P, S, T, *, *]] {
+    override def unfirst[A, B, C](p: Re[P, S, T, (A, C), (B, C)]): Re[P, S, T, A, B] =
+      Re(p.runRe compose ev.first)
+
+    override def unsecond[A, B, C](p: Re[P, S, T, (A, B), (A, C)]): Re[P, S, T, B, C] =
+      Re(p.runRe compose ev.second)
+
+    override def dimap[A, B, C, D](fab: Re[P, S, T, A, B])(f: C => A)(g: B => D): Re[P, S, T, C, D] =
+      profunctorRe[P, S, T].dimap(fab)(f)(g)
+  }
+
+  implicit def costrongRe[P[_, _], S, T](implicit ev: Costrong[P]): Strong[Re[P, S, T, *, *]] = new Strong[Re[P, S, T, *, *]] {
+    override def first[A, B, C](fa: Re[P, S, T, A, B]): Re[P, S, T, (A, C), (B, C)] =
+      Re(fa.runRe compose ev.unfirst)
+
+    override def second[A, B, C](fa: Re[P, S, T, A, B]): Re[P, S, T, (C, A), (C, B)] =
+      Re(fa.runRe compose ev.unsecond)
+
+    override def dimap[A, B, C, D](fab: Re[P, S, T, A, B])(f: C => A)(g: B => D): Re[P, S, T, C, D] =
+      profunctorRe[P, S, T].dimap(fab)(f)(g)
+  }
+}
+
+object Re extends ReInstances
--- src/main/scala/optics/internal/Tagged.scala
+++ src/main/scala/optics/internal/Tagged.scala
@@ -8,39 +8,39 @@ import optics.profunctor.{Choice, Closed, Costrong}
 
 import scala.Function.const
 
-final case class Tagged[A, B](tag: B)
+final case class Tagged[A, B](runTag: B)
 
 abstract class TaggedInstances {
   implicit final def eqTagged[A, B](implicit ev: Eq[B]): Eq[Tagged[A, B]] = new Eq[Tagged[A, B]] {
-    override def eqv(x: Tagged[A, B], y: Tagged[A, B]): Boolean = x.tag === y.tag
+    override def eqv(x: Tagged[A, B], y: Tagged[A, B]): Boolean = x.runTag === y.runTag
   }
 
   implicit final def orderTagged[A, B](implicit ev: Order[B]): Order[Tagged[A, B]] = new Order[Tagged[A, B]] {
-    override def compare(x: Tagged[A, B], y: Tagged[A, B]): Int = x.tag compare y.tag
+    override def compare(x: Tagged[A, B], y: Tagged[A, B]): Int = x.runTag compare y.runTag
   }
 
   implicit final def functorTagged[F[_], C]: Functor[Tagged[C, *]]  = new Functor[Tagged[C, *]] {
-    override def map[A, B](fa: Tagged[C, A])(f: A => B): Tagged[C, B] = Tagged(f(fa.tag))
+    override def map[A, B](fa: Tagged[C, A])(f: A => B): Tagged[C, B] = Tagged(f(fa.runTag))
   }
 
   implicit final val profunctorTagged: Profunctor[Tagged]  = new Profunctor[Tagged] {
     override def dimap[A, B, C, D](fab: Tagged[A, B])(f: C => A)(g: B => D): Tagged[C, D] =
-      Tagged(g(fab.tag))
+      Tagged(g(fab.runTag))
   }
 
   implicit final val choiceTagged: Choice[Tagged]  = new Choice[Tagged] {
-    override def left[A, B, C](pab: Tagged[A, B]): Tagged[Either[A, C], Either[B, C]] = Tagged(pab.tag.asLeft[C])
+    override def left[A, B, C](pab: Tagged[A, B]): Tagged[Either[A, C], Either[B, C]] = Tagged(pab.runTag.asLeft[C])
 
-    override def right[A, B, C](pab: Tagged[B, C]): Tagged[Either[A, B], Either[A, C]] = Tagged(pab.tag.asRight[A])
+    override def right[A, B, C](pab: Tagged[B, C]): Tagged[Either[A, B], Either[A, C]] = Tagged(pab.runTag.asRight[A])
 
     override def dimap[A, B, C, D](fab: Tagged[A, B])(f: C => A)(g: B => D): Tagged[C, D] =
       profunctorTagged.dimap(fab)(f)(g)
   }
 
   implicit final val costrongTagged: Costrong[Tagged]  = new Costrong[Tagged] {
-    override def unfirst[A, B, C](p: Tagged[(A, C), (B, C)]): Tagged[A, B] = Tagged(p.tag._1)
+    override def unfirst[A, B, C](p: Tagged[(A, C), (B, C)]): Tagged[A, B] = Tagged(p.runTag._1)
 
-    override def unsecond[A, B, C](p: Tagged[(A, B), (A, C)]): Tagged[B, C] = Tagged(p.tag._2)
+    override def unsecond[A, B, C](p: Tagged[(A, B), (A, C)]): Tagged[B, C] = Tagged(p.runTag._2)
 
     override def dimap[A, B, C, D](fab: Tagged[A, B])(f: C => A)(g: B => D): Tagged[C, D] =
       profunctorTagged.dimap(fab)(f)(g)
@@ -48,21 +48,21 @@ abstract class TaggedInstances {
 
   implicit final val closedTagged: Closed[Tagged]  = new Closed[Tagged] {
     override def closed[A, B, C](pab: Tagged[A, B]): Tagged[C => A, C => B] =
-      Tagged(const(pab.tag))
+      Tagged(const(pab.runTag))
 
     override def dimap[A, B, C, D](fab: Tagged[A, B])(f: C => A)(g: B => D): Tagged[C, D] =
       profunctorTagged.dimap(fab)(f)(g)
   }
 
   implicit final def foldableTagged[C]: Foldable[Tagged[C, *]]  = new Foldable[Tagged[C, *]] {
-    override def foldLeft[A, B](fa: Tagged[C, A], b: B)(f: (B, A) => B): B = f(b, fa.tag)
+    override def foldLeft[A, B](fa: Tagged[C, A], b: B)(f: (B, A) => B): B = f(b, fa.runTag)
 
-    override def foldRight[A, B](fa: Tagged[C, A], lb: Eval[B])(f: (A, Eval[B]) => Eval[B]): Eval[B] = f(fa.tag, lb)
+    override def foldRight[A, B](fa: Tagged[C, A], lb: Eval[B])(f: (A, Eval[B]) => Eval[B]): Eval[B] = f(fa.runTag, lb)
   }
 
   implicit final def traverseTagged[C]: Traverse[Tagged[C, *]]  = new Traverse[Tagged[C, *]] {
     override def traverse[G[_], A, B](fa: Tagged[C, A])(f: A => G[B])(implicit ev: Applicative[G]): G[Tagged[C, B]] =
-      ev.map(f(fa.tag))(Tagged[C, B])
+      ev.map(f(fa.runTag))(Tagged[C, B])
 
     override def foldLeft[A, B](fa: Tagged[C, A], b: B)(f: (B, A) => B): B =
       foldableTagged.foldLeft(fa, b)(f)
