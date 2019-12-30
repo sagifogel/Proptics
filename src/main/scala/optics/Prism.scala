409d3ee73e2f0134e3fa46c7af0e55970db63b7f
Sagi <sagi.fogel@gmail.com>
2019-12-30 20:57:32 +0200

    apply method for Prism

--- src/main/scala/optics/Prism.scala
+++ src/main/scala/optics/Prism.scala
@@ -1,6 +1,7 @@
 package optics
 
-import cats.arrow.Choice
+import optics.profunctor.Choice
+
 
 /**
  * @tparam P an evidence of [[Choice]] [[cats.arrow.Profunctor]]
@@ -9,7 +10,24 @@ import cats.arrow.Choice
  * @tparam A the target of a [[Prism]]
  * @tparam B the modified target of a [[Prism]]
  */
-abstract class Prism[P[_, _]: Choice, S, T, A, B] extends Optic[P, S, T, A, B] { self =>
+abstract class Prism[P[_, _] : Choice, S, T, A, B] extends Optic[P, S, T, A, B] { self =>
+}
+
+object Prism {
+  private[optics] def apply[P[_, _], S, T, A, B](f: P[A, B] => P[S, T])(implicit ev: Choice[P]): Prism[P, S, T, A, B] = new Prism[P, S, T, A, B] {
+    override def apply(pab: P[A, B]): P[S, T] = f(pab)
+  }
+
+  def apply[P[_, _], S, T, A, B](to: B => T)(from: S => Either[T, A])(implicit ev: Choice[P]): Prism[P, S, T, A, B] =
+    prism(to)(from)
+
+  def prism[P[_, _], S, T, A, B](to: B => T)(from: S => Either[T, A])(implicit ev: Choice[P]): Prism[P, S, T, A, B] = {
+    Prism(pab => {
+      val right = ev.right[T, A, T](ev.rmap(pab)(to))
+
+      ev.dimap(right)(from)(_.fold(identity, identity))
+    })
+  }
 }
 
 
