7fd4073de0ae564276c2a7aeae0517a83deb6f33
Sagi <sagi.fogel@gmail.com>
2020-01-02 01:19:41 +0200

    apply methods for Fold

--- src/main/scala/optics/Fold.scala
+++ src/main/scala/optics/Fold.scala
@@ -13,3 +13,12 @@ import optics.internal.Forget
  */
 abstract class Fold[R, S, T, A, B] extends Optic[Forget[R, *, *], S, T, A, B] {
 }
+
+object Fold {
+  private[optics] def apply[R, S, T, A, B](f: Forget[R, A, B] => Forget[R, S, T]): Fold[R, S, T, A, B] = new Fold[R, S, T, A, B] {
+    override def apply(pab: Forget[R, A, B]): Forget[R, S, T] = f(pab)
+  }
+
+  def apply[R, S, T, A, B](f: S => A)(implicit ev: DummyImplicit): Fold[R, S, T, A, B] =
+    Fold((forget: Forget[R, A, B]) => Forget[R, S, T](forget.runForget compose f))
+}
