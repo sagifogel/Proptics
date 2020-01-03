0251db9bcb0f1a13d2a4328f47ca4b1ad3145e5c
Sagi <sagi.fogel@gmail.com>
2020-01-04 01:12:11 +0200

    addition of on function

--- src/main/scala/optics/syntax/FunctionSyntax.scala
+++ src/main/scala/optics/syntax/FunctionSyntax.scala
@@ -1,7 +1,11 @@
 package optics.syntax
 
 object FunctionSyntax {
-  implicit class Function2Ops[A, B, C](val fn: A => B => C) extends AnyVal {
-    def flip: B => A => C = b => a => fn(a)(b)
+  implicit class Function2Ops[A, B, C](val f: A => B => C) extends AnyVal {
+    def flip: B => A => C = b => a => f(a)(b)
+  }
+
+  implicit class Function2Domain[A, B, C](val f: B => B => C) extends AnyVal {
+    def on(g: A => B): A => A => C = x => y => f(g(x))(g(y))
   }
 }
