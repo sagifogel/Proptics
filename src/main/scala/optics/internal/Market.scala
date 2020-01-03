ace892d8df7a7793e1b0f338b431f466d4050a8d
Sagi <sagi.fogel@gmail.com>
2020-01-04 01:14:54 +0200

    refactor to be final methods

--- src/main/scala/optics/internal/Market.scala
+++ src/main/scala/optics/internal/Market.scala
@@ -9,18 +9,18 @@ import optics.profunctor.Choice
 final case class Market[A, B, S, T](to: B => T, from: S => Either[T, A])
 
 abstract class MarketInstances {
-  implicit def functorMarket[C, D, S]: Functor[Market[C, D, S, *]] = new Functor[Market[C, D, S, *]] {
+  implicit final def functorMarket[C, D, S]: Functor[Market[C, D, S, *]] = new Functor[Market[C, D, S, *]] {
     override def map[A, B](fa: Market[C, D, S, A])(f: A => B): Market[C, D, S, B] =
       Market(f compose fa.to, fa.from(_).leftMap(f))
   }
 
-  implicit def profunctorMarket[E, F]: Profunctor[Market[E, F, *, *]] = new Profunctor[Market[E, F, *, *]] {
+  implicit final def profunctorMarket[E, F]: Profunctor[Market[E, F, *, *]] = new Profunctor[Market[E, F, *, *]] {
     override def dimap[A, B, C, D](fab: Market[E, F, A, B])(f: C => A)(g: B => D): Market[E, F, C, D] = {
       Market(g compose fab.to, c => fab.from(f(c)).leftMap(g))
     }
   }
 
-  implicit def choiceMarket[E, F]: Choice[Market[E, F, *, *]] = new Choice[Market[E, F, *, *]] {
+  implicit final def choiceMarket[E, F]: Choice[Market[E, F, *, *]] = new Choice[Market[E, F, *, *]] {
     override def left[A, B, C](pab: Market[E, F, A, B]): Market[E, F, Either[A, C], Either[B, C]] =
       Market(pab.to(_).asLeft[C], _.fold(pab.from(_).leftMap(_.asLeft[C]), _.asRight[B].asLeft[E]))
 
--- src/main/scala/optics/internal/Re.scala
+++ src/main/scala/optics/internal/Re.scala
@@ -6,12 +6,12 @@ import optics.profunctor.{Choice, Cochoice, Costrong}
 final case class Re[P[_, _], S, T, A, B](runRe: P[B, A] => P[T, S])
 
 abstract class ReInstances {
-  implicit def profunctorRe[P[_, _], S, T](implicit ev: Profunctor[P]): Profunctor[Re[P, S, T, *, *]] = new Profunctor[Re[P, S, T, *, *]] {
+  implicit final def profunctorRe[P[_, _], S, T](implicit ev: Profunctor[P]): Profunctor[Re[P, S, T, *, *]] = new Profunctor[Re[P, S, T, *, *]] {
     override def dimap[A, B, C, D](fab: Re[P, S, T, A, B])(f: C => A)(g: B => D): Re[P, S, T, C, D] =
       Re(pdc => fab.runRe(ev.dimap(pdc)(g)(f)))
   }
 
-  implicit def choiceRe[P[_, _], S, T](implicit ev: Choice[P]): Cochoice[Re[P, S, T, *, *]] = new Cochoice[Re[P, S, T, *, *]] {
+  implicit final def choiceRe[P[_, _], S, T](implicit ev: Choice[P]): Cochoice[Re[P, S, T, *, *]] = new Cochoice[Re[P, S, T, *, *]] {
     override def unleft[A, B, C](p: Re[P, S, T, Either[A, C], Either[B, C]]): Re[P, S, T, A, B] =
       Re(p.runRe compose ev.left)
 
@@ -22,7 +22,7 @@ abstract class ReInstances {
       profunctorRe[P, S, T].dimap(fab)(f)(g)
   }
 
-  implicit def cochoiceRe[P[_, _], S, T](implicit ev: Cochoice[P]): Choice[Re[P, S, T, *, *]] = new Choice[Re[P, S, T, *, *]] {
+  implicit final def cochoiceRe[P[_, _], S, T](implicit ev: Cochoice[P]): Choice[Re[P, S, T, *, *]] = new Choice[Re[P, S, T, *, *]] {
     override def left[A, B, C](pab: Re[P, S, T, A, B]): Re[P, S, T, Either[A, C], Either[B, C]] =
       Re(pab.runRe compose ev.unleft)
 
@@ -33,7 +33,7 @@ abstract class ReInstances {
       profunctorRe[P, S, T].dimap(fab)(f)(g)
   }
 
-  implicit def strongRe[P[_, _], S, T](implicit ev: Strong[P]): Costrong[Re[P, S, T, *, *]] = new Costrong[Re[P, S, T, *, *]] {
+  implicit final def strongRe[P[_, _], S, T](implicit ev: Strong[P]): Costrong[Re[P, S, T, *, *]] = new Costrong[Re[P, S, T, *, *]] {
     override def unfirst[A, B, C](p: Re[P, S, T, (A, C), (B, C)]): Re[P, S, T, A, B] =
       Re(p.runRe compose ev.first)
 
@@ -44,7 +44,7 @@ abstract class ReInstances {
       profunctorRe[P, S, T].dimap(fab)(f)(g)
   }
 
-  implicit def costrongRe[P[_, _], S, T](implicit ev: Costrong[P]): Strong[Re[P, S, T, *, *]] = new Strong[Re[P, S, T, *, *]] {
+  implicit final def costrongRe[P[_, _], S, T](implicit ev: Costrong[P]): Strong[Re[P, S, T, *, *]] = new Strong[Re[P, S, T, *, *]] {
     override def first[A, B, C](fa: Re[P, S, T, A, B]): Re[P, S, T, (A, C), (B, C)] =
       Re(fa.runRe compose ev.unfirst)
 
