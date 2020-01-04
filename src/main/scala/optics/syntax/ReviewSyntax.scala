b0ac999be580ab4105cc742dc81d5c42c1182256
Sagi <sagi.fogel@gmail.com>
2020-01-04 23:55:38 +0200

    reformatting

--- src/main/scala/optics/syntax/ReviewSyntax.scala
+++ src/main/scala/optics/syntax/ReviewSyntax.scala
@@ -5,6 +5,6 @@ import optics.internal.Tagged
 
 object ReviewSyntax {
   implicit class ReviewOps[S, T, A, B](val _review: Optic[Tagged, S, T, A, B]) extends AnyVal {
-      def review(b: B): T = _review(Tagged[A, B](b)).runTag
+    def review(b: B): T = _review(Tagged[A, B](b)).runTag
   }
 }
