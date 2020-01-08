688aeee2656d696bba9dfe6eb5de96cf94cded65
Sagi <sagi.fogel@gmail.com>
2020-01-08 08:08:47 +0200

    addition of foldlOf, allOf, anyOf, findOf

--- src/main/scala/optics/syntax/IndexedFoldSyntax.scala
+++ src/main/scala/optics/syntax/IndexedFoldSyntax.scala
@@ -1,13 +1,16 @@
 package optics.syntax
 
+import cats.syntax.option._
 import cats.mtl.MonadState
 import optics.IndexedOptic
 import optics.internal.{Forget, Indexed}
-import optics.newtype.Endo
+import optics.newtype.{Conj, Disj, Endo}
+import optics.syntax.FunctionSyntax._
 
 import scala.Function.uncurried
 
 object IndexedFoldSyntax {
+
   implicit class IndexedFoldOps[R, I, S, T, A, B](val indexedFold: IndexedOptic[Forget[R, *, *], I, S, T, A, B]) extends AnyVal {
     def foldMapOf(f: I => A => R)(s: S): R = indexedFold(Indexed(Forget(uncurried(f).tupled))).runForget(s)
   }
@@ -19,8 +22,28 @@ object IndexedFoldSyntax {
 
   }
 
-  implicit class IndexedFoldEndoOps[R, I, S, T, A, B](val indexedFold: IndexedOptic[Forget[Endo[* => *, R], *, *], I, S, T, A, B]) extends AnyVal {
-    def foldOf(f: I => A => R => R)(r: R)(s: S): R =
+  implicit class IndexedFoldrEndoOps[R, I, S, T, A, B](val indexedFold: IndexedOptic[Forget[Endo[* => *, R], *, *], I, S, T, A, B]) extends AnyVal {
+    def foldrOf(f: I => A => R => R)(r: R)(s: S): R =
       indexedFold.foldMapOf(i => Endo[* => *, R] _ compose f(i))(s).runEndo(r)
+  }
+
+  implicit class IndexedFoldlEndoOps[R, I, S, T, A, B](val indexedFold: IndexedOptic[Forget[Endo[* => *, R], *, *], I, S, T, A, B]) extends AnyVal {
+    def foldlOf(f: I => R => A => R)(r: R)(s: S): R =
+      indexedFold.foldMapOf(i => Endo[* => *, R] _ compose f(i).flip)(s).runEndo(r)
+  }
+
+  implicit class IndexedFoldConjOps[R, I, S, T, A, B](val indexedFold: IndexedOptic[Forget[Conj[R], *, *], I, S, T, A, B]) extends AnyVal {
+    def allOf(f: I => A => R)(s: S): R =
+      indexedFold.foldMapOf(i => Conj[R] _ compose f(i))(s).runConj
+  }
+
+  implicit class IndexedFoldDisjOps[R, I, S, T, A, B](val indexedFold: IndexedOptic[Forget[Disj[R], *, *], I, S, T, A, B]) extends AnyVal {
+    def anyOf(f: I => A => R)(s: S): R =
+      indexedFold.foldMapOf(i => Disj[R] _ compose f(i))(s).runDisj
+  }
+
+  implicit class IndexedFoldOptionOps[R, I, S, T, A, B](val indexedFold: IndexedOptic[Forget[Endo[* => *, Option[A]], *, *], I, S, T, A, B]) extends AnyVal {
+    def findOf(f: I => A => Boolean)(s: S): Option[A] =
+      indexedFold.foldrOf(i => a => _.fold(if (f(i)(a)) a.some else None)(Some[A]))(None)(s)
   }
 }
