83b335f94524c684550adf492fd266de10989c20
Sagi <sagi.fogel@gmail.com>
2020-01-10 01:13:16 +0200

    apply method for Traversal/Traversal_

--- src/main/scala/optics/Traversal.scala
+++ src/main/scala/optics/Traversal.scala
@@ -15,13 +15,18 @@ abstract class Traversal[P[_, _] : Wander, S, T, A, B] extends Optic[P, S, T, A,
 }
 
 object Traversal {
-  def apply[P[_, _], S, T, A, B](implicit ev: Wander[P]): Traversal[P, S, T, A, B] = new Traversal[P, S, T, A, B] {
+  def apply[P[_, _], S, T, A, B](get: S => A)(set: S => B => T)(implicit ev: Wander[P]): Traversal[P, S, T, A, B] = new Traversal[P, S, T, A, B] {
     override def apply(pab: P[A, B]): P[S, T] = {
       val traversing = new Traversing[S, T, A, B] {
-        override def apply[F[_]](fab: A => F[B])(implicit ev: Applicative[F]): S => F[T] = ???
+        override def apply[F[_]](fab: A => F[B])(implicit ev: Applicative[F]): S => F[T] = s =>
+          ev.map(fab(get(s)))(set(s))
       }
 
       ev.wander(traversing)(pab)
     }
   }
+}
+
+object Traversal_ {
+  def apply[P[_, _], S, A](get: S => A)(set: S => A => S)(implicit ev: Wander[P]): Traversal_[P, S, A] = Traversal(get)(set)
 }
