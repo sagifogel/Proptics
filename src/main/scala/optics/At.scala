680c758da64ef7c816609332273e656429ae9a7f
Sagi <sagi.fogel@gmail.com>
2020-01-10 21:54:54 +0200

    addition of atOption

--- src/main/scala/optics/At.scala
+++ src/main/scala/optics/At.scala
@@ -5,16 +5,25 @@ import cats.syntax.option._
 import optics.internal.Wander
 import optics.Index._
 
-trait At[P[_, _], A, B, C] extends Index[P, A, B, C] {
-  def at(a: A): Lens_[P, A, Option[C]]
+import scala.Function.const
+
+trait At[P[_, _], M, A, B] extends Index[P, M, A, B] {
+  def at(a: A): Lens_[P, M, Option[B]]
 }
 
 abstract class AtInstances {
   implicit final def atIdentity[P[_, _] : Wander, A]: At[P, Id[A], Unit, A] = new At[P, Id[A], Unit, A] {
-    override def at(a: Id[A]): Lens_[P, Id[A], Option[A]] =
+    override def at(a: Unit): Lens_[P, Id[A], Option[A]] =
       Lens_[P, Id[A], Option[A]](_.some)(a => _.getOrElse(a))
 
-    override def ix(f: Id[A]): Traversal_[P, Id[A], A] = indexIdentity[P, A].ix(f)
+    override def ix(a: Unit): optics.Traversal_[P, Id[A], A] = indexIdentity[P, A].ix(a)
+  }
+
+  implicit final def atOption[P[_, _] : Wander, A]: At[P, Option[A], Unit, A] = new At[P, Option[A], Unit, A] {
+    override def at(a: Unit): Lens_[P, Option[A], Option[A]] =
+      Lens_[P, Option[A], Option[A]](identity)(const(identity))
+
+    override def ix(a: Unit): Traversal_[P, Option[A], A] = indexOption[P, A].ix(a)
   }
 }
 
