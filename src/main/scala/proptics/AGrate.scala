690f22c30c5053dcf22412de23c70a5807655ef9
Sagi <sagi.fogel@gmail.com>
2020-01-14 03:29:48 +0200

    addition of withGrate, cloneGrate

--- src/main/scala/proptics/AGrate.scala
+++ src/main/scala/proptics/AGrate.scala
@@ -1,6 +1,7 @@
 package proptics
 
 import proptics.internal.Grating
+import proptics.profunctor.Closed
 
 /**
  * An [[Optic] with fixed type [[Grating]] [[cats.arrow.Profunctor]]
@@ -10,5 +11,8 @@ import proptics.internal.Grating
  * @tparam A the target of an [[AGrate]]
  * @tparam B the modified target of an [[AGrate]]
  */
-abstract class AGrate[S, T, A, B] extends Optic[Grating[A, B, *, *], S, T, A, B] {
+abstract class AGrate[S, T, A, B] extends Optic[Grating[A, B, *, *], S, T, A, B] { self =>
+  def cloneGrate[P[_, _]: Closed]: Grate[P, S, T, A, B] = Grate(f => withGrate(f))
+
+  def withGrate(f: (S => A) => B): T = self(Grating(f => f(identity[A]))).runGrating(f)
 }
