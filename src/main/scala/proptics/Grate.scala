05f7fd901642a372230d67e8ed98487d0090227f
Sagi <sagi.fogel@gmail.com>
2020-01-21 23:42:28 +0200

    addition of cotraversed

--- src/main/scala/proptics/Grate.scala
+++ src/main/scala/proptics/Grate.scala
@@ -6,32 +6,33 @@ import proptics.syntax.FunctionSyntax._
 
 /**
  * <a href="http://r6research.livejournal.com/28050.html">A [[Grate]]</a>
+ *
  * @tparam P an evidence of [[Closed]] [[cats.arrow.Profunctor]]
  * @tparam S the source of an [[Grate]]
  * @tparam T the modified source of an [[Grate]]
  * @tparam A the target of an [[Grate]]
  * @tparam B the modified target of an [[Grate]]
  */
-abstract class Grate[P[_, _]: Closed, S, T, A, B] extends Optic[P, S, T, A, B] { self =>
+abstract class Grate[P[_, _] : Closed, S, T, A, B] extends Optic[P, S, T, A, B] {
+  self =>
 }
 
 object Grate {
   private[proptics] def apply[P[_, _], S, T, A, B](f: P[A, B] => P[S, T])(implicit ev: Closed[P]): Grate[P, S, T, A, B] = new Grate[P, S, T, A, B] {
-      override def apply(pab: P[A, B]): P[S, T] = f(pab)
+    override def apply(pab: P[A, B]): P[S, T] = f(pab)
   }
 
   def apply[P[_, _], S, T, A, B](to: ((S => A) => B) => T)(implicit ev: Closed[P], ev2: DummyImplicit): Grate[P, S, T, A, B] = grate(to)
 
-  def grate[P[_, _], S, T, A, B](to: ((S => A) => B) => T)(implicit ev: Closed[P]): Grate[P, S, T, A, B] = {
+  def grate[P[_, _], S, T, A, B](to: ((S => A) => B) => T)(implicit ev: Closed[P]): Grate[P, S, T, A, B] =
     Grate((pab: P[A, B]) => {
       ev.dimap[(S => A) => A, (S => A) => B, S, T](ev.closed(pab))(_.`#`)(to)
     })
-  }
 
-  def cotraversed[P[_, _]: Closed, F[_], A, B](implicit ev: Distributive[F]): Grate[P, F[A], F[B], A, B] = {
-    def cotraverse[G[_]](f: G[A] => B)(gfa: G[F[A]])(implicit ev1: Functor[G]): F[B] =
+  def cotraversed[P[_, _] : Closed, F[_], A, B](implicit ev: Distributive[F], ev1: Functor[F[A] => *]): Grate[P, F[A], F[B], A, B] = {
+    def cotraverse[G[_]: Functor](f: G[A] => B)(gfa: G[F[A]]): F[B] =
       ev.map(ev.cosequence(gfa))(f)
 
-    ???
+    Grate.grate[P, F[A], F[B], A, B](cotraverse(_)(identity))
   }
 }
