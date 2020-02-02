a28bfcac4cbcb1e2e703946b2e4811bb6339266a
Sagi <sagi.fogel@gmail.com>
2020-02-02 02:03:47 +0200

    addition of traverseOf, sequenceOf

--- src/main/scala/proptics/syntax/StarSyntax.scala
+++ src/main/scala/proptics/syntax/StarSyntax.scala
@@ -6,6 +6,14 @@ import proptics.newtype.Disj
 import proptics.profunctor.Star
 
 object StarSyntax {
+  implicit class TraverseOfStarOps[P[_, _], F[_], S, T, A, B](val traverse: Optic[Star[F, *, *], S, T, A, B]) extends AnyVal {
+    def traverseOf(f: A => F[B])(s: S): F[T] = traverse(Star(f)).runStar(s)
+  }
+
+  implicit class SequenceOfstarOps[P[_, _], F[_], S, T, A, B](val traverse: Optic[Star[F, *, *], S, T, F[A], A]) extends AnyVal {
+    def sequenceOf(s: S): F[T] = traverse.traverseOf(identity)(s)
+  }
+
   implicit class StarOps[F[_], S, T, A, B](val star: Optic[Star[F, *, *], S, T, A, B]) extends AnyVal {
     def collectOf(f: A => F[B]): S => F[T] = star(Star(f)).runStar
   }
