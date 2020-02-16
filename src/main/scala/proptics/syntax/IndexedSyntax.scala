ec1eb2bd471a6cfc5cb6df9c097680efb513ded3
Sagi <sagi.fogel@gmail.com>
2020-02-16 23:36:07 +0200

    implementation of elementsOf

--- src/main/scala/proptics/syntax/IndexedSyntax.scala
+++ src/main/scala/proptics/syntax/IndexedSyntax.scala
@@ -1,19 +1,27 @@
 package proptics.syntax
 
+import cats.Applicative
+import proptics.internal.{Indexed, Wander}
+import proptics.profunctor.Star
+import proptics.rank2types.LensLikeIndexedTraversal
 import proptics.{IndexedOptic, IndexedTraversal}
+import proptics.IndexedTraversal.iwander
 
 object IndexedSyntax {
   implicit class IndexedTraversalOps[P[_, _], I, S, T, A](val indexedTraversal: IndexedTraversal[I, S, T, A, A]) extends AnyVal {
-    def elementsOf(pr: I => Boolean): IndexedOptic[P, I, S, T, A, A] = ???
-//      IndexedTraversal.iwander(new LensLikeIndexedTraversal[I, S, T, A, A] {
-//        override def apply[F[_]](f: I => A => F[A])(implicit ev1: Applicative[F]): S => F[T] = s => {
-//          val starIndex: Indexed[Star[F, *, *], I, A, A] = Indexed[Star[F, *, *], I, A, A](Star {
-//            case (i, a) => if (pr(i)) f(i)(a) else ev1.pure(a)
-//          })
-//
-//
-//          indexedTraversal.(starIndex)
-//        }
-//      }
+    def elementsOf(pr: I => Boolean)(implicit ev0: Wander[P]): IndexedOptic[P, I, S, T, A, A] = {
+      val indexTraversal: IndexedTraversal[I, S, T, A, A] =
+        iwander(new LensLikeIndexedTraversal[I, S, T, A, A] {
+          override def apply[F[_]](f: I => A => F[A])(implicit ev: Applicative[F]): S => F[T] = {
+            val starIndex: Indexed[Star[F, *, *], I, A, A] = Indexed[Star[F, *, *], I, A, A](Star {
+              case (i, a) => if (pr(i)) f(i)(a) else ev.pure(a)
+            })
+
+            indexedTraversal(starIndex).runStar
+          }
+        })
+
+      IndexedOptic(indexTraversal(_))
+    }
   }
 }
