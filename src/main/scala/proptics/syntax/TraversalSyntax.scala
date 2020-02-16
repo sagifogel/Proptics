53806bf862314ba21e9c8edff220438c1a10d15f
Sagi <sagi.fogel@gmail.com>
2020-02-16 23:35:10 +0200

    implementation of elementsOf

--- src/main/scala/proptics/syntax/TraversalSyntax.scala
+++ src/main/scala/proptics/syntax/TraversalSyntax.scala
@@ -1,17 +1,15 @@
 package proptics.syntax
 
+import cats.data.State
 import cats.instances.int._
 import cats.syntax.eq._
 import proptics.internal.Wander
 import proptics.syntax.IndexedSyntax._
-import proptics.{IndexedTraversal, Optic, Traversal}
+import proptics.{Optic, Traversal}
 
 object TraversalSyntax {
-  implicit class TraversalOps[P[_, _], I, S, T, A, B](val traversal: Traversal[S, T, A, B]) extends AnyVal {
-    def positions(implicit ev: Wander[P]): IndexedTraversal[Int, S, T, A, B] = ???
-  }
-
   implicit class TraversalElementsOps[P[_, _]: Wander, I, S, T, A](val traversal: Traversal[S, T, A, A]) {
-    def element(i: Int): Optic[P, S, T, A, A] = traversal.positions.elementsOf(_ === 0).unIndex
+    def element(i: Int)(implicit ev: State[Int, Int]): Optic[P, S, T, A, A] =
+      traversal.positions.elementsOf(_ === 0).unIndex
   }
 }
