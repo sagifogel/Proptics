c269da6a42b3e170639ed4643ba76310876bbd29
Sagi <sagi.fogel@gmail.com>
2020-02-28 15:13:53 +0200

    rename to indexed

--- src/main/scala/proptics/rank2types/Rank2TypeIndexedTraversalLike.scala
+++ src/main/scala/proptics/rank2types/Rank2TypeIndexedTraversalLike.scala
@@ -3,5 +3,5 @@ package proptics.rank2types
 import proptics.internal.{Indexed, Wander}
 
 trait Rank2TypeIndexedTraversalLike[I, S, T, A, B] {
-  def apply[P[_, _]](index: Indexed[P, I, A, B])(implicit ev: Wander[P]): P[S, T]
+  def apply[P[_, _]](indexed: Indexed[P, I, A, B])(implicit ev: Wander[P]): P[S, T]
 }
