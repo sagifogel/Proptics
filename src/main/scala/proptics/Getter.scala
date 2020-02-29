0059e9278fe3148897d0449903e8749d555c7eb2
Sagi <sagi.fogel@gmail.com>
2020-03-01 01:04:48 +0200

    addition of polymorphic universal quantifier R to the apply method

--- src/main/scala/proptics/Getter.scala
+++ src/main/scala/proptics/Getter.scala
@@ -6,28 +6,25 @@ import proptics.internal.Forget
 /**
  * A [[Getter]] is a [[Fold]]
  *
- * @tparam R the return type of a [[Getter]]
  * @tparam S the source of a [[Getter]]
  * @tparam T the modified source of a [[Getter]]
  * @tparam A the target of a [[Getter]]
  * @tparam B the modified target of a [[Getter]]
  */
-abstract class Getter[R, S, T, A, B] extends Fold[R, S, T, A, B] {
+abstract class Getter[S, T, A, B] extends Serializable {
+  def apply[R](forget: Forget[R, A, B]): Forget[R, S, T]
 }
 
 object Getter {
-  private[Getter] def apply[R, S, T, A, B](f: Forget[R, A, B] => Forget[R, S, T]): Getter[R, S, T, A, B] = new Getter[R, S, T, A, B] {
-    override def apply(forget: Forget[R, A, B]): Forget[R, S, T] = f(forget)
+  def apply[S, T, A, B](f: S => A): Getter[S, T, A, B] = new Getter[S, T, A, B] {
+    override def apply[R](forget: Forget[R, A, B]): Forget[R, S, T] = liftForget[R, S, T, A, B](f)(forget)
   }
 
-  def apply[R, S, T, A, B](f: S => A)(implicit ev: DummyImplicit): Getter[R, S, T, A, B] =
-    Getter(liftForget[R, S, T, A, B](f))
-
-  def to[R, S, T, A, B](f: S => A): Getter[R, S, T, A, B] = Getter(f)
+  def to[S, T, A, B](f: S => A): Getter[S, T, A, B] = Getter(f)
 }
 
 object Getter_ {
-  def apply[R, S, A](f: S => A): Getter_[R, S, A] = to(f)
+  def apply[S, A](f: S => A): Getter_[S, A] = to(f)
 
-  def to[R, S, T, A, B](f: S => A): Getter_[R, S, A] = Getter(f)
+  def to[S, T, A, B](f: S => A): Getter_[S, A] = Getter(f)
 }
