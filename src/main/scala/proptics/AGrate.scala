e0e80d891e50279ba665bbe2397af6b22ff47619
Sagi <sagi.fogel@gmail.com>
2020-02-29 12:01:11 +0200

    addition of apply method for AGrate,  AGrate_

--- src/main/scala/proptics/AGrate.scala
+++ src/main/scala/proptics/AGrate.scala
@@ -16,11 +16,18 @@ abstract class AGrate[S, T, A, B] { self =>
 
   def cloneGrate: Grate[S, T, A, B] = Grate(withGrate _)
 
-  def withGrate(f: (S => A) => B): T = self.apply(Grating(_.apply(identity))).runGrating(f)
+  def withGrate(f: (S => A) => B): T = self(Grating(_.apply(identity))).runGrating(f)
 }
 
 object AGrate {
-  def apply[S, T, A, B](f: Grating[A, B, A, B] => Grating[A, B, S, T]): AGrate[S, T, A, B] = new AGrate[S, T, A, B] {
+  private[proptics] def apply[S, T, A, B](f: Grating[A, B, A, B] => Grating[A, B, S, T]): AGrate[S, T, A, B] = new AGrate[S, T, A, B] {
     override def apply[P[_, _]](grating: Grating[A, B, A, B])(implicit ev: Closed[P]): Grating[A, B, S, T] = f(grating)
   }
+
+  def apply[S, T, A, B](to: ((S => A) => B) => T)(implicit ev: DummyImplicit): AGrate[S, T, A, B] = AGrate((_: Grating[A, B, A, B]) => Grating(to))
+}
+
+
+object AGrate_ {
+  def apply[S, A](to: ((S => A) => A) => S): AGrate_[S, A] = AGrate(to)
 }
