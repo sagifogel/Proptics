eb1b730d178b6573b2e7ce61e733d3bcb2810312
Sagi <sagi.fogel@gmail.com>
2020-03-02 23:16:20 +0200

    removed unnecessary type params

--- src/main/scala/proptics/AGrate.scala
+++ src/main/scala/proptics/AGrate.scala
@@ -1,10 +1,9 @@
 package proptics
 
 import proptics.internal.Grating
-import proptics.profunctor.Closed
 
 /**
- * An [[Optic] with fixed type [[Grating]] [[cats.arrow.Profunctor]]
+ * An Optic with fixed type [[Grating [[cats.arrow.Profunctor]]
  *
  * @tparam S the source of an [[AGrate]]
  * @tparam T the modified source of an [[AGrate]]
@@ -12,7 +11,7 @@ import proptics.profunctor.Closed
  * @tparam B the modified target of an [[AGrate]]
  */
 abstract class AGrate[S, T, A, B] { self =>
-  def apply[P[_, _]](grating: Grating[A, B, A, B])(implicit ev: Closed[P]): Grating[A, B, S, T]
+  def apply(grating: Grating[A, B, A, B]): Grating[A, B, S, T]
 
   def cloneGrate: Grate[S, T, A, B] = Grate(withGrate _)
 
@@ -21,7 +20,7 @@ abstract class AGrate[S, T, A, B] { self =>
 
 object AGrate {
   private[proptics] def apply[S, T, A, B](f: Grating[A, B, A, B] => Grating[A, B, S, T]): AGrate[S, T, A, B] = new AGrate[S, T, A, B] {
-    override def apply[P[_, _]](grating: Grating[A, B, A, B])(implicit ev: Closed[P]): Grating[A, B, S, T] = f(grating)
+    override def apply(grating: Grating[A, B, A, B]): Grating[A, B, S, T] = f(grating)
   }
 
   def apply[S, T, A, B](to: ((S => A) => B) => T)(implicit ev: DummyImplicit): AGrate[S, T, A, B] = AGrate((_: Grating[A, B, A, B]) => Grating(to))
