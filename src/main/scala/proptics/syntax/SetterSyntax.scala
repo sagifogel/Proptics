6d37e7f6789d87ed98f69ba3de26510d196dd89c
Sagi <sagi.fogel@gmail.com>
2020-03-02 23:10:15 +0200

    over, set were moved to Lens, Syntax has been changed to Setter

--- src/main/scala/proptics/syntax/SetterSyntax.scala
+++ src/main/scala/proptics/syntax/SetterSyntax.scala
@@ -3,19 +3,13 @@ package proptics.syntax
 import algebra.lattice.Heyting
 import cats.kernel.Semigroup
 import cats.mtl.MonadState
-import proptics.Optic
+import proptics.Setter
 import spire.algebra.{Field, Ring, Semiring}
 
 import scala.Function.const
 
 object SetterSyntax {
-  implicit class SetterOps[S, T, A, B](val setter: Optic[* => *, S, T, A, B]) extends AnyVal {
-    def over(f: A => B): S => T = setter(f)
-
-    def set(b: B): S => T = over(const(b))
-  }
-
-  implicit class SetterSTAAOps[S, T, A](val setter: Optic[* => *, S, T, A, A]) extends AnyVal {
+  implicit class SetterSTAAOps[S, T, A](val setter: Setter[S, T, A, A]) extends AnyVal {
     def addOver(a: A)(implicit ev: Semiring[A]): S => T = setter.over(ev.additive.combine(_, a))
 
     def mulOver(a: A)(implicit ev: Semiring[A]): S => T = setter.over(ev.multiplicative.combine(_, a))
@@ -31,14 +25,14 @@ object SetterSyntax {
     def appendOver(a: A)(implicit ev: Semigroup[A]): S => T = setter.over(ev.combine(_, a))
   }
 
-  implicit class SetterSSABOps[S, A, B](val setter: Optic[* => *, S, S, A, B]) extends AnyVal {
+  implicit class SetterSSABOps[S, A, B](val setter: Setter[ S, S, A, B]) extends AnyVal {
     def assign[F[_]](b: B)(implicit ev: MonadState[F, S]): F[Unit] = modifying(const(b))
 
     def modifying[F[_]](f: A => B)(implicit ev: MonadState[F, S]): F[Unit] =
       ev.monad.void(ev.modify(setter.over(f)(_)))
   }
 
-  implicit class SetterSAOps[S, A](val setter: Optic[* => *, S, S, A, A]) extends AnyVal {
+  implicit class SetterSAOps[S, A](val setter: Setter[S, S, A, A]) extends AnyVal {
     def addModifying[F[_]](a: A)(implicit ev: MonadState[F, S], ev2: Semiring[A]): F[Unit] =
       setter.modifying(ev2.additive.combine(a, _))
 
@@ -61,11 +55,11 @@ object SetterSyntax {
       setter.modifying(ev2.combine(a, _))
   }
 
-  implicit class SetterSTAOptionB[S, T, A, B](val setter: Optic[* => *, S, T, A, Option[B]]) extends AnyVal {
+  implicit class SetterSTAOptionB[S, T, A, B](val setter: Setter[S, T, A, Option[B]]) extends AnyVal {
     def setJust(b: B): S => T = setter.set(Some(b))
   }
 
-  implicit class SetterSSAOptionB[F[_], S, A, B](val setter: Optic[* => *, S, S, A, Option[B]]) extends AnyVal {
+  implicit class SetterSSAOptionB[F[_], S, A, B](val setter: Setter[S, S, A, Option[B]]) extends AnyVal {
     def assignJust(b: B)(implicit ev: MonadState[F, S]): F[Unit] = setter.assign(Some(b))
   }
 }
