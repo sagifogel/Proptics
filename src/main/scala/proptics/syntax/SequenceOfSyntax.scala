ac364c8afbb194810588e142d98508dd3cad8a4d
Sagi Fogel <sagi@hunters.ai>
2020-03-03 20:34:13 +0200

    rename to SequenceOfSyntax

--- src/main/scala/proptics/syntax/SequenceOfSyntax.scala
+++ src/main/scala/proptics/syntax/SequenceOfSyntax.scala
@@ -4,7 +4,7 @@ import cats.arrow.Strong
 import proptics.Lens
 import proptics.profunctor.Star
 
-object StarSyntax {
+object SequenceOfSyntax {
   implicit class SequenceOfLensStarOps[F[_], S, T, A](val lens: Lens[S, T, F[A], A]) extends AnyVal {
     def sequenceOf(s: S)(implicit ev: Strong[Star[F, *, *]]): F[T] = lens.traverseOf(identity)(s)
   }
