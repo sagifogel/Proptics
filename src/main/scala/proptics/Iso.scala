849dfc8334d016a049b8baa7858898de89c97012
Sagi <sagi.fogel@gmail.com>
2020-03-06 01:32:21 +0200

    addition of over, set, asLens

--- src/main/scala/proptics/Iso.scala
+++ src/main/scala/proptics/Iso.scala
@@ -1,13 +1,14 @@
 package proptics
 
 import cats.Eq
-import cats.arrow.Profunctor
+import cats.arrow.{Profunctor, Strong}
 import cats.syntax.eq._
 import cats.syntax.option._
 import proptics.internal.{Forget, Re}
 import proptics.rank2types.Rank2TypeIsoLike
 import proptics.syntax.FunctionSyntax._
 
+import scala.Function.const
 import scala.{Function => F}
 
 /**
@@ -18,14 +19,22 @@ import scala.{Function => F}
  * @tparam A the target of a [[Iso]]
  * @tparam B the modified target of a [[Iso]]
  */
-abstract class Iso[S, T, A, B] { self =>
+abstract class Iso[S, T, A, B] extends Serializable { self =>
   def apply[P[_, _]](pab: P[A, B])(implicit ev: Profunctor[P]): P[S, T]
 
   def view[R](s: S): A = self[Forget[A, *, *]](Forget(identity[A])).runForget(s)
 
+  def over(f: A => B)(implicit ev: Profunctor[* => *]): S => T = self(f)
+
+  def set(b: B)(implicit ev: Profunctor[* => *]): S => T = over(const(b))
+
   def reverse: Iso[B, A, T, S] = new Iso[B, A, T, S] {
     override def apply[P[_, _]](pab: P[T, S])(implicit ev: Profunctor[P]): P[B, A] =
       self(Re(identity[P[B, A]])).runRe(pab)
+  }
+
+  def asLens: Lens[S, T, A, B] = new Lens[S, T, A, B] {
+    override private[proptics] def apply[P[_, _]](pab: P[A, B])(implicit ev: Strong[P]) = self(pab)
   }
 }
 
