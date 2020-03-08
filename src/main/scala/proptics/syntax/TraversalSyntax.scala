72a74ce214cb606a8f7c095774791f1858150dfa
Sagi <sagi.fogel@gmail.com>
2020-03-08 02:02:40 +0200

    change element to return IndexedTraversal

--- src/main/scala/proptics/syntax/TraversalSyntax.scala
+++ src/main/scala/proptics/syntax/TraversalSyntax.scala
@@ -3,13 +3,13 @@ package proptics.syntax
 import cats.data.State
 import cats.instances.int._
 import cats.syntax.eq._
+import proptics.Traversal
 import proptics.internal.Wander
 import proptics.syntax.IndexedTraversalSyntax._
-import proptics.{Optic, Traversal}
 
 object TraversalSyntax {
   implicit class TraversalElementsOps[P[_, _]: Wander, I, S, T, A](val traversal: Traversal[S, T, A, A]) {
-    def element(i: Int)(implicit ev: State[Int, A]): Optic[P, S, T, A, A] =
-      traversal.positions.elementsOf(_ === 0).unIndex
+    def element(i: Int)(implicit ev: State[Int, A]): Traversal[S, T, A, A] =
+      traversal.positions.elements(_ === 0).unIndex
   }
 }
