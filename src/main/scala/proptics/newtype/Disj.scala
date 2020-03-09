1f1a4d956d30aabe9ee2f9b2fd6a2590cf66dbee
Sagi <sagi.fogel@gmail.com>
2020-03-10 00:45:43 +0200

    addition of monoidDisj

--- src/main/scala/proptics/newtype/Disj.scala
+++ src/main/scala/proptics/newtype/Disj.scala
@@ -1,3 +1,16 @@
 package proptics.newtype
 
+import algebra.lattice.Heyting
+import cats.kernel.Monoid
+
 final case class Disj[A](runDisj: A) extends AnyVal
+
+abstract class DisjInstances {
+  implicit final def monoidDisj[A](implicit ev: Heyting[A]): Monoid[Disj[A]] = new Monoid[Disj[A]] {
+    override def empty: Disj[A] = Disj(ev.zero)
+
+    override def combine(x: Disj[A], y: Disj[A]): Disj[A] = Disj(ev.or(x.runDisj, y.runDisj))
+  }
+}
+
+object Disj extends DisjInstances
