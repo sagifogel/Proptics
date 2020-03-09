2b8fa5f9b1137586e9b16adbd0786c2bb102e0ad
Sagi <sagi.fogel@gmail.com>
2020-03-10 00:45:04 +0200

    addition of monoidConj

--- src/main/scala/proptics/newtype/Conj.scala
+++ src/main/scala/proptics/newtype/Conj.scala
@@ -1,3 +1,16 @@
 package proptics.newtype
 
+import algebra.lattice.Heyting
+import cats.kernel.Monoid
+
 final case class Conj[A](runConj: A) extends AnyVal
+
+abstract class ConjInstances {
+  implicit final def monoidConj[A](implicit ev: Heyting[A]): Monoid[Conj[A]] = new Monoid[Conj[A]] {
+    override def empty: Conj[A] = Conj(ev.one)
+
+    override def combine(x: Conj[A], y: Conj[A]): Conj[A] = Conj(ev.and(x.runConj, y.runConj))
+  }
+}
+
+object Conj extends ConjInstances
