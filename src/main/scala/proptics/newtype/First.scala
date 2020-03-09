2f4859aef819e06eebc6d0661cf77dce2f906561
Sagi <sagi.fogel@gmail.com>
2020-03-10 00:46:11 +0200

    addition of monoidFirst

--- src/main/scala/proptics/newtype/First.scala
+++ src/main/scala/proptics/newtype/First.scala
@@ -1,3 +1,15 @@
 package proptics.newtype
 
+import cats.kernel.Monoid
+
 final case class First[A](runFirst: Option[A]) extends AnyVal
+
+abstract class FirstInstances {
+  implicit final def monoidFirst[A]: Monoid[First[A]] = new Monoid[First[A]] {
+    def empty: First[A] = First(None)
+
+    def combine(x: First[A], y: First[A]): First[A] = First(x.runFirst orElse y.runFirst)
+  }
+}
+
+object First extends FirstInstances
