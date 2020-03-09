85c80b30bed57df293f79f134ba6d259d72fdb2a
Sagi <sagi.fogel@gmail.com>
2020-03-10 00:44:06 +0200

    addition of heytingBool

---
+++ src/main/scala/proptics/internal/heyting/HeytingInstances.scala
@@ -0,0 +1,19 @@
+package proptics.internal.heyting
+
+import spire.algebra.lattice.Heyting
+
+object HeytingInstances {
+  implicit final def heytingBool: Heyting[Boolean] = new Heyting[Boolean] {
+    override def and(a: Boolean, b: Boolean): Boolean = a && b
+
+    override def or(a: Boolean, b: Boolean): Boolean = a || b
+
+    override def imp(a: Boolean, b: Boolean): Boolean = !a || b
+
+    override def complement(a: Boolean): Boolean = !a
+
+    override def zero: Boolean = false
+
+    override def one: Boolean = true
+  }
+}
