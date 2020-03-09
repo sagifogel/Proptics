3ce54a64475fede98582c8f652c5057bdb260d7a
Sagi <sagi.fogel@gmail.com>
2020-03-10 00:44:43 +0200

    addition of monoidAdditive

--- src/main/scala/proptics/newtype/Additive.scala
+++ src/main/scala/proptics/newtype/Additive.scala
@@ -1,3 +1,16 @@
 package proptics.newtype
 
+import cats.syntax.monoid._
+import cats.kernel.Monoid
+
 final case class Additive[A](runAdditive: A) extends AnyVal
+
+abstract class AdditiveInstances {
+  implicit final def monoidAdditive[A](implicit ev: Monoid[A]): Monoid[Additive[A]] = new Monoid[Additive[A]] {
+    def empty: Additive[A] = Additive(ev.empty)
+
+    def combine(x: Additive[A], y: Additive[A]): Additive[A] = Additive(x.runAdditive |+| y.runAdditive)
+  }
+}
+
+object Additive extends AdditiveInstances
