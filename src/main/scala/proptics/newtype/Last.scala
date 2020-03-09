c9c35ac104565621ab7b5356c1e438b4cf0c676c
Sagi <sagi.fogel@gmail.com>
2020-03-10 00:46:24 +0200

    addition of monoidLast

--- src/main/scala/proptics/newtype/Last.scala
+++ src/main/scala/proptics/newtype/Last.scala
@@ -1,3 +1,15 @@
 package proptics.newtype
 
+import cats.kernel.Monoid
+
 final case class Last[A](runLast: Option[A]) extends AnyVal
+
+abstract class LastInstances {
+  implicit final def monoidLast[A]: Monoid[Last[A]] = new Monoid[Last[A]] {
+    def empty: Last[A] = Last(None)
+
+    def combine(x: Last[A], y: Last[A]): Last[A] = Last(y.runLast orElse x.runLast)
+  }
+}
+
+object Last extends LastInstances
