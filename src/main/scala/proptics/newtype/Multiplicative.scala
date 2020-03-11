ccae4000d863c5f3488e1dd609484e5fbff1dc80
Sagi <sagi.fogel@gmail.com>
2020-03-11 23:35:15 +0200

    addition of monoidMultiplicative implicit

--- src/main/scala/proptics/newtype/Multiplicative.scala
+++ src/main/scala/proptics/newtype/Multiplicative.scala
@@ -1,3 +1,18 @@
 package proptics.newtype
 
+import spire.syntax.semiring._
+import cats.kernel.Monoid
+import spire.algebra.Semiring
+
 final case class Multiplicative[A](runMultiplicative: A) extends AnyVal
+
+abstract class MultiplicativeInstances {
+  implicit final def monoidMultiplicative[A](implicit ev: Semiring[A]): Monoid[Multiplicative[A]] = new Monoid[Multiplicative[A]] {
+    def empty: Multiplicative[A] = Multiplicative(ev.zero)
+
+    def combine(x: Multiplicative[A], y: Multiplicative[A]): Multiplicative[A] =
+      Multiplicative(x.runMultiplicative * y.runMultiplicative)
+  }
+}
+
+object Multiplicative extends MultiplicativeInstances
