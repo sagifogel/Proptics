015eb1d4af996d22f07915c69a6fb4645568e29f
Sagi <sagi.fogel@gmail.com>
2020-03-11 23:36:53 +0200

    addition of monoidEndo implicit

--- src/main/scala/proptics/newtype/Endo.scala
+++ src/main/scala/proptics/newtype/Endo.scala
@@ -1,3 +1,16 @@
 package proptics.newtype
 
+import cats.kernel.Monoid
+
 final case class Endo[C[_, _], A](runEndo: C[A, A]) extends AnyVal
+
+abstract class EndoInstances {
+  implicit final def monoidEndo[A]: Monoid[Endo[* => *, A]] = new Monoid[Endo[* => *, A]] {
+    override def empty: Endo[* => *, A] = Endo(identity)
+
+    override def combine(x: Endo[* => *, A], y: Endo[* => *, A]): Endo[* => *, A] =
+      Endo(x.runEndo compose y.runEndo)
+  }
+}
+
+object Endo extends EndoInstances
