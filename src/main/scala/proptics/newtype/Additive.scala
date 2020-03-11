cc94b482fe9b684ee0255f40258eaa44e67ddb70
Sagi <sagi.fogel@gmail.com>
2020-03-11 23:36:16 +0200

    addition of monoidAdditive implicit

--- src/main/scala/proptics/newtype/Additive.scala
+++ src/main/scala/proptics/newtype/Additive.scala
@@ -1,15 +1,16 @@
 package proptics.newtype
 
-import cats.syntax.monoid._
+import spire.syntax.semiring._
 import cats.kernel.Monoid
+import spire.algebra.Semiring
 
 final case class Additive[A](runAdditive: A) extends AnyVal
 
 abstract class AdditiveInstances {
-  implicit final def monoidAdditive[A](implicit ev: Monoid[A]): Monoid[Additive[A]] = new Monoid[Additive[A]] {
-    def empty: Additive[A] = Additive(ev.empty)
+  implicit final def monoidAdditive[A](implicit ev: Semiring[A]): Monoid[Additive[A]] = new Monoid[Additive[A]] {
+    def empty: Additive[A] = Additive(ev.zero)
 
-    def combine(x: Additive[A], y: Additive[A]): Additive[A] = Additive(x.runAdditive |+| y.runAdditive)
+    def combine(x: Additive[A], y: Additive[A]): Additive[A] = Additive(x.runAdditive + y.runAdditive)
   }
 }
 
