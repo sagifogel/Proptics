590a852b3569b226ebfa529b649769b997d6dfc5
Sagi <sagi.fogel@gmail.com>
2020-03-12 20:37:40 +0200

    addition of monoidDual

--- src/main/scala/proptics/newtype/Dual.scala
+++ src/main/scala/proptics/newtype/Dual.scala
@@ -1,3 +1,16 @@
 package proptics.newtype
 
+import cats.kernel.Monoid
+import cats.syntax.monoid._
+
 final case class Dual[A](runDual: A) extends AnyVal
+
+abstract class DualInstances {
+  implicit final def monoidDual[A](implicit ev: Monoid[A]): Monoid[Dual[A]] = new Monoid[Dual[A]] {
+    def empty: Dual[A] = Dual(ev.empty)
+
+    def combine(x: Dual[A], y: Dual[A]): Dual[A] = Dual(x.runDual |+| y.runDual)
+  }
+}
+
+object Dual extends DualInstances
