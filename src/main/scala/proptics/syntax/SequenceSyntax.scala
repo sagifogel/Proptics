00a134a17b104482cb9b8040db3bc63cd9f8799a
Sagi <sagi.fogel@gmail.com>
2020-03-15 01:41:35 +0200

    sequence has been changed to return S => F[T]

--- src/main/scala/proptics/syntax/SequenceSyntax.scala
+++ src/main/scala/proptics/syntax/SequenceSyntax.scala
@@ -6,6 +6,6 @@ import proptics.profunctor.Star
 
 object SequenceSyntax {
   implicit class SequenceLensStarOps[F[_], S, T, A](val lens: Lens[S, T, F[A], A]) extends AnyVal {
-    def sequence(s: S)(implicit ev: Strong[Star[F, *, *]]): F[T] = lens.traverse(identity)(s)
+    def sequence(implicit ev: Strong[Star[F, *, *]]): S => F[T] = lens.traverse(identity)
   }
 }
