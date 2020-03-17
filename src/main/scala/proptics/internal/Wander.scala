dfc73e6580ee6c553bbf8c0d03e5e8ba290c9fdc
Sagi <sagi.fogel@gmail.com>
2020-03-18 00:52:08 +0200

    code reuse of choiceFunction, strongStar

--- src/main/scala/proptics/internal/Wander.scala
+++ src/main/scala/proptics/internal/Wander.scala
@@ -1,11 +1,12 @@
 package proptics.internal
 
 import cats.arrow.Strong
-import cats.syntax.either._
 import cats.{Applicative, Id}
 import proptics.newtype.Newtype
 import proptics.newtype.Newtype.Aux
 import proptics.profunctor.{Choice, Star}
+import proptics.profunctor.Star.strongStar
+import proptics.profunctor.Choice.{choiceFunction, choiceStar}
 
 trait Traversing[S, T, A, B] {
   def apply[F[_]](f: A => F[B])(implicit ev: Applicative[F]): S => F[T]
@@ -27,9 +28,9 @@ abstract class WanderInstances {
       alaF[Id, Id, S, S, T, T](identity)(traversal[Id](pab))
     }
 
-    override def left[A, B, C](pab: A => B): Either[A, C] => Either[B, C] = _.leftMap(pab)
+    override def left[A, B, C](pab: A => B): Either[A, C] => Either[B, C] = choiceFunction.left(pab)
 
-    override def right[A, B, C](pab: B => C): Either[A, B] => Either[A, C] = _.map(pab)
+    override def right[A, B, C](pab: B => C): Either[A, B] => Either[A, C] = choiceFunction.right(pab)
 
     override def first[A, B, C](fa: A => B): ((A, C)) => (B, C) = {
       case (a, c) => (fa(a), c)
@@ -39,34 +40,23 @@ abstract class WanderInstances {
       case (c, a) => (c, fa(a))
     }
 
-    override def dimap[A, B, C, D](fab: A => B)(f: C => A)(g: B => D): C => D = g compose fab compose f
+    override def dimap[A, B, C, D](fab: A => B)(f: C => A)(g: B => D): C => D = choiceFunction.dimap(fab)(f)(g)
   }
 
   implicit final def wanderStar[F[_]](implicit ev: Applicative[F]): Wander[Star[F, *, *]] = new Wander[Star[F, *, *]] {
     override def wander[S, T, A, B](traversal: Traversing[S, T, A, B])(pab: Star[F, A, B]): Star[F, S, T] =
       Star(traversal(pab.runStar))
 
-    override def left[A, B, C](pab: Star[F, A, B]): Star[F, Either[A, C], Either[B, C]] =
-      Star {
-        case Left(a) => ev.map(pab.runStar(a))(_.asLeft[C])
-        case Right(c) => ev.pure(c.asRight[B])
-      }
+    override def left[A, B, C](pab: Star[F, A, B]): Star[F, Either[A, C], Either[B, C]] = choiceStar.left(pab)
 
-    override def right[A, B, C](pab: Star[F, B, C]): Star[F, Either[A, B], Either[A, C]] =
-      Star {
-        case Left(a) => ev.pure(a.asLeft[C])
-        case Right(b) => ev.map(pab.runStar(b))(_.asRight[A])
-      }
+    override def right[A, B, C](pab: Star[F, B, C]): Star[F, Either[A, B], Either[A, C]] = choiceStar.right(pab)
 
-    override def first[A, B, C](fa: Star[F, A, B]): Star[F, (A, C), (B, C)] =
-      Star { case (a, c) => ev.map(fa.runStar(a))((_, c)) }
+    override def first[A, B, C](fa: Star[F, A, B]): Star[F, (A, C), (B, C)] = strongStar.first(fa)
 
-    override def second[A, B, C](fa: Star[F, A, B]): Star[F, (C, A), (C, B)] =
-      Star { case (c, a) => ev.map(fa.runStar(a))((c, _)) }
+    override def second[A, B, C](fa: Star[F, A, B]): Star[F, (C, A), (C, B)] = strongStar.second(fa)
 
-    override def dimap[A, B, C, D](fab: Star[F, A, B])(f: C => A)(g: B => D): Star[F, C, D] = {
-      Star(ev.lift(g) compose fab.runStar compose f)
-    }
+    override def dimap[A, B, C, D](fab: Star[F, A, B])(f: C => A)(g: B => D): Star[F, C, D] =
+      choiceStar.dimap(fab)(f)(g)
   }
 }
 
