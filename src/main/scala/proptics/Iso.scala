1f6cd99c7051e45fd75c35333b5e76c4e649315d
Sagi Fogel <sagi@hunters.ai>
2020-03-20 22:29:55 +0200

    make apply method private, rename reverse to re

--- src/main/scala/proptics/Iso.scala
+++ src/main/scala/proptics/Iso.scala
@@ -20,7 +20,7 @@ import scala.{Function => F}
  * @tparam B the modified target of a [[Iso]]
  */
 abstract class Iso[S, T, A, B] extends Serializable { self =>
-  def apply[P[_, _]](pab: P[A, B])(implicit ev: Profunctor[P]): P[S, T]
+  private[proptics] def apply[P[_, _]](pab: P[A, B])(implicit ev: Profunctor[P]): P[S, T]
 
   def view[R](s: S): A = self[Forget[A, *, *]](Forget(identity[A])).runForget(s)
 
@@ -28,13 +28,13 @@ abstract class Iso[S, T, A, B] extends Serializable { self =>
 
   def set(b: B)(implicit ev: Profunctor[* => *]): S => T = over(const(b))
 
-  def reverse: Iso[B, A, T, S] = new Iso[B, A, T, S] {
+  def re: Iso[B, A, T, S] = new Iso[B, A, T, S] {
     override def apply[P[_, _]](pab: P[T, S])(implicit ev: Profunctor[P]): P[B, A] =
       self(Re(identity[P[B, A]])).runRe(pab)
   }
 
   def asLens: Lens[S, T, A, B] = new Lens[S, T, A, B] {
-    override private[proptics] def apply[P[_, _]](pab: P[A, B])(implicit ev: Strong[P]) = self(pab)
+    override private[proptics] def apply[P[_, _]](pab: P[A, B])(implicit ev: Strong[P]): P[S, T] = self(pab)
   }
 }
 
