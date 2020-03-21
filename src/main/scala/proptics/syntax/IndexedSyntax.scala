c94aa0078b8ec20fd7cc3e5012a9b6d13374f5b0
Sagi Fogel <sagi@hunters.ai>
2020-03-22 01:04:16 +0200

    removed implicit parameter Profunctor[* => *]

--- src/main/scala/proptics/syntax/IndexedSyntax.scala
+++ src/main/scala/proptics/syntax/IndexedSyntax.scala
@@ -1,6 +1,7 @@
 package proptics.syntax
 
 import cats.arrow.{Profunctor, Strong}
+import cats.instances.function._
 import proptics.Setter
 import proptics.internal.Indexed
 import proptics.iso.Newtype.newtype
@@ -9,12 +10,12 @@ import proptics.newtype.Newtype.Aux
 
 object IndexedSyntax {
   implicit class IndexedOps[P[_, _], I, S, T, R](val indexed: Indexed[P, I, S, T => R]) extends AnyVal {
-    def reindexed[J](f: I => J)(implicit ev0: Strong[* => *], ev1: Profunctor[P], ev2: Newtype.Aux[(J, S), (I, S)]): Indexed[P, J, S, T => R] = {
-      val firstIndex = ev0.first[I, J, S](f)
+    def reindexed[J](f: I => J)(implicit ev0: Profunctor[P], ev1: Newtype.Aux[(J, S), (I, S)]): Indexed[P, J, S, T => R] = {
+      val firstIndex = Strong[* => *].first[I, J, S](f)
       val setter = Setter[P[(I, S), T => R], P[(J, S), T => R], (I, S), (J, S)](is2js => pistr => {
-        val aux: Aux[(I, S), (J, S)] = Newtype.newtype(ev2.unwrap)(is2js)
+        val aux: Aux[(I, S), (J, S)] = Newtype.newtype(ev1.unwrap)(is2js)
 
-        ev1.lmap(pistr)(js => newtype(ev2)(aux).set(js)(js))
+        ev0.lmap(pistr)(js => newtype(ev1)(aux).set(js)(js))
       })
 
       Indexed(setter.over(firstIndex)(indexed.runIndex))
