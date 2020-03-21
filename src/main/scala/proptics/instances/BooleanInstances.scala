7c6cf76ce476b8a71b338c8f29eb92a996c7d848
Sagi Fogel <sagi@hunters.ai>
2020-03-22 01:03:20 +0200

    addition of BooleanInstances

--- src/main/scala/proptics/instances/BooleanInstances.scala
+++ src/main/scala/proptics/instances/BooleanInstances.scala
@@ -1,8 +1,29 @@
 package proptics.instances
 
+import cats.{Monoid, Semigroup}
 import spire.algebra.lattice.Heyting
 
-object HeytingInstances {
+object BooleanInstances {
+  implicit final val semigroupBoolAll: Semigroup[Boolean] = new Semigroup[Boolean] {
+    override def combine(x: Boolean, y: Boolean): Boolean = heytingBool.and(x, y)
+  }
+
+  implicit final val semigroupBoolAny: Semigroup[Boolean] = new Semigroup[Boolean] {
+    override def combine(x: Boolean, y: Boolean): Boolean = heytingBool.or(x, y)
+  }
+
+  implicit final val monoidBoolAll: Monoid[Boolean] = new Monoid[Boolean] {
+    override def empty: Boolean = true
+
+    override def combine(x: Boolean, y: Boolean): Boolean = semigroupBoolAll.combine(x, y)
+  }
+
+  implicit final val monoidBoolAny: Monoid[Boolean] = new Monoid[Boolean] {
+    override def empty: Boolean = false
+
+    override def combine(x: Boolean, y: Boolean): Boolean = semigroupBoolAll.combine(x, y)
+  }
+
   implicit final def heytingBool: Heyting[Boolean] = new Heyting[Boolean] {
     override def and(a: Boolean, b: Boolean): Boolean = a && b
 
