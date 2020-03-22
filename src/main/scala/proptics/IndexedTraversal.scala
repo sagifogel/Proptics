33978b82d4d66a85abd20305e45227befe0b8e75
Sagi Fogel <sagi@hunters.ai>
2020-03-22 17:41:59 +0200

    addition of all traversal methods

--- src/main/scala/proptics/IndexedTraversal.scala
+++ src/main/scala/proptics/IndexedTraversal.scala
@@ -1,11 +1,25 @@
 package proptics
 
-import cats.{Applicative, Traverse}
+import cats.data.Const
+import cats.syntax.apply._
+import cats.syntax.eq._
+import cats.syntax.option._
+import cats.instances.list._
+import cats.instances.int._
+import cats.mtl.MonadState
+import cats.{Applicative, Eq, Id, Monoid, Order, Traverse}
 import proptics.IndexedLens.liftIndexedOptic
 import proptics.internal.{Indexed, Traversing, Wander}
+import proptics.newtype.{Additive, Conj, Disj, Dual, Endo, First, Last, Multiplicative, Newtype}
+import proptics.profunctor.Star
+import proptics.instances.BooleanInstances._
 import proptics.rank2types.{LensLikeIndexedTraversal, Rank2TypeIndexedTraversalLike, Rank2TypeTraversalLike}
+import proptics.syntax.FunctionSyntax._
+import spire.algebra.Semiring
+import spire.algebra.lattice.Heyting
 
-import scala.Function.untupled
+import scala.Function.{const, untupled}
+import scala.reflect.ClassTag
 
 /**
  * An [[IndexedTraversal]] is An IndexedOptic constrained with [[Wander]] [[cats.arrow.Profunctor]]
@@ -19,12 +33,93 @@ import scala.Function.untupled
 abstract class IndexedTraversal[I, S, T, A, B] { self =>
   def apply[P[_, _]](indexed: Indexed[P, I, A, B])(implicit ev: Wander[P]): P[S, T]
 
+  def view(s: S)(implicit ev: Monoid[(I, A)]): (I, A) = foldMap(s)(identity)
+
+  def viewAll(s: S)(implicit ev: Monoid[(I, A)]): List[(I, A)] = foldMap(s)(List(_))
+
+  def preview(s: S): Option[(I, A)] = foldMapNewtype[First[(I, A)], Option[(I, A)]](s)(_.some)
+
+  def set(b: B): S => T = over(const(b))
+
+  def over(f: ((I, A)) => B): S => T = self(Indexed(f))
+
+  def overF[F[_] : Applicative](f: ((I, A)) => F[B])(s: S): F[T] = traverse(s)(f)
+
+  def traverse[F[_] : Applicative](s: S)(f: ((I, A)) => F[B]): F[T] =
+    self[Star[F, *, *]](Indexed(Star[F, (I, A), B](f))).runStar(s)
+
+  def foldMap[R: Monoid](s: S)(f: ((I, A)) => R): R =
+    overF[Const[R, *]](Const[R, B] _ compose f)(s).getConst
+
+  def fold(s: S)(implicit ev: Monoid[(I, A)]): (I, A) = foldMap(s)(identity)
+
+  def sequence_[F[_]](s: S)(implicit ev: Applicative[F]): F[Unit] = traverse_(s)(ev.pure)
+
+  def traverse_[F[_], R](s: S)(f: ((I, A)) => F[R])(implicit ev: Applicative[F]): F[Unit] =
+    foldr[F[Unit]](s)(ev.pure(()))(ia => ev.void(f(ia)) *> _)
+
+  def foldr[R](s: S)(r: R)(f: ((I, A)) => R => R): R = foldMap(s)(Endo[* => *, R] _ compose f).runEndo(r)
+
+  def foldl[R](s: S)(r: R)(f: R => ((I, A)) => R): R =
+    foldMap(s)(Dual[Endo[* => *, R]] _ compose Endo[* => *, R] compose f.flip).runDual.runEndo(r)
+
+  def sum(s: S)(implicit ev: Semiring[(I, A)]): (I, A) = foldMapNewtype[Additive[(I, A)], (I, A)](s)(identity)
+
+  def product(s: S)(implicit ev: Semiring[(I, A)]): (I, A) = foldMapNewtype[Multiplicative[(I, A)], (I, A)](s)(identity)
+
+  def all(f: ((I, A)) => Boolean): S => Boolean = s => allOf(s)(f)
+
+  def allOf[R: Heyting](s: S)(f: ((I, A)) => R): R = foldMapNewtype[Conj[R], R](s)(f)
+
+  def and(s: S)(implicit ev: Heyting[(I, A)]): (I, A) = allOf(s)(identity)
+
+  def or(s: S)(implicit ev: Heyting[(I, A)]): (I, A) = anyOf[Id, (I, A)](s)(identity)
+
+  def exists(f: ((I, A)) => Boolean): S => Boolean = s => anyOf[Disj, Boolean](s)(f)
+
+  def anyOf[F[_], R: Heyting](s: S)(f: ((I, A)) => R): R = foldMapNewtype[Disj[R], R](s)(f)
+
+  def contains(s: S)(a: ((I, A)))(implicit ev: Eq[(I, A)]): Boolean = exists(_ === a)(s)
+
+  def notContains(s: S)(a: ((I, A)))(implicit ev: Eq[(I, A)]): Boolean = !contains(s)(a)
+
+  def length(s: S): Int = foldMap(s)(const(1))
+
+  def has[R](s: S)(implicit ev: Heyting[R]): R = hasOrHasnt(s)(ev.one)
+
+  def hasNot[R](s: S)(implicit ev: Heyting[R]): R = hasOrHasnt(s)(ev.zero)
+
+  def find(f: ((I, A)) => Boolean): S => Option[(I, A)] = s =>
+    foldr[Option[(I, A)]](s)(None)(ia => _.fold(if (f(ia)) ia.some else None)(Some[(I, A)]))
+
+  def first(s: S): Option[(I, A)] = preview(s)
+
+  def last(s: S): Option[(I, A)] = foldMapNewtype[Last[(I, A)], Option[(I, A)]](s)(_.some)
+
+  def minimum(s: S)(implicit ev: Order[(I, A)]): Option[(I, A)] = minMax(s)(ev.min)
+
+  def maximum(s: S)(implicit ev: Order[(I, A)]): Option[(I, A)] = minMax(s)(ev.max)
+
+  def toArray[AA >: (I, A)](s: S)(implicit ev0: ClassTag[AA], ev1: Monoid[(I, A)]): Array[AA] = toList(s).toArray
+
+  def toList(s: S)(implicit ev: Monoid[(I, A)]): List[(I, A)] = viewAll(s)
+
+  def use[M[_]](implicit ev0: MonadState[M, S], ev1: Monoid[(I, A)]): M[List[(I, A)]] = ev0.inspect(viewAll)
+
   def unIndex: Traversal[S, T, A, B] = Traversal(new Rank2TypeTraversalLike[S, T, A, B] {
     override def apply[P[_, _]](pab: P[A, B])(implicit ev: Wander[P]): P[S, T] =
       self(Indexed(ev.dimap[A, B, (I, A), B](pab)(_._2)(identity)))
   })
 
   def asTraversal: Traversal[S, T, A, B] = unIndex
+
+  private def hasOrHasnt[R: Heyting](s: S)(r: R): R = foldMap(s)(const(Disj(r))).runDisj
+
+  private def foldMapNewtype[F: Monoid, R](s: S)(f: ((I, A)) => R)(implicit ev: Newtype.Aux[F, R]): R =
+    ev.unwrap(foldMap(s)(ev.wrap _ compose f))
+
+  private def minMax(s: S)(f: ((I, A), (I, A)) => (I, A))(implicit ev: Order[(I, A)]): Option[(I, A)] =
+    foldr[Option[(I, A)]](s)(None)(a => op => f(a, op.getOrElse(a)).some)
 }
 
 object IndexedTraversal {
@@ -32,11 +127,11 @@ object IndexedTraversal {
     override def apply[P[_, _]](indexed: Indexed[P, I, A, B])(implicit ev: Wander[P]): P[S, T] = f(indexed)
   }
 
-  def apply[I, S, T, A, B](get: S => (I, A))(set: S => B => T): IndexedTraversal[I, S, T, A, B] = new IndexedTraversal[I, S, T, A, B] {
+  def apply[I, S, T, A, B](get: S => (I, A))(_set: S => B => T): IndexedTraversal[I, S, T, A, B] = new IndexedTraversal[I, S, T, A, B] {
     override def apply[P[_, _]](indexed: Indexed[P, I, A, B])(implicit ev: Wander[P]): P[S, T] = {
       val traversing = new Traversing[S, T, (I, A), B] {
         override def apply[F[_]](f: ((I, A)) => F[B])(implicit ev: Applicative[F]): S => F[T] =
-          s => ev.map(f(get(s)))(set(s))
+          s => ev.map(f(get(s)))(_set(s))
       }
 
       ev.wander(traversing)(indexed.runIndex)
