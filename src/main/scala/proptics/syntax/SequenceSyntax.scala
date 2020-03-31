8c8e77f9fee3c231ac2f131fbb62386a2687f68a
Sagi Fogel <sagi@hunters.ai>
2020-04-01 02:02:55 +0300

    addition of more sequence methods for some of the optics

--- src/main/scala/proptics/syntax/SequenceSyntax.scala
+++ src/main/scala/proptics/syntax/SequenceSyntax.scala
@@ -1,10 +1,23 @@
 package proptics.syntax
 
-import cats.Functor
-import proptics.Lens
+import cats.{Applicative, Functor}
+import proptics.{Iso, Lens, Prism, Traversal}
 
 object SequenceSyntax {
-  implicit class SequenceLensStarOps[F[_], S, T, A](val lens: Lens[S, T, F[A], A]) extends AnyVal {
+  implicit class TraversalSequenceOps[F[_], S, T, A](val traversal: Traversal[S, T, F[A], A]) extends AnyVal {
+    def sequence(s: S)(implicit ev: Applicative[F]): F[T] = traversal.traverse(s)(identity)
+  }
+
+  implicit class PrismSequenceOps[F[_], S, T, A](val prism: Prism[S, T, F[A], A]) extends AnyVal {
+    def sequence(s: S)(implicit ev: Applicative[F]): F[T] = prism.traverse(s)(identity)
+  }
+
+  implicit class LensSequenceOps[F[_], S, T, A](val lens: Lens[S, T, F[A], A]) extends AnyVal {
     def sequence(s: S)(implicit ev: Functor[F]): F[T] = lens.traverse(s)(identity)
   }
+
+  implicit class IsoSequenceOps[F[_], S, T, A](val iso: Iso[S, T, F[A], A]) extends AnyVal {
+    def sequence(s: S)(implicit ev: Applicative[F]): F[T] = iso.traverse(s)(identity)
+  }
+
 }
