1046c133fc88e9a6151487146685783671315415
Sagi Fogel <sagi@hunters.ai>
2020-04-06 02:25:46 +0300

    addition of extends AnyVal

--- core/shared/src/main/scala/proptics/syntax/TraversalSyntax.scala
+++ core/shared/src/main/scala/proptics/syntax/TraversalSyntax.scala
@@ -8,7 +8,7 @@ import proptics.internal.Wander
 import proptics.syntax.IndexedTraversalSyntax._
 
 object TraversalSyntax {
-  implicit class TraversalElementsOps[P[_, _]: Wander, I, S, T, A](val traversal: Traversal_[S, T, A, A]) {
+  implicit class TraversalElementsOps[P[_, _]: Wander, I, S, T, A](val traversal: Traversal_[S, T, A, A]) extends AnyVal {
     def element(i: Int)(implicit ev: State[Int, A]): Traversal_[S, T, A, A] =
       traversal.positions.elements(_ === 0).unIndex
   }
