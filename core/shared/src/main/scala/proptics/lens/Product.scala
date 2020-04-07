17c242813ad2edd0e6e3f41be03f0d5c5a262cf7
Sagi Fogel <sagi@hunters.ai>
2020-04-08 02:12:20 +0300

    fixing Product bugs

--- core/shared/src/main/scala/proptics/lens/Product.scala
+++ core/shared/src/main/scala/proptics/lens/Product.scala
@@ -5,12 +5,12 @@ import proptics.Lens_
 
 object Product {
   def _1[F[_], G[_], H[_], A]: Lens_[Tuple2K[F, G, A], Tuple2K[H, G, A], F[A], H[A]] =
-    Lens_[Tuple2K[F, G, A], Tuple2K[H, G, A], F[A], H[A]](_.first)(fgh => ha => Tuple2K[H, G, A](ha, fgh.second))
+    Lens_((get: Tuple2K[F, G, A]) => get.first)(fgh => ha => Tuple2K[H, G, A](ha, fgh.second))
 
   def fst[F[_], G[_], H[_], A]: Lens_[Tuple2K[F, G, A], Tuple2K[H, G, A], F[A], H[A]] = _1
 
   def _2[F[_], G[_], H[_], A]: Lens_[Tuple2K[F, G, A], Tuple2K[F, H, A], G[A], H[A]] =
-    Lens_[Tuple2K[F, G, A], Tuple2K[F, H, A], G[A], H[A]](_.second)(fgh => ha => Tuple2K(fgh.first, ha))
+    Lens_((get: Tuple2K[F, G, A]) => get.second)(fgh => ha => Tuple2K(fgh.first, ha))
 
-  def _2[F[_], G[_], H[_], A]: Lens_[Tuple2K[F, G, A], Tuple2K[F, H, A], G[A], H[A]] = _2
+  def snd[F[_], G[_], H[_], A]: Lens_[Tuple2K[F, G, A], Tuple2K[F, H, A], G[A], H[A]] = _2
 }
