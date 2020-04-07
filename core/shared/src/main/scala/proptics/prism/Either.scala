e010ab2caf41b05ee0518f79e1b2eac23e4e7545
Sagi Fogel <sagi@hunters.ai>
2020-04-08 02:13:46 +0300

    fixing Either bugs

--- core/shared/src/main/scala/proptics/prism/Either.scala
+++ core/shared/src/main/scala/proptics/prism/Either.scala
@@ -5,8 +5,8 @@ import proptics.Prism_
 
 object Either {
   def left[A, B, C]: Prism_[Either[A, C], Either[B, C], A, B] =
-    Prism_[Either[C, A], Either[B, C], A, B](_.asLeft[C])(_.bimap[Either[C, B], A](_.asLeft[B], identity))
+    Prism_((b: B) => b.asLeft[C])(_.fold(_.asRight[Either[B, C]], _.asRight[B].asLeft[A]))
 
   def right[A, B, C]: Prism_[Either[C, A], Either[C, B], A, B] =
-    Prism_[Either[C, A], Either[C, B], A, B](_.asRight[C])(_.bimap[Either[C, B], A](_.asLeft[B], identity))
+    Prism_((b: B) => b.asRight[C])(_.fold(_.asLeft[B].asLeft[A], _.asRight[Either[C, B]]))
 }
