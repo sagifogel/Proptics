fcb84c968e4eabdaafdf5f65537aac744ab6b8ad
Sagi Fogel <sagi@hunters.ai>
2020-04-08 02:13:05 +0300

    fixing Option bugs, addition of some, none

--- core/shared/src/main/scala/proptics/prism/Option.scala
+++ core/shared/src/main/scala/proptics/prism/Option.scala
@@ -2,14 +2,21 @@ package proptics.prism
 
 import cats.syntax.option._
 import cats.syntax.either._
-import proptics.Prism_
+import proptics.{Prism, Prism_}
+
 import scala.{Option => Maybe}
 import scala.Function.const
 
 object Option {
-  def none[A, B]: Prism_[Option[A], Option[B], Unit, Unit] =
-    Prism_[Option[A], Option[B], Unit, Unit](const(Maybe.empty[B]))(const(().asRight[Option[B]]))
+  def none_[A, B]: Prism_[Option[A], Option[B], Unit, Unit] =
+    Prism_((_: Unit) => Maybe.empty[B])(const(().asRight[Option[B]]))
 
-  def some[A, B]: Prism_[Option[A], Option[B], A, B] =
-    Prism_[Option[A], Option[B], A, B](_.some)(_.fold(Maybe.empty[B].asLeft[A])(_.asRight[Option[B]]))
+  def some_[A, B]: Prism_[Option[A], Option[B], A, B] =
+    Prism_((b: B) => b.some)(_.fold(Maybe.empty[B].asLeft[A])(_.asRight[Option[B]]))
+
+  def none[A, B]: Prism[Option[A], Unit] =
+    Prism[Option[A], Unit](const(Maybe.empty[A]))(_.asLeft[Unit])
+
+  def some[A, B]: Prism[Option[A], A] =
+    Prism[Option[A], A](_.some)(_.fold(Maybe.empty[A].asLeft[A])(_.asRight[Option[A]]))
 }
