72ee25e520c970cab99e1ef904fd5403a59fcee8
Sagi Fogel <sagi@hunters.ai>
2020-04-10 14:17:57 +0300

    addition of compose with Prism

--- core/shared/src/main/scala/proptics/Lens.scala
+++ core/shared/src/main/scala/proptics/Lens.scala
@@ -6,7 +6,7 @@ import cats.syntax.apply._
 import cats.syntax.eq._
 import cats.syntax.option._
 import cats.{Alternative, Comonad, Eq, Functor}
-import proptics.internal.{Forget, Zipping}
+import proptics.internal.{Forget, Wander, Zipping}
 import proptics.newtype.Disj
 import proptics.profunctor.{Costar, Star}
 import proptics.rank2types.Rank2TypeLensLike
@@ -14,15 +14,15 @@ import proptics.rank2types.Rank2TypeLensLike
 import scala.Function.const
 
 /**
- * Given a type whose "focus element" always exists,
- * a [[Lens_]] provides a convenient way to view, set, and transform
- * that element.
- *
- * @tparam S the source of a [[Lens_]]
- * @tparam T the modified source of a [[Lens_]]
- * @tparam A the target of a [[Lens_]]
- * @tparam B the modified target of a [[Lens_]]
- */
+  * Given a type whose "focus element" always exists,
+  * a [[Lens_]] provides a convenient way to view, set, and transform
+  * that element.
+  *
+  * @tparam S the source of a [[Lens_]]
+  * @tparam T the modified source of a [[Lens_]]
+  * @tparam A the target of a [[Lens_]]
+  * @tparam B the modified target of a [[Lens_]]
+  */
 abstract class Lens_[S, T, A, B] extends Serializable { self =>
   private[proptics] def apply[P[_, _]](pab: P[A, B])(implicit ev: Strong[P]): P[S, T]
 
@@ -50,7 +50,7 @@ abstract class Lens_[S, T, A, B] extends Serializable { self =>
     val star = Star[(Disj[Boolean], *), A, B](a => (Disj(true), f(a)))
 
     self(star).runStar(s) match {
-      case (Disj(true), x) => ev1.pure(x)
+      case (Disj(true), x)  => ev1.pure(x)
       case (Disj(false), _) => ev1.empty
     }
   }
@@ -60,11 +60,15 @@ abstract class Lens_[S, T, A, B] extends Serializable { self =>
   def zipWithF[F[_]: Comonad](fs: F[S])(f: F[A] => B): T = self(Costar(f)).runCostar(fs)
 
   def compose[C, D](other: Lens_[A, B, C, D]): Lens_[S, T, C, D] = new Lens_[S, T, C, D] {
-    override private[proptics] def apply[P[_, _]](pab: P[C, D])(implicit ev: Strong[P]) = self(other(pab))
+    override private[proptics] def apply[P[_, _]](pab: P[C, D])(implicit ev: Strong[P]): P[S, T] = self(other(pab))
   }
 
   def compose[C, D](other: Iso_[A, B, C, D]): Lens_[S, T, C, D] = new Lens_[S, T, C, D] {
-    override private[proptics] def apply[P[_, _]](pab: P[C, D])(implicit ev: Strong[P]) = self(other(pab))
+    override private[proptics] def apply[P[_, _]](pab: P[C, D])(implicit ev: Strong[P]): P[S, T] = self(other(pab))
+  }
+
+  def compose[C, D](other: Prism_[A, B, C, D]): Traversal_[S, T, C, D] = new Traversal_[S, T, C, D] {
+    override private[proptics] def apply[P[_, _]](pab: P[C, D])(implicit ev: Wander[P]) = self(other(pab))
   }
 }
 
@@ -74,14 +78,15 @@ object Lens_ {
   }
 
   /**
-   * Create a [[Lens_]] from a getter/setter pair.
-   */
+    * Create a [[Lens_]] from a getter/setter pair.
+    */
   def apply[S, T, A, B](get: S => A)(set: S => B => T): Lens_[S, T, A, B] = Lens_((get, set).mapN(Tuple2.apply))
 
-  def apply[S, T, A, B](to: S => (A, B => T)): Lens_[S, T, A, B] = Lens_(new Rank2TypeLensLike[S, T, A, B] {
-    override def apply[P[_, _]](pab: P[A, B])(implicit ev: Strong[P]): P[S, T] =
-      liftOptic(to)(ev)(pab)
-  })
+  def apply[S, T, A, B](to: S => (A, B => T)): Lens_[S, T, A, B] =
+    Lens_(new Rank2TypeLensLike[S, T, A, B] {
+      override def apply[P[_, _]](pab: P[A, B])(implicit ev: Strong[P]): P[S, T] =
+        liftOptic(to)(ev)(pab)
+    })
 
   private[proptics] def liftOptic[P[_, _], S, T, A, B](to: S => (A, B => T))(implicit ev: Strong[P]): P[A, B] => P[S, T] =
     pab => ev.dimap(ev.first[A, B, B => T](pab))(to) { case (b, f) => f(b) }
