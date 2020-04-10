e01f7f6625670cc271d73dfbd205df05ab2a0691
Sagi Fogel <sagi@hunters.ai>
2020-04-11 01:10:26 +0300

    reformat

--- core/shared/src/main/scala/proptics/internal/Grating.scala
+++ core/shared/src/main/scala/proptics/internal/Grating.scala
@@ -13,9 +13,7 @@ abstract class GratingInstances {
 
   implicit final def closedGrating[E, F]: Closed[Grating[E, F, *, *]] = new Closed[Grating[E, F, *, *]] {
     override def closed[A, B, C](pab: Grating[E, F, A, B]): Grating[E, F, C => A, C => B] =
-      Grating(c2a2e2f => c => {
-        pab.runGrating(a2e => c2a2e2f(c2a => a2e(c2a(c))))
-      })
+      Grating(c2a2e2f => c => pab.runGrating(a2e => c2a2e2f(c2a => a2e(c2a(c)))))
 
     override def dimap[A, B, C, D](fab: Grating[E, F, A, B])(f: C => A)(g: B => D): Grating[E, F, C, D] =
       profunctorGrating.dimap(fab)(f)(g)
