7d638e4d2324136e31fdf1a28dc7cb43190b7617
Sagi Fogel <sagi@hunters.ai>
2020-04-11 01:15:42 +0300

    addition of compose with ALens, AnIso, APrism, Traversal, Fold and more

--- core/shared/src/main/scala/proptics/Lens.scala
+++ core/shared/src/main/scala/proptics/Lens.scala
@@ -5,8 +5,8 @@ import cats.instances.function._
 import cats.syntax.apply._
 import cats.syntax.eq._
 import cats.syntax.option._
-import cats.{Alternative, Comonad, Eq, Functor}
-import proptics.internal.{Forget, Wander, Zipping}
+import cats.{Alternative, Applicative, Comonad, Eq, Functor, Monoid}
+import proptics.internal._
 import proptics.newtype.Disj
 import proptics.profunctor.{Costar, Star}
 import proptics.rank2types.Rank2TypeLensLike
@@ -63,12 +63,66 @@ abstract class Lens_[S, T, A, B] extends Serializable { self =>
     override private[proptics] def apply[P[_, _]](pab: P[C, D])(implicit ev: Strong[P]): P[S, T] = self(other(pab))
   }
 
+  def compose[C, D](other: ALens_[A, B, C, D]): ALens_[S, T, C, D] = new ALens_[S, T, C, D] {
+    override def apply(shop: Shop[C, D, C, D]): Shop[C, D, S, T] = self(other(shop))
+  }
+
   def compose[C, D](other: Iso_[A, B, C, D]): Lens_[S, T, C, D] = new Lens_[S, T, C, D] {
     override private[proptics] def apply[P[_, _]](pab: P[C, D])(implicit ev: Strong[P]): P[S, T] = self(other(pab))
   }
 
+  def compose[C, D](other: AnIso_[A, B, C, D]): Lens_[S, T, C, D] = new Lens_[S, T, C, D] {
+    override private[proptics] def apply[P[_, _]](pcd: P[C, D])(implicit ev: Strong[P]) = {
+      val exchange = other(Exchange(identity, identity))
+      val pab = ev.dimap[C, D, A, B](pcd)(exchange.get)(exchange.inverseGet)
+
+      self(pab)
+    }
+  }
+
   def compose[C, D](other: Prism_[A, B, C, D]): Traversal_[S, T, C, D] = new Traversal_[S, T, C, D] {
     override private[proptics] def apply[P[_, _]](pab: P[C, D])(implicit ev: Wander[P]) = self(other(pab))
+  }
+
+  def compose[C, D](other: APrism_[A, B, C, D]): Traversal_[S, T, C, D] = self compose other.asPrism_
+
+  def compose[C, D](other: Traversal_[A, B, C, D]): Traversal_[S, T, C, D] = new Traversal_[S, T, C, D] {
+    override def apply[P[_, _]](pab: P[C, D])(implicit ev: Wander[P]): P[S, T] = self(other(pab))
+  }
+
+  def compose[C, D](other: ATraversal_[A, B, C, D]): ATraversal_[S, T, C, D] =
+    ATraversal_(new RunBazaar[* => *, C, D, S, T] {
+      override def apply[F[_]](pafb: C => F[D])(s: S)(implicit ev: Applicative[F]): F[T] = {
+        val bazaar = other(new Bazaar[* => *, C, D, C, D] {
+          override def runBazaar: RunBazaar[* => *, C, D, C, D] = new RunBazaar[* => *, C, D, C, D] {
+            override def apply[G[_]](pafb: C => G[D])(s: C)(implicit ev: Applicative[G]): G[D] = pafb(s)
+          }
+        })
+
+        self(bazaar).runBazaar(pafb)(s)
+      }
+    })
+
+  def compose[C, D](other: Setter_[A, B, C, D]): Setter_[S, T, C, D] = new Setter_[S, T, C, D] {
+    override private[proptics] def apply(pab: C => D) = self(other(pab))
+  }
+
+  def compose[C, D](other: Getter_[A, B, C, D]): Getter_[S, T, C, D] = new Getter_[S, T, C, D] {
+    override def apply[R: Monoid](forget: Forget[R, C, D]): Forget[R, S, T] = self(other(forget))(Forget.wanderForget)
+  }
+
+  def compose[C, D](other: AGetter_[A, B, C, D]): AGetter_[S, T, C, D] = new AGetter_[S, T, C, D] {
+    override private[proptics] def apply(forget: Forget[C, C, D]) = self(other(forget))
+
+    override protected def foldMap[R](s: S)(f: C => R)(implicit ev: Monoid[R]): R = {
+      val forget = other(Forget(identity))
+
+      f(forget.runForget(self.view(s)))
+    }
+  }
+
+  def compose[C, D](other: Fold_[A, B, C, D]): Fold_[S, T, C, D] = new Fold_[S, T, C, D] {
+    override def apply[R: Monoid](forget: Forget[R, C, D]): Forget[R, S, T] = self(other(forget))(Forget.wanderForget)
   }
 }
 
