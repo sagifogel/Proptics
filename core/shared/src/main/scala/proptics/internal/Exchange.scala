bc3c348d5ca5951e8f50b9651ccc986ac803057c
Sagi Fogel <sagi@hunters.ai>
2020-04-13 00:55:34 +0300

    reformat

--- core/shared/src/main/scala/proptics/internal/Exchange.scala
+++ core/shared/src/main/scala/proptics/internal/Exchange.scala
@@ -8,9 +8,8 @@ final case class Exchange[A, B, S, T](get: S => A, inverseGet: B => T)
 
 abstract class ExchangeInstances {
   implicit final def functorExchange[C, D, S]: Functor[Exchange[C, D, S, *]] = new Functor[Exchange[C, D, S, *]] {
-    override def map[A, B](fa: Exchange[C, D, S, A])(f: A => B): Exchange[C, D, S, B] = {
+    override def map[A, B](fa: Exchange[C, D, S, A])(f: A => B): Exchange[C, D, S, B] =
       Exchange(fa.get, f compose fa.inverseGet)
-    }
   }
 
   implicit final def profunctorExchange[E, F]: Profunctor[Exchange[E, F, *, *]] = new Profunctor[Exchange[E, F, *, *]] {
