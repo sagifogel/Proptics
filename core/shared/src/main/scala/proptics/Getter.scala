a22ed7615c6334513a58cf37994084457d98ff9c
Sagi Fogel <sagi@hunters.ai>
2020-04-13 01:01:38 +0300

    removed superclass of Fold

--- core/shared/src/main/scala/proptics/Getter.scala
+++ core/shared/src/main/scala/proptics/Getter.scala
@@ -1,45 +1,58 @@
 package proptics
 
-import Fold_._
-import cats.{Foldable, Monoid}
+import cats.Eq
+import cats.Monoid
+import cats.syntax.eq._
+import cats.syntax.option._
 import proptics.internal.Forget
-import proptics.rank2types.Rank2TypeFoldLike
+import proptics.newtype.Disj
+import spire.algebra.lattice.Heyting
+
 
 /**
- * A [[Getter_]] is a [[Fold]]
- *
- * @tparam S the source of a [[Getter_]]
- * @tparam T the modified source of a [[Getter_]]
- * @tparam A the target of a [[Getter_]]
- * @tparam B the modified target of a [[Getter_]]
- */
-abstract class Getter_[S, T, A, B] extends Fold_[S, T, A, B]
+  * A [[Getter_]] is a [[Fold]] that ignores the supplied Monoid
+  *
+  * @tparam S the source of a [[Getter_]]
+  * @tparam T the modified source of a [[Getter_]]
+  * @tparam A the target of a [[Getter_]]
+  * @tparam B the modified target of a [[Getter_]]
+  */
+abstract class Getter_[S, T, A, B] extends Serializable { self =>
+  private[proptics] def apply(forget: Forget[A, A, B]): Forget[A, S, T]
+
+  def view(s: S): A = self(Forget(identity)).runForget(s)
+
+  def exists(f: A => Boolean): S => Boolean = f compose view
+
+  def contains(a: A)(s: S)(implicit ev: Eq[A]): Boolean = exists(_ === a)(s)
+
+  def notContains(a: A)(s: S)(implicit ev: Eq[A]): Boolean = !contains(a)(s)
+
+  def has(s: S)(implicit ev: Heyting[A]): A = hasOrHasnt(s)(ev.one)
+
+  def hasNot(s: S)(implicit ev: Heyting[A]): A = hasOrHasnt(s)(ev.zero)
+
+  def find(f: A => Boolean): S => Option[A] = s => view(s).some.find(f)
+
+  def asFold_ : Fold_[S, T, A, B] = new Fold_[S, T, A, B] {
+    override private[proptics] def apply[R: Monoid](forget: Forget[R, A, B]): Forget[R, S, T] =
+      Forget(s => forget.runForget(self(Forget(identity)).runForget(s)))
+  }
+
+  private[proptics] def hasOrHasnt(s: S)(r: A)(implicit ev: Heyting[A]): A =
+    Monoid[Disj[A]].combine(Disj(view(s)), Disj(ev.one)).runDisj
+}
 
 object Getter_ {
-  def apply[S, T, A, B](f: Rank2TypeFoldLike[S, T, A, B]): Getter_[S, T, A, B] = new Getter_[S, T, A, B] {
-    override def apply[R: Monoid](forget: Forget[R, A, B]): Forget[R, S, T] = f(forget)
+  private[Getter_] def apply[S, T, A, B](aGetter: Forget[A, A, B] => Forget[A, S, T]): Getter_[S, T, A, B] = new Getter_[S, T, A, B] {
+    override def apply(forget: Forget[A, A, B]): Forget[A, S, T] = aGetter(forget)
   }
 
-  def apply[S, T, A, B](get: S => A): Getter_[S, T, A, B] = Getter_(fromGetRank2TypeFoldLike[S, T, A, B](get))
+  def apply[S, T, A, B](f: S => A)(implicit ev: DummyImplicit): Getter_[S, T, A, B] =
+    Getter_((forget: Forget[A, A, B]) => Forget[A, S, T](forget.runForget compose f))
 
-  def fromFoldable[F[_], A, B, T](implicit ev0: Foldable[F]): Getter_[F[A], B, A, T] = new Getter_[F[A], B, A, T] {
-    override private[proptics] def apply[R: Monoid](forget: Forget[R, A, T]): Forget[R, F[A], B] =
-      Forget[R, F[A], B](ev0.foldMap(_)(forget.runForget))
-
-    override def foldMap[R](s: F[A])(f: A => R)(implicit ev: Monoid[R]): R = ev0.foldMap(s)(f)
-  }
-
-  def replicate[A, B, T](i: Int): Getter_[A, B, A, T] = Getter_(replicateRank2TypeFoldLike[A, B, T](i))
-
-  def unfolded[S, T, A, B](f: S => Option[(A, S)]): Getter_[S, T, A, B] = Getter_(unfoldRank2TypeFoldLike[S, T, A, B](f))
 }
 
 object Getter {
-  def apply[S, A](f: S => A): Getter[S, A] = Getter_[S, S, A, A](f)
-
-  def fromFoldable[F[_], A, T](implicit ev0: Foldable[F]): Getter_[F[A], A, A, T] = Getter_.fromFoldable
-
-  def replicate[A, B, T](i: Int): Getter_[A, B, A, T] = Getter_.replicate(i)
-
-  def unfolded[S, T, A, B](f: S => Option[(A, S)]): Getter_[S, T, A, B] = Getter_.unfolded(f)
+  def apply[S, A](f: S => A): Getter[S, A] = Getter_(f)
 }
