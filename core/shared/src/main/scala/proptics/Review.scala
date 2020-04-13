d9b1d2bb5ff7bc473749929cca37ed297575f47b
Sagi Fogel <sagi@hunters.ai>
2020-04-13 13:17:21 +0300

    addition of compose with Iso, Prism

--- core/shared/src/main/scala/proptics/Review.scala
+++ core/shared/src/main/scala/proptics/Review.scala
@@ -1,19 +1,40 @@
 package proptics
 
-import proptics.internal.Tagged
+import cats.syntax.either._
+import proptics.internal.{Exchange, Market, Tagged}
 
 /** A [[Review_]] is an Optic with fixed type [[Tagged]] [[cats.arrow.Profunctor]]
- *  A [[Review_]] describes how to construct a single value.
- *
- * @tparam S the source of a [[Review_]]
- * @tparam T the modified source of a [[Review_]]
- * @tparam A the target of a [[Review_]]
- * @tparam B the modified target of a [[Review_]]
- */
+  *  A [[Review_]] describes how to construct a single value.
+  *
+  * @tparam S the source of a [[Review_]]
+  * @tparam T the modified source of a [[Review_]]
+  * @tparam A the target of a [[Review_]]
+  * @tparam B the modified target of a [[Review_]]
+  */
 abstract class Review_[S, T, A, B] extends Serializable { self =>
   private[proptics] def apply(tagged: Tagged[A, B]): Tagged[S, T]
 
   def review(b: B): T = self(Tagged[A, B](b)).runTag
+
+  def compose[C, D](other: Review_[A, B, C, D]): Review_[S, T, C, D] = new Review_[S, T, C, D] {
+    override private[proptics] def apply(tagged: Tagged[C, D]) = self(other(tagged))
+  }
+
+  def compose[C, D](other: Iso_[A, B, C, D]): Review_[S, T, C, D] = new Review_[S, T, C, D] {
+    override private[proptics] def apply(tagged: Tagged[C, D]) = self(other(tagged))
+  }
+
+  def compose[C, D](other: AnIso_[A, B, C, D]): Review_[S, T, C, D] = new Review_[S, T, C, D] {
+    override private[proptics] def apply(tagged: Tagged[C, D]) = self(other(Exchange(identity, identity)))
+  }
+
+  def compose[C, D](other: Prism_[A, B, C, D]): Review_[S, T, C, D] = new Review_[S, T, C, D] {
+    override private[proptics] def apply(tagged: Tagged[C, D]) = self(other(tagged))
+  }
+
+  def compose[C, D](other: APrism_[A, B, C, D]): Review_[S, T, C, D] = new Review_[S, T, C, D] {
+    override private[proptics] def apply(tagged: Tagged[C, D]) = self(other(Market(identity, _.asRight[D])))
+  }
 }
 
 object Review_ {
