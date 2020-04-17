6aa67cc5c5617a7db2c14f918ff325fcd53c61f8
Sagi Fogel <sagi@hunters.ai>
2020-04-17 14:10:18 +0300

    addition of compose with indexed - Lens, Traversal, Setter, Getter, Fold

--- core/shared/src/main/scala/proptics/IndexedLens.scala
+++ core/shared/src/main/scala/proptics/IndexedLens.scala
@@ -5,8 +5,8 @@ import cats.instances.function._
 import cats.syntax.apply._
 import cats.syntax.eq._
 import cats.syntax.option._
-import cats.{Alternative, Applicative, Comonad, Eq}
-import proptics.internal.{Forget, Indexed, Zipping}
+import cats.{Alternative, Applicative, Comonad, Eq, Monoid}
+import proptics.internal.{Forget, Indexed, Shop, Traversing, Wander, Zipping}
 import proptics.newtype.Disj
 import proptics.profunctor.{Costar, Star}
 import proptics.rank2types.Rank2TypeIndexedLensLike
@@ -59,7 +59,45 @@ abstract class IndexedLens_[I, S, T, A, B] extends Serializable { self =>
 
   def asLens_ : Lens_[S, T, A, B] = new Lens_[S, T, A, B] {
     override private[proptics] def apply[P[_, _]](pab: P[A, B])(implicit ev: Strong[P]): P[S, T] =
-      self(Indexed(ev.dimap[A, B, (I, A), B](pab)(_._2)(identity)))
+      self(Indexed(ev.lmap[A, B, (I, A)](pab)(_._2)))
+  }
+
+  def compose[C, D](other: IndexedLens_[I, A, B, C, D]): IndexedLens_[I, S, T, C, D] = new IndexedLens_[I, S, T, C, D] {
+    override private[proptics] def apply[P[_, _]](indexed: Indexed[P, I, C, D])(implicit ev: Strong[P]): P[S, T] =
+      self(Indexed(ev.lmap[A, B, (I, A)](other(indexed))(_._2)))
+  }
+
+  def compose[C, D](other: AnIndexedLens_[I, A, B, C, D]): AnIndexedLens_[I, S, T, C, D] = new AnIndexedLens_[I, S, T, C, D] {
+    override def apply(indexed: Indexed[Shop[(I, C), D, *, *], I, C, D]): Shop[(I, C), D, S, T] =
+      Shop(s => other.view(self.view(s)._2), s => d => self.set(other.set(d)(self.view(s)._2))(s))
+  }
+
+  def compose[C, D](other: IndexedTraversal_[I, A, B, C, D]): IndexedTraversal_[I, S, T, C, D] = new IndexedTraversal_[I, S, T, C, D] {
+    override def apply[P[_, _]](indexed: Indexed[P, I, C, D])(implicit ev: Wander[P]): P[S, T] = {
+      val traversing: Traversing[S, T, (I, C), D] = new Traversing[S, T, (I, C), D] {
+        override def apply[F[_]](f: ((I, C)) => F[D])(s: S)(implicit ev: Applicative[F]): F[T] =
+          self.overF { case (_, a) => other.overF(f)(a) }(s)
+      }
+
+      ev.wander(traversing)(indexed.runIndex)
+    }
+  }
+
+  def compose[C, D](other: IndexedSetter_[I, A, B, C, D]): IndexedSetter_[I, S, T, C, D] = new IndexedSetter_[I, S, T, C, D] {
+    override private[proptics] def apply(indexed: Indexed[* => *, I, C, D]): S => T = {
+      val a2b = other(indexed)
+      self(Indexed[* => *, I, A, B] { case (_, a) => a2b(a) })
+    }
+  }
+
+  def compose[C, D](other: IndexedGetter_[I, A, B, C, D]): IndexedGetter_[I, S, T, C, D] = new IndexedGetter_[I, S, T, C, D] {
+    override private[proptics] def apply(indexed: Indexed[Forget[(I, C), *, *], I, C, D]): Forget[(I, C), S, T] =
+      Forget[(I, C), S, T](s => other.view(self.view(s)._2))
+  }
+
+  def compose[C, D](other: IndexedFold_[I, A, B, C, D]): IndexedFold_[I, S, T, C, D] = new IndexedFold_[I, S, T, C, D] {
+    override private[proptics] def apply[R: Monoid](indexed: Indexed[Forget[R, *, *], I, C, D]): Forget[R, S, T] =
+      Forget[R, S, T](s => other.foldMap(self.view(s)._2)(indexed.runIndex.runForget))
   }
 }
 
