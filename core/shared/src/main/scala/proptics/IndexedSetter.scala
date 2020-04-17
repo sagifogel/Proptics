a3a377388624016d1c10bf642a7d65fe6fe3d34d
Sagi Fogel <sagi@hunters.ai>
2020-04-17 14:08:55 +0300

    addition of compose with IndexedLens, IndexedTraversal

--- core/shared/src/main/scala/proptics/IndexedSetter.scala
+++ core/shared/src/main/scala/proptics/IndexedSetter.scala
@@ -1,24 +1,41 @@
 package proptics
 
+import cats.instances.function._
 import proptics.internal.Indexed
 
 import scala.Function.const
 
 /**
- * An [[IndexedSetter_]] is an indexed optic. A generalization of indexed fmap.
- *
- * @tparam S the source of an [[IndexedSetter_]]
- * @tparam I the index of an [[IndexedSetter_]]
- * @tparam T the modified source of an [[IndexedSetter_]]
- * @tparam A the target of an [[IndexedSetter_]]
- * @tparam B the modified target of an [[IndexedSetter_]]
- */
+  * An [[IndexedSetter_]] is an indexed optic. A generalization of indexed fmap.
+  *
+  * @tparam S the source of an [[IndexedSetter_]]
+  * @tparam I the index of an [[IndexedSetter_]]
+  * @tparam T the modified source of an [[IndexedSetter_]]
+  * @tparam A the target of an [[IndexedSetter_]]
+  * @tparam B the modified target of an [[IndexedSetter_]]
+  */
 abstract class IndexedSetter_[I, S, T, A, B] extends Serializable { self =>
   private[proptics] def apply(indexed: Indexed[* => *, I, A, B]): S => T
 
   def set(b: B): S => T = over(const(b))
 
   def over(f: ((I, A)) => B): S => T = self(Indexed(f))
+
+  def compose[C, D](other: IndexedLens_[I, A, B, C, D]): IndexedSetter_[I, S, T, C, D] = new IndexedSetter_[I, S, T, C, D] {
+    override private[proptics] def apply(indexed: Indexed[* => *, I, C, D]): S => T = {
+      val a2b = other(indexed)
+      self(Indexed { case (_, a) => a2b(a) })
+    }
+  }
+
+  def compose[C, D](other: AnIndexedLens_[I, A, B, C, D]): IndexedSetter_[I, S, T, C, D] = self compose other.asIndexedLens_
+
+  def compose[C, D](other: IndexedTraversal_[I, A, B, C, D]): IndexedSetter_[I, S, T, C, D] = new IndexedSetter_[I, S, T, C, D] {
+    override private[proptics] def apply(indexed: Indexed[* => *, I, C, D]): S => T = {
+      val a2b = other(indexed)
+      self(Indexed { case (_, a) => a2b(a) })
+    }
+  }
 }
 
 object IndexedSetter_ {
@@ -27,9 +44,9 @@ object IndexedSetter_ {
   }
 
   def apply[I, S, T, A, B](get: ((I, A) => B) => S => T)(implicit ev: DummyImplicit): IndexedSetter_[I, S, T, A, B] =
-    IndexedSetter_((indexed: Indexed[* => *, I, A, B]) => {
+    IndexedSetter_ { (indexed: Indexed[* => *, I, A, B]) =>
       get { case (i, a) => indexed.runIndex(i, a) }
-    })
+    }
 }
 
 object IndexedSetter {
