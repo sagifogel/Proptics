a8a3d8f12427f356e74d30f04a47688733422600
Sagi Fogel <sagi@hunters.ai>
2020-04-19 00:01:40 +0300

    addition of compose with Iso, Lens, Prism, Traversal, Setter, Getter ...

--- core/shared/src/main/scala/proptics/APrism.scala
+++ core/shared/src/main/scala/proptics/APrism.scala
@@ -1,24 +1,25 @@
 package proptics
 
-import cats.data.Const
+import cats.data.{Const, Kleisli}
+import cats.instances.either._
 import cats.syntax.either._
 import cats.syntax.eq._
 import cats.syntax.option._
 import cats.{Applicative, Eq, Id, Monoid}
 import proptics.instances.BooleanInstances._
-import proptics.internal.Market
+import proptics.internal._
 import proptics.newtype.{Disj, First, Newtype}
 
 import scala.Function.const
 
 /**
- * * A [[Prism_]] with fixed type [[Market]] [[cats.arrow.Profunctor]]
- *
- * @tparam S the source of an [[APrism_]]
- * @tparam T the modified source of an [[APrism_]]
- * @tparam A the target of an [[APrism_]]
- * @tparam B the modified target of an [[APrism_]]
- */
+  * * A [[Prism_]] with fixed type [[Market]] [[cats.arrow.Profunctor]]
+  *
+  * @tparam S the source of an [[APrism_]]
+  * @tparam T the modified source of an [[APrism_]]
+  * @tparam A the target of an [[APrism_]]
+  * @tparam B the modified target of an [[APrism_]]
+  */
 abstract class APrism_[S, T, A, B] { self =>
   private[proptics] def apply(market: Market[A, B, A, B]): Market[A, B, S, T]
 
@@ -62,7 +63,86 @@ abstract class APrism_[S, T, A, B] { self =>
 
   def matching(s: S): Either[T, A] = withPrism(const(_.apply(s)))
 
-  def asPrism_ : Prism_[S, T, A, B] = self.withPrism(Prism_[S, T, A, B])
+  def asPrism_ : Prism_[S, T, A, B] = withPrism(Prism_[S, T, A, B])
+
+  def compose[C, D](other: Iso_[A, B, C, D]): APrism_[S, T, C, D] = new APrism_[S, T, C, D] {
+    override private[proptics] def apply(market: Market[C, D, C, D]): Market[C, D, S, T] =
+      self(Market(identity, _.asRight[B])) compose other(market)
+
+    override def traverse[F[_]](s: S)(f: C => F[D])(implicit ev: Applicative[F]): F[T] =
+      self.traverse(s)(other.traverse(_)(f))
+  }
+
+  def compose[C, D](other: AnIso_[A, B, C, D]): APrism_[S, T, C, D] = self compose other.asIso_
+
+  def compose[C, D](other: Lens_[A, B, C, D]): Traversal_[S, T, C, D] = new Traversal_[S, T, C, D] {
+    override private[proptics] def apply[P[_, _]](pcd: P[C, D])(implicit ev: Wander[P]): P[S, T] = {
+      val traversing = new Traversing[S, T, C, D] {
+        override def apply[F[_]](f: C => F[D])(s: S)(implicit ev: Applicative[F]): F[T] =
+          self.traverse(s)(other.traverse(_)(f))
+      }
+
+      ev.wander(traversing)(pcd)
+    }
+  }
+
+  def compose[C, D](other: ALens_[A, B, C, D]): Traversal_[S, T, C, D] = new Traversal_[S, T, C, D] {
+    override private[proptics] def apply[P[_, _]](pcd: P[C, D])(implicit ev: Wander[P]): P[S, T] = {
+      val traversing = new Traversing[S, T, C, D] {
+        override def apply[F[_]](f: C => F[D])(s: S)(implicit ev: Applicative[F]): F[T] =
+          self.traverse(s)(other.traverse(_)(f))
+      }
+
+      ev.wander(traversing)(pcd)
+    }
+  }
+
+  def compose[C, D](other: Prism_[A, B, C, D]): APrism_[S, T, C, D] = new APrism_[S, T, C, D] {
+    override private[proptics] def apply(market: Market[C, D, C, D]): Market[C, D, S, T] =
+      self(Market(identity, _.asRight[B])) compose other(market)
+
+    override def traverse[F[_]](s: S)(f: C => F[D])(implicit ev: Applicative[F]): F[T] =
+      self.traverse(s)(other.traverse(_)(f))
+  }
+
+  def compose[C, D](other: APrism_[A, B, C, D]): APrism_[S, T, C, D] = self compose other.asPrism_
+
+  def compose[C, D](other: Traversal_[A, B, C, D]): Traversal_[S, T, C, D] = new Traversal_[S, T, C, D] {
+    override private[proptics] def apply[P[_, _]](pcd: P[C, D])(implicit ev: Wander[P]): P[S, T] = {
+      val traversing = new Traversing[S, T, C, D] {
+        override def apply[F[_]](f: C => F[D])(s: S)(implicit ev: Applicative[F]): F[T] =
+          self.traverse(s)(other.traverse(_)(f))
+      }
+
+      ev.wander(traversing)(pcd)
+    }
+  }
+
+  def compose[C, D](other: ATraversal_[A, B, C, D]): ATraversal_[S, T, C, D] =
+    ATraversal_(new RunBazaar[* => *, C, D, S, T] {
+      override def apply[F[_]](f: C => F[D])(s: S)(implicit ev: Applicative[F]): F[T] =
+        self.traverse(s)(other.traverse(_)(f))
+    })
+
+  def compose[C, D](other: Setter_[A, B, C, D]): Setter_[S, T, C, D] = new Setter_[S, T, C, D] {
+    override private[proptics] def apply(pab: C => D): S => T = s => {
+      val market = self(Market(identity[B], _.asRight[B]))
+
+      market.from(s).fold(identity, self.review compose other(pab))
+    }
+  }
+
+  def compose[C, D](other: Getter_[A, B, C, D]): Fold_[S, T, C, D] = new Fold_[S, T, C, D] {
+    override private[proptics] def apply[R: Monoid](forget: Forget[R, C, D]): Forget[R, S, T] =
+      Forget(self.foldMap(_)(forget.runForget compose other.view))
+  }
+
+  def compose[C, D](other: Fold_[A, B, C, D]): Fold_[S, T, C, D] = new Fold_[S, T, C, D] {
+    override private[proptics] def apply[R: Monoid](forget: Forget[R, C, D]): Forget[R, S, T] =
+      Forget(self.foldMap(_)(other.foldMap(_)(forget.runForget)))
+  }
+
+  def compose[C, D](other: Review[A, B, C, D]): Review_[S, T, C, D] = self.asPrism_ compose other
 
   private def foldMapNewtype[F: Monoid, R](f: A => R)(s: S)(implicit ev: Newtype.Aux[F, R]): R =
     ev.unwrap(foldMap(s)(ev.wrap _ compose f))
@@ -75,8 +155,8 @@ object APrism_ {
     override private[proptics] def apply(market: Market[A, B, A, B]): Market[A, B, S, T] = Market(to, from)
 
     override def traverse[F[_]](s: S)(f: A => F[B])(implicit ev: Applicative[F]): F[T] = from(s) match {
-        case Right(a) => ev.map(f(a))(to)
-        case Left(t) => ev.pure(t)
+      case Right(a) => ev.map(f(a))(to)
+      case Left(t)  => ev.pure(t)
     }
   }
 }
