c6cd8295b6966396e471973f4f8767173ca27f4d
Sagi Fogel <sagi@hunters.ai>
2020-04-20 00:05:43 +0300

    addition of compose method

--- core/shared/src/main/scala/proptics/internal/Exchange.scala
+++ core/shared/src/main/scala/proptics/internal/Exchange.scala
@@ -4,7 +4,10 @@ import cats.Functor
 import cats.arrow.Profunctor
 
 /** The [[Exchange]] profunctor characterizes an [[proptics.Iso]] */
-final case class Exchange[A, B, S, T](get: S => A, inverseGet: B => T)
+final case class Exchange[A, B, S, T](get: S => A, inverseGet: B => T) { self =>
+  def compose[C, D](other: Exchange[C, D, A, B]): Exchange[C, D, S, T] =
+    Exchange(other.get compose self.get, self.inverseGet compose other.inverseGet)
+}
 
 abstract class ExchangeInstances {
   implicit final def functorExchange[C, D, S]: Functor[Exchange[C, D, S, *]] = new Functor[Exchange[C, D, S, *]] {
