32984e198ac82b09b868cf9875c57e7935c693ce
Sagi Fogel <sagi@hunters.ai>
2020-04-20 00:06:27 +0300

    addition of compose with Iso

--- core/shared/src/main/scala/proptics/AnIso.scala
+++ core/shared/src/main/scala/proptics/AnIso.scala
@@ -9,13 +9,13 @@ import proptics.internal.Exchange
 import scala.Function.const
 
 /**
- * An Iso with fixed type [[Exchange]] [[cats.arrow.Profunctor]]
- *
- * @tparam S the source of a [[AnIso_]]
- * @tparam T the modified source of an [[AnIso_]]
- * @tparam A the target of an [[AnIso_]]
- * @tparam B the modified target of an [[AnIso_]]
- */
+  * An Iso with fixed type [[Exchange]] [[cats.arrow.Profunctor]]
+  *
+  * @tparam S the source of a [[AnIso_]]
+  * @tparam T the modified source of an [[AnIso_]]
+  * @tparam A the target of an [[AnIso_]]
+  * @tparam B the modified target of an [[AnIso_]]
+  */
 abstract class AnIso_[S, T, A, B] { self =>
   private[proptics] def apply(exchange: Exchange[A, B, A, B]): Exchange[A, B, S, T]
 
@@ -47,8 +47,7 @@ abstract class AnIso_[S, T, A, B] { self =>
     f(exchange.get)(exchange.inverseGet)
   }
 
-  def au[P[_, _], E](f: (B => T) => E => S): E => A =
-    withIso(sa => bt => e => sa(f(bt)(e)))
+  def au[P[_, _], E](f: (B => T) => E => S): E => A = withIso(sa => bt => e => sa(f(bt)(e)))
 
   def auf[P[_, _], E, R](f: P[R, A] => E => B)(g: P[R, S])(implicit ev: Profunctor[P]): E => T =
     withIso(sa => bt => e => bt(f(ev.rmap(g)(sa))(e)))
@@ -58,10 +57,23 @@ abstract class AnIso_[S, T, A, B] { self =>
   def mapping[P[_, _], F[_], G[_]](implicit ev0: Functor[F], ev1: Functor[G]): Iso_[F[S], G[T], F[A], G[B]] =
     withIso(sa => bt => Iso_(ev0.lift(sa))(ev1.lift(bt)))
 
-  def dimapping[P[_, _], Q[_, _], SS, TT, AA, BB](that: AnIso_[SS, TT, AA, BB])(implicit ev0: Profunctor[P], ev1: Profunctor[Q]): Iso_[P[A, SS], Q[B, TT], P[S, AA], Q[T, BB]] =
-    withIso[P, Iso_[P[A, SS], Q[B, TT], P[S, AA], Q[T, BB]]](sa => bt => that.withIso[Q, Iso_[P[A, SS], Q[B, TT], P[S, AA], Q[T, BB]]](ssaa => bbtt => {
-      Iso_.iso[P[A, SS], Q[B, TT], P[S, AA], Q[T, BB]](ev0.dimap(_)(sa)(ssaa))(ev1.dimap(_)(bt)(bbtt))
-    }))
+  def dimapping[P[_, _], Q[_, _], SS, TT, AA, BB](
+      that: AnIso_[SS, TT, AA, BB])(implicit ev0: Profunctor[P], ev1: Profunctor[Q]): Iso_[P[A, SS], Q[B, TT], P[S, AA], Q[T, BB]] =
+    withIso[P, Iso_[P[A, SS], Q[B, TT], P[S, AA], Q[T, BB]]] { sa => bt =>
+      that.withIso[Q, Iso_[P[A, SS], Q[B, TT], P[S, AA], Q[T, BB]]] { ssaa => bbtt =>
+        Iso_.iso[P[A, SS], Q[B, TT], P[S, AA], Q[T, BB]](ev0.dimap(_)(sa)(ssaa))(ev1.dimap(_)(bt)(bbtt))
+      }
+    }
+
+  def compose[C, D](other: Iso_[A, B, C, D]): AnIso_[S, T, C, D] = new AnIso_[S, T, C, D] {
+    override private[proptics] def apply(exchange: Exchange[C, D, C, D]) =
+      self(Exchange(identity, identity)) compose other(exchange)
+  }
+
+  def compose[C, D](other: AnIso_[A, B, C, D]): AnIso_[S, T, C, D] = new AnIso_[S, T, C, D] {
+    override private[proptics] def apply(exchange: Exchange[C, D, C, D]): Exchange[C, D, S, T] =
+      self(Exchange(identity, identity)) compose other(exchange)
+  }
 }
 
 object AnIso_ {
