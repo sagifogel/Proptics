279bbda636174057f344ab2e88f69889981eaf66
Sagi Fogel <sagi@hunters.ai>
2020-04-23 01:30:47 +0300

    addition of compose with Iso, Lens, Prism, Traversal

--- core/shared/src/main/scala/proptics/ALens.scala
+++ core/shared/src/main/scala/proptics/ALens.scala
@@ -4,19 +4,19 @@ import cats.instances.function._
 import cats.syntax.apply._
 import cats.syntax.eq._
 import cats.syntax.option._
-import cats.{Eq, Functor, Id}
-import proptics.internal.Shop
+import cats.{Applicative, Eq, Functor, Id}
+import proptics.internal.{Shop, Traversing, Wander}
 
 import scala.Function.const
 
 /**
- * A [[Lens_]] with fixed type [[Shop]] [[cats.arrow.Profunctor]]
- *
- * @tparam S the source of a [[ALens_]]
- * @tparam T the modified source of a [[ALens_]]
- * @tparam A the target of a [[ALens_]]
- * @tparam B the modified target of a [[ALens_]]
- */
+  * A [[Lens_]] with fixed type [[Shop]] [[cats.arrow.Profunctor]]
+  *
+  * @tparam S the source of a [[ALens_]]
+  * @tparam T the modified source of a [[ALens_]]
+  * @tparam A the target of a [[ALens_]]
+  * @tparam B the modified target of a [[ALens_]]
+  */
 abstract class ALens_[S, T, A, B] { self =>
   def apply(shop: Shop[A, B, A, B]): Shop[A, B, S, T]
 
@@ -29,7 +29,7 @@ abstract class ALens_[S, T, A, B] { self =>
   def overF[F[_]: Functor](f: A => F[B])(s: S): F[T] = traverse(s)(f)
 
   def traverse[F[_]: Functor](s: S)(f: A => F[B])(implicit ev: Functor[F]): F[T] = {
-    val shop = self(Shop[A, B, A, B](identity, const(identity)))
+    val shop = self(Shop(identity[A], const(identity[B])))
 
     ev.map(f(shop.get(s)))(shop.set(s))
   }
@@ -45,7 +45,7 @@ abstract class ALens_[S, T, A, B] { self =>
   def notContains(s: S)(a: A)(implicit ev: Eq[A]): Boolean = !contains(s)(a)
 
   def withLens[R](f: (S => A) => (S => B => T) => R): R = {
-    val shop = self(Shop(identity, const(identity)))
+    val shop = toShop
 
     f(shop.get)(shop.set)
   }
@@ -53,36 +53,65 @@ abstract class ALens_[S, T, A, B] { self =>
   def asLens_ : Lens_[S, T, A, B] = withLens(Lens_[S, T, A, B])
 
   /**
-   * Converts a [[Lens_]] into the form that [[Lens]] accepts.
-   *
-   * Can be useful when defining a lens where the focus appears under multiple
-   * constructors of an algebraic data type. This function would be called for
-   * each case of the data type.
-   */
-  def lensStore(s: S): (A, B => T) = {
-    withLens(sa => sbt => (sa, sbt).mapN(Tuple2.apply))(s)
+    * Converts a [[Lens_]] into the form that [[Lens]] accepts.
+    *
+    * Can be useful when defining a lens where the focus appears under multiple
+    * constructors of an algebraic data type. This function would be called for
+    * each case of the data type.
+    */
+  def lensStore(s: S): (A, B => T) = withLens(sa => sbt => (sa, sbt).mapN(Tuple2.apply))(s)
+
+  def compose[C, D](other: Iso_[A, B, C, D]): ALens_[S, T, C, D] = new ALens_[S, T, C, D] {
+    override def apply(shop: Shop[C, D, C, D]): Shop[C, D, S, T] =
+      self(Shop(identity, const(identity))) compose other(shop)
   }
+
+  def compose[C, D](other: AnIso_[A, B, C, D]): ALens_[S, T, C, D] = self compose other.asIso_
+
+  def compose[C, D](other: Lens_[A, B, C, D]): ALens_[S, T, C, D] = new ALens_[S, T, C, D] {
+    override def apply(shop: Shop[C, D, C, D]): Shop[C, D, S, T] =
+      self(Shop(identity, const(identity))) compose other(shop)
+  }
+
+  def compose[C, D](other: ALens_[A, B, C, D]): ALens_[S, T, C, D] = new ALens_[S, T, C, D] {
+    override def apply(shop: Shop[C, D, C, D]): Shop[C, D, S, T] =
+      self(Shop(identity, const(identity))) compose other(shop)
+  }
+
+  def compose[C, D](other: Prism_[A, B, C, D]): Traversal_[S, T, C, D] = new Traversal_[S, T, C, D] {
+    override def apply[P[_, _]](pab: P[C, D])(implicit ev: Wander[P]): P[S, T] = {
+      val traversing = new Traversing[S, T, C, D] {
+        override def apply[F[_]](f: C => F[D])(s: S)(implicit ev: Applicative[F]): F[T] =
+          self.traverse(s)(other.traverse(_)(f))
+      }
+
+      ev.wander(traversing)(pab)
+    }
+  }
+
+  def compose[C, D](other: APrism_[A, B, C, D]): Traversal_[S, T, C, D] = self compose other.asPrism_
+
+  private[this] def toShop: Shop[A, B, S, T] = self(Shop(identity, const(identity)))
 }
 
 object ALens_ {
   private[proptics] def apply[S, T, A, B](f: Shop[A, B, A, B] => Shop[A, B, S, T]): ALens_[S, T, A, B] = new ALens_[S, T, A, B] { self =>
-      override def apply(shop: Shop[A, B, A, B]): Shop[A, B, S, T] = f(shop)
+    override def apply(shop: Shop[A, B, A, B]): Shop[A, B, S, T] = f(shop)
   }
 
   def apply[S, T, A, B](get: S => A)(set: S => B => T): ALens_[S, T, A, B] =
-    ALens_(shop => {
-      Shop(shop.get compose get, s => b => {
-        val a = get(s)
-        val b2 = shop.set(a)(b)
+    ALens_ { shop =>
+      Shop(
+        shop.get compose get,
+        s =>
+          b => {
+            val a = get(s)
 
-        set(s)(b2)
-      })
-    })
+            set(s)(shop.set(a)(b))
+          })
+    }
 }
 
 object ALens {
   def apply[S, A](get: S => A)(set: S => A => S): ALens[S, A] = ALens_(get)(set)
 }
-
-
-
