ddeb24b13f7b18590c832e185b1b332023ff9b08
Sagi Fogel <sagi@hunters.ai>
2020-04-24 23:57:42 +0300

    addition of compose with Indexed Lens, Traversal, Setter, Getter, Fold

--- core/shared/src/main/scala/proptics/AnIndexedLens.scala
+++ core/shared/src/main/scala/proptics/AnIndexedLens.scala
@@ -2,8 +2,9 @@ package proptics
 
 import cats.syntax.eq._
 import cats.syntax.option._
-import cats.{Applicative, Eq, Id}
-import proptics.internal.{Indexed, Shop}
+import cats.{Applicative, Eq, Id, Monoid}
+import proptics.internal.{Forget, Indexed, Shop, Traversing, Wander}
+import proptics.syntax.Tuple2Syntax._
 
 import scala.Function.const
 
@@ -49,6 +50,39 @@ abstract class AnIndexedLens_[I, S, T, A, B] { self =>
   def asIndexedLens_ : IndexedLens_[I, S, T, A, B] = withIndexedLens(IndexedLens_[I, S, T, A, B])
 
   def asLens_ : Lens_[S, T, A, B] = withIndexedLens(sia => sbt => Lens_(s => (sia(s)._2, sbt(s))))
+
+  def compose[C, D](other: IndexedLens_[I, A, B, C, D]): AnIndexedLens_[I, S, T, C, D] = new AnIndexedLens_[I, S, T, C, D] {
+    def apply(indexed: Indexed[Shop[(I, C), D, *, *], I, C, D]): Shop[(I, C), D, S, T] =
+      Shop(other.view _ compose Tuple2._2[I, A] compose self.view, s => d => self.set(other.set(d)(self.view(s)._2))(s))
+  }
+
+  def compose[C, D](other: AnIndexedLens_[I, A, B, C, D]): AnIndexedLens_[I, S, T, C, D] = new AnIndexedLens_[I, S, T, C, D] {
+    def apply(indexed: Indexed[Shop[(I, C), D, *, *], I, C, D]): Shop[(I, C), D, S, T] =
+      Shop(other.view _ compose Tuple2._2[I, A] compose self.view, s => d => self.set(other.set(d)(self.view(s)._2))(s))
+  }
+
+  def compose[C, D](other: IndexedTraversal_[I, A, B, C, D]): IndexedTraversal_[I, S, T, C, D] = new IndexedTraversal_[I, S, T, C, D] {
+    override def apply[P[_, _]](indexed: Indexed[P, I, C, D])(implicit ev: Wander[P]): P[S, T] = {
+      val traversing: Traversing[S, T, (I, C), D] = new Traversing[S, T, (I, C), D] {
+        override def apply[F[_]](f: ((I, C)) => F[D])(s: S)(implicit ev: Applicative[F]): F[T] =
+          self.overF { case (_, a) => other.overF(f)(a) }(s)
+      }
+
+      ev.wander(traversing)(indexed.runIndex)
+    }
+  }
+
+  def compose[C, D](other: IndexedSetter_[I, A, B, C, D]): IndexedSetter_[I, S, T, C, D] = new IndexedSetter_[I, S, T, C, D] {
+    override private[proptics] def apply(indexed: Indexed[* => *, I, C, D]): S => T =
+      self.traverse[Id](_)(other(indexed) compose Tuple2._2)
+  }
+
+  def compose[C, D](other: IndexedGetter_[I, A, B, C, D]): IndexedFold_[I, S, T, C, D] = self compose other.asIndexedFold_
+
+  def compose[C, D](other: IndexedFold_[I, A, B, C, D]): IndexedFold_[I, S, T, C, D] = new IndexedFold_[I, S, T, C, D] {
+    override private[proptics] def apply[R: Monoid](indexed: Indexed[Forget[R, *, *], I, C, D]): Forget[R, S, T] =
+      Forget(s => other(indexed).runForget(self.view(s)._2))
+  }
 
   private def applyShop: Shop[(I, A), B, S, T] = self(Indexed(Shop(identity, const(identity))))
 }
