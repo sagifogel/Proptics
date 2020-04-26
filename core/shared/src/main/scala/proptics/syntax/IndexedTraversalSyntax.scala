4bf7702aca425a5f1ec75eabc27aba56f78264c3
Sagi Fogel <sagi@hunters.ai>
2020-04-26 20:28:06 +0300

    moving sequence to IndexedTraversal

--- core/shared/src/main/scala/proptics/syntax/IndexedTraversalSyntax.scala
+++ core/shared/src/main/scala/proptics/syntax/IndexedTraversalSyntax.scala
@@ -1,17 +1,31 @@
 package proptics.syntax
 
 import cats.Applicative
-import proptics.IndexedTraversal_
+import proptics.{IndexedTraversal, IndexedTraversal_, Traversal}
 import proptics.IndexedTraversal_.wander
 import proptics.internal.{Indexed, Wander}
 import proptics.profunctor.Star
 import proptics.rank2types.Rank2TypeLensLikeWithIndex
 
 trait IndexedTraversalSyntax {
-  implicit def indexedTraversalOps[I, S, T, A](indexedTraversal: IndexedTraversal_[I, S, T, A, A]) = IndexedTraversalOps(indexedTraversal)
+  implicit def indexedTraversalElementsOps[I, S, T, A](indexedTraversal: IndexedTraversal_[I, S, T, A, A]) =
+    IndexedTraversalElementsOps(indexedTraversal)
+
+  implicit def indexedTraversalSequenceOps[F[_], I, S, T, A](indexedTraversal: IndexedTraversal_[I, S, T, F[A], A]) =
+    IndexedTraversalSequenceOps(indexedTraversal)
+
+  implicit def isoAsLensOps[I, S, A](indexedTraversal: IndexedTraversal[I, S, A]) = IndexedTraversalAsOps(indexedTraversal)
 }
 
-final case class IndexedTraversalOps[I, S, T, A](private val indexedTraversal: IndexedTraversal_[I, S, T, A, A]) extends AnyVal {
+final case class IndexedTraversalAsOps[I, S, A](private val indexedTraversal: IndexedTraversal[I, S, A]) extends AnyVal {
+  def asTraversal: Traversal[S, A] = indexedTraversal.asTraversal_
+}
+
+final case class IndexedTraversalSequenceOps[F[_], I, S, T, A](private val iso: IndexedTraversal_[I, S, T, F[A], A]) extends AnyVal {
+  def sequence(s: S)(implicit ev: Applicative[F]): F[T] = iso.traverse(s)(_._2)
+}
+
+final case class IndexedTraversalElementsOps[I, S, T, A](private val indexedTraversal: IndexedTraversal_[I, S, T, A, A]) extends AnyVal {
   def elements[P[_, _]](pr: I => Boolean)(implicit ev0: Wander[P]): IndexedTraversal_[I, S, T, A, A] =
     wander(new Rank2TypeLensLikeWithIndex[I, S, T, A, A] {
       override def apply[F[_]](f: ((I, A)) => F[A])(implicit ev: Applicative[F]): S => F[T] = {
