a53e339f22230c99a710e9f3608c30aa8ac1c41b
Sagi Fogel <sagi@hunters.ai>
2020-04-26 20:26:56 +0300

    moving asIso to AnIsoSyntax

--- core/shared/src/main/scala/proptics/syntax/AnIsoSyntax.scala
+++ core/shared/src/main/scala/proptics/syntax/AnIsoSyntax.scala
@@ -1,12 +1,18 @@
 package proptics.syntax
 
 import cats.Applicative
-import proptics.AnIso_
+import proptics.{AnIso, AnIso_, Iso}
 
 trait AnIsoSyntax {
+  implicit def asIsoOps[S, A](anIso: AnIso[S, A]) = AsIsoOps(anIso)
+
   implicit def anIsoSequenceOps[F[_], S, T, A](iso: AnIso_[S, T, F[A], A]) = AnIsoSequenceOps(iso)
 }
 
 final case class AnIsoSequenceOps[F[_], S, T, A](private val iso: AnIso_[S, T, F[A], A]) extends AnyVal {
   def sequence(s: S)(implicit ev0: Applicative[F]): F[T] = iso.traverse(s)(identity)
+}
+
+final case class AsIsoOps[S, A](private val anIso: AnIso[S, A]) extends AnyVal {
+  def asIso: Iso[S, A] = anIso.asIso_
 }
--- core/shared/src/main/scala/proptics/syntax/AsIsoSyntax.scala
+++
@@ -1,9 +0,0 @@
-package proptics.syntax
-
-import proptics.{AnIso, Iso}
-
-object AsIsoSyntax {
-  implicit class AsIsoOps[S, A](val anIso: AnIso[S, A]) extends AnyVal {
-    def asIso: Iso[S, A] = anIso.asIso_
-  }
-}
