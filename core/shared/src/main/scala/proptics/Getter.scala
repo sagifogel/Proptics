ae05659410c2b1a0119a37a99db3bb0985b4a521
Sagi Fogel <sagi@hunters.ai>
2020-05-02 23:54:44 +0300

    removed has, hasNot, added documentation

--- core/shared/src/main/scala/proptics/Getter.scala
+++ core/shared/src/main/scala/proptics/Getter.scala
@@ -5,31 +5,32 @@ import cats.{Eq, Monoid}
 import cats.syntax.eq._
 import cats.syntax.option._
 import proptics.internal.Forget
-import proptics.newtype.Disj
-import spire.algebra.lattice.Heyting
 
 /**
   * A [[Getter_]] is a [[Fold]] that ignores the supplied Monoid
   *
   * @tparam S the source of a [[Getter_]]
   * @tparam T the modified source of a [[Getter_]]
-  * @tparam A the target of a [[Getter_]]
-  * @tparam B the modified target of a [[Getter_]]
+  * @tparam A the focus of a [[Getter_]]
+  * @tparam B the modified focus of a [[Getter_]]
   */
 abstract class Getter_[S, T, A, B] extends Serializable { self =>
   private[proptics] def apply(forget: Forget[A, A, B]): Forget[A, S, T]
 
+  /** view the focus of a [[Getter_]] */
   def view(s: S): A = self(Forget(identity)).runForget(s)
 
+  /** tests whether a predicate holds for the focus of a [[Getter_]] */
   def exists(f: A => Boolean): S => Boolean = f compose view
 
+  /** tests whether a predicate does not hold for the focus of a [[Getter_]] */
+  def noExists(f: A => Boolean): S => Boolean = s => !exists(f)(s)
+
+  /** tests whether a [[Getter_]] contains a specific focus */
   def contains(a: A)(s: S)(implicit ev: Eq[A]): Boolean = exists(_ === a)(s)
 
+  /** tests whether a [[Getter_]] does not contain a specific focus */
   def notContains(a: A)(s: S)(implicit ev: Eq[A]): Boolean = !contains(a)(s)
-
-  def has(s: S)(implicit ev: Heyting[A]): A = hasOrHasnt(s)(ev.one)
-
-  def hasNot(s: S)(implicit ev: Heyting[A]): A = hasOrHasnt(s)(ev.zero)
 
   def find(f: A => Boolean): S => Option[A] = s => view(s).some.find(f)
 
@@ -77,9 +78,6 @@ abstract class Getter_[S, T, A, B] extends Serializable { self =>
     override private[proptics] def apply[R: Monoid](forget: Forget[R, C, D]): Forget[R, S, T] =
       Forget(s => other.foldMap(self.view(s))(forget.runForget))
   }
-
-  private[proptics] def hasOrHasnt(s: S)(r: A)(implicit ev: Heyting[A]): A =
-    Monoid[Disj[A]].combine(Disj(view(s)), Disj(ev.one)).runDisj
 }
 
 object Getter_ {
@@ -88,7 +86,7 @@ object Getter_ {
   }
 
   def apply[S, T, A, B](f: S => A)(implicit ev: DummyImplicit): Getter_[S, T, A, B] =
-    Getter_((forget: Forget[A, A, B]) => Forget[A, S, T](forget.runForget compose f))
+    Getter_ { forget: Forget[A, A, B] => Forget[A, S, T](forget.runForget compose f) }
 
 }
 
