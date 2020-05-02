ac1e55f028a48184b94faebd93c584aa8de1ef8d
Sagi Fogel <sagi@hunters.ai>
2020-05-03 01:03:44 +0300

    addition of documentation

--- core/shared/src/main/scala/proptics/Getter.scala
+++ core/shared/src/main/scala/proptics/Getter.scala
@@ -8,7 +8,9 @@ import proptics.internal.Forget
 
 /**
   * A [[Getter_]] is a [[Fold]] that ignores the supplied Monoid
-  *
+ * <p>
+  * [[Getter_]] is just any get function (S -> A)
+ * </p>
   * @tparam S the source of a [[Getter_]]
   * @tparam T the modified source of a [[Getter_]]
   * @tparam A the focus of a [[Getter_]]
@@ -32,48 +34,61 @@ abstract class Getter_[S, T, A, B] extends Serializable { self =>
   /** tests whether a [[Getter_]] does not contain a specific focus */
   def notContains(a: A)(s: S)(implicit ev: Eq[A]): Boolean = !contains(a)(s)
 
+  /** finds if the focus of a [[Getter_]] is satisfying a predicate. */
   def find(f: A => Boolean): S => Option[A] = s => view(s).some.find(f)
 
+  /** view the focus of a [[Getter_]] in the state of a monad */
   def use[M[_]](implicit ev: MonadState[M, S]): M[A] = ev.inspect(view)
 
+  /** transform a [[Getter_]] to a [[Fold_]] */
   def asFold : Fold_[S, T, A, B] = new Fold_[S, T, A, B] {
     override private[proptics] def apply[R: Monoid](forget: Forget[R, A, B]): Forget[R, S, T] =
       Forget(forget.runForget compose self.view)
   }
 
+  /** compose [[Getter_]] with an [[Iso_]] */
   def compose[C, D](other: Iso_[A, B, C, D]): Getter_[S, T, C, D] = new Getter_[S, T, C, D] {
     override private[proptics] def apply(forget: Forget[C, C, D]): Forget[C, S, T] =
       Forget(forget.runForget compose other.view compose self.view)
   }
 
+  /** compose [[Getter_]] with an [[AnIso_]] */
   def compose[C, D](other: AnIso_[A, B, C, D]): Getter_[S, T, C, D] = self compose other.asIso
 
+  /** compose [[Getter_]] with a [[Lens_]] */
   def compose[C, D](other: Lens_[A, B, C, D]): Getter_[S, T, C, D] = new Getter_[S, T, C, D] {
     override private[proptics] def apply(forget: Forget[C, C, D]): Forget[C, S, T] =
       Forget(forget.runForget compose other.view compose self.view)
   }
 
+  /** compose [[Getter_]] with an [[ALens_]] */
   def compose[C, D](other: ALens_[A, B, C, D]): Getter_[S, T, C, D] = self compose other.asLens
 
+  /** compose [[Getter_]] with a [[Prism_]] */
   def compose[C, D](other: Prism_[A, B, C, D]): Fold_[S, T, C, D] = new Fold_[S, T, C, D] {
     override private[proptics] def apply[R: Monoid](forget: Forget[R, C, D]): Forget[R, S, T] =
       Forget(s => other.preview(self.view(s)).fold(Monoid[R].empty)(forget.runForget))
   }
 
+  /** compose [[Getter_]] with an [[APrism_]] */
   def compose[C, D](other: APrism_[A, B, C, D]): Fold_[S, T, C, D] = self compose other.asPrism
 
+  /** compose [[Getter_]] with a [[Traversal_]] */
   def compose[C, D](other: Traversal_[A, B, C, D]): Fold_[S, T, C, D] = new Fold_[S, T, C, D] {
     override private[proptics] def apply[R: Monoid](forget: Forget[R, C, D]): Forget[R, S, T] =
       Forget(s => other.foldMap(self.view(s))(forget.runForget))
   }
 
+  /** compose [[Getter_]] with an [[ATraversal_]] */
   def compose[C, D](other: ATraversal_[A, B, C, D]): Fold_[S, T, C, D] = self compose other.asTraversal
 
+  /** compose [[Getter_]] with a [[Getter_]] */
   def compose[C, D](other: Getter_[A, B, C, D]): Getter_[S, T, C, D] = new Getter_[S, T, C, D] {
     override private[proptics] def apply(forget: Forget[C, C, D]): Forget[C, S, T] =
       Forget(forget.runForget compose other.view compose self.view)
   }
 
+  /** compose [[Getter_]] with a [[Fold_]] */
   def compose[C, D](other: Fold_[A, B, C, D]): Fold_[S, T, C, D] = new Fold_[S, T, C, D] {
     override private[proptics] def apply[R: Monoid](forget: Forget[R, C, D]): Forget[R, S, T] =
       Forget(s => other.foldMap(self.view(s))(forget.runForget))
@@ -81,15 +96,17 @@ abstract class Getter_[S, T, A, B] extends Serializable { self =>
 }
 
 object Getter_ {
+  /** create a polymorphic getter from a [[Getter_.apply]] function */
   private[Getter_] def apply[S, T, A, B](f: Forget[A, A, B] => Forget[A, S, T]): Getter_[S, T, A, B] = new Getter_[S, T, A, B] {
     override def apply(forget: Forget[A, A, B]): Forget[A, S, T] = f(forget)
   }
 
+  /** create a polymorphic getter from a getter function */
   def apply[S, T, A, B](f: S => A)(implicit ev: DummyImplicit): Getter_[S, T, A, B] =
     Getter_ { forget: Forget[A, A, B] => Forget[A, S, T](forget.runForget compose f) }
-
 }
 
 object Getter {
+  /** create a monomorphic getter from a getter function */
   def apply[S, A](f: S => A): Getter[S, A] = Getter_(f)
 }
