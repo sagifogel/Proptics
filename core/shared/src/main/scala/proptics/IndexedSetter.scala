94c6e2c52411d1174c3c633cd23be356ba3f4bc1
Sagi Fogel <sagi@hunters.ai>
2020-05-12 23:38:10 +0300

    addition of documentation

--- core/shared/src/main/scala/proptics/IndexedSetter.scala
+++ core/shared/src/main/scala/proptics/IndexedSetter.scala
@@ -12,33 +12,40 @@ import scala.Function.{const, untupled}
   * @tparam S the source of an [[IndexedSetter_]]
   * @tparam I the index of an [[IndexedSetter_]]
   * @tparam T the modified source of an [[IndexedSetter_]]
-  * @tparam A the target of an [[IndexedSetter_]]
-  * @tparam B the modified target of an [[IndexedSetter_]]
+  * @tparam A the focus an [[IndexedSetter_]]
+  * @tparam B the modified focus of an [[IndexedSetter_]]
   */
 abstract class IndexedSetter_[I, S, T, A, B] extends Serializable { self =>
   private[proptics] def apply(indexed: Indexed[* => *, I, A, B]): S => T
 
+  /** set the modified focus of an [[IndexedSetter_]] */
   def set(b: B): S => T = over(const(b))
 
+  /** modify the focus type of an [[IndexedSetter_]] using a function, resulting in a change of type to the full structure  */
   def over(f: ((I, A)) => B): S => T = self(Indexed(f))
 
+  /** transforms an [[IndexedSetter_]] to a [[Setter_]] */
   def asSetter: Setter_[S, T, A, B] = new Setter_[S, T, A, B] {
     override private[proptics] def apply(pab: A => B) =
       self(Indexed(pab compose Tuple2._2[I, A]))
   }
 
+  /** compose [[IndexedSetter_]] with an [[IndexedLens_]] */
   def compose[C, D](other: IndexedLens_[I, A, B, C, D]): IndexedSetter_[I, S, T, C, D] = new IndexedSetter_[I, S, T, C, D] {
     override private[proptics] def apply(indexed: Indexed[* => *, I, C, D]): S => T =
       self(Indexed(other(indexed) compose Tuple2._2[I, A]))
   }
 
+  /** compose [[IndexedSetter_]] with an [[AnIndexedLens_]] */
   def compose[C, D](other: AnIndexedLens_[I, A, B, C, D]): IndexedSetter_[I, S, T, C, D] = self compose other.asIndexedLens
 
+  /** compose [[IndexedSetter_]] with an [[IndexedTraversal_]] */
   def compose[C, D](other: IndexedTraversal_[I, A, B, C, D]): IndexedSetter_[I, S, T, C, D] = new IndexedSetter_[I, S, T, C, D] {
     override private[proptics] def apply(indexed: Indexed[* => *, I, C, D]): S => T =
       self(Indexed(other(indexed) compose Tuple2._2[I, A]))
   }
 
+  /** compose [[IndexedSetter_]] with an [[IndexedSetter_]] */
   def compose[C, D](other: IndexedSetter_[I, A, B, C, D]): IndexedSetter_[I, S, T, C, D] = new IndexedSetter_[I, S, T, C, D] {
     override private[proptics] def apply(indexed: Indexed[* => *, I, C, D]) =
       self(Indexed(other(indexed) compose Tuple2._2[I, A]))
@@ -46,15 +53,18 @@ abstract class IndexedSetter_[I, S, T, A, B] extends Serializable { self =>
 }
 
 object IndexedSetter_ {
-  private[proptics] def apply[I, S, T, A, B](f: Indexed[* => *, I, A, B] => S => T): IndexedSetter_[I, S, T, A, B] = new IndexedSetter_[I, S, T, A, B] {
-    override def apply(indexed: Indexed[* => *, I, A, B]): S => T = f(indexed)
+  /** create a polymorphic [[IndexedSetter_]] from an Indexed over function */
+  private[proptics] def apply[I, S, T, A, B](_over: Indexed[* => *, I, A, B] => S => T): IndexedSetter_[I, S, T, A, B] = new IndexedSetter_[I, S, T, A, B] {
+    override def apply(indexed: Indexed[* => *, I, A, B]): S => T = _over(indexed)
   }
 
+  /** create a polymorphic [[IndexedSetter_]] from an [[IndexedSetter_.over]] function */
   def apply[I, S, T, A, B](get: ((I, A) => B) => S => T)(implicit ev: DummyImplicit): IndexedSetter_[I, S, T, A, B] =
     IndexedSetter_ { indexed: Indexed[* => *, I, A, B] => get(untupled(indexed.runIndex)) }
 }
 
 object IndexedSetter {
+  /** create a monomorphic [[IndexedSetter]] from an [[IndexedSetter_.over]] function */
   def apply[I, S, A](get: ((I, A) => A) => S => S): IndexedSetter[I, S, A] =
     IndexedSetter_ { indexed: Indexed[* => *, I, A, A] => get(untupled(indexed.runIndex)) }
 }
