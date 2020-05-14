eb7c26cddfa5265075f12359f3e7a25d00d60e70
Sagi Fogel <sagi@hunters.ai>
2020-05-15 01:11:09 +0300

    using implicit instead of context bound

--- core/shared/src/main/scala/proptics/rank2types/Rank2TypeIndexedFoldLike.scala
+++ core/shared/src/main/scala/proptics/rank2types/Rank2TypeIndexedFoldLike.scala
@@ -4,5 +4,5 @@ import cats.Monoid
 import proptics.internal.{Forget, Indexed}
 
 private[proptics] trait Rank2TypeIndexedFoldLike[I, S, T, A, B] {
-  def apply[R: Monoid](indexed: Indexed[Forget[R, *, *], I, A, B]): Forget[R, S, T]
+  def apply[R](indexed: Indexed[Forget[R, *, *], I, A, B])(implicit ev: Monoid[R]): Forget[R, S, T]
 }
