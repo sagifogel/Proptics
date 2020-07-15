b8320196d2f1c48ef01b5009e2986a0230796018
sagifogel <sagi.fogel@gmail.com>
2020-07-15 18:39:36 +0300

    addition of tuple instances of an ALens

--- core/shared/src/main/scala/proptics/instances/TuplesInstances.scala
+++ core/shared/src/main/scala/proptics/instances/TuplesInstances.scala
@@ -1,8 +1,8 @@
 package proptics.instances
 
 import cats.arrow.Strong
-import proptics.Lens_
 import proptics.rank2types.Rank2TypeLensLike
+import proptics.{ALens_, Lens_}
 
 trait TuplesInstances {
   final def _1[A, B, C]: Lens_[(A, C), (B, C), A, B] =
@@ -12,10 +12,20 @@ trait TuplesInstances {
 
   final def fst[A, B, C]: Lens_[(A, C), (B, C), A, B] = _1
 
+  final def fstA[A, B, C]: ALens_[(A, C), (B, C), A, B] = _1A
+
+  final def _1A[A, B, C]: ALens_[(A, C), (B, C), A, B] =
+    ALens_.lens[(A, C), (B, C), A, B](_._1) { case (_, c) => b => (b, c) }
+
   final def _2[A, B, C]: Lens_[(C, A), (C, B), A, B] =
     Lens_(new Rank2TypeLensLike[(C, A), (C, B), A, B] {
       override def apply[P[_, _]](pab: P[A, B])(implicit ev: Strong[P]): P[(C, A), (C, B)] = ev.second(pab)
     })
 
+  final def _2A[A, B, C]: ALens_[(C, A), (C, B), A, B] =
+    ALens_.lens[(C, A), (C, B), A, B](_._2) { case (c, _) => b => (c, b) }
+
   final def snd[A, B, C]: Lens_[(C, A), (C, B), A, B] = _2
+
+  final def sndA[A, B, C]: ALens_[(C, A), (C, B), A, B] = _2A
 }
