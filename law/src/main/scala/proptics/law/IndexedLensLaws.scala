16b2fb5172b85cb2823fb56d898d2ca74e0193c1
sagifogel <sagi.fogel@gmail.com>
2020-07-25 14:52:38 +0300

    addition of property based/unit tests for IndexedLens

---
+++ law/src/main/scala/proptics/law/IndexedLensLaws.scala
@@ -0,0 +1,20 @@
+package proptics.law
+
+import cats.laws._
+import proptics.IndexedLens
+
+final case class IndexedLensLaws[I, S, A](indexedLens: IndexedLens[I, S, A]) extends AnyVal {
+  private def setWhatYouGet(s: S): S = indexedLens.set(indexedLens.view(s)._2)(s)
+  private def getWhatYouSet(s: S)(a: A): A = indexedLens.view(indexedLens.set(a)(s))._2
+
+  def setGet(s: S): IsEq[S] = setWhatYouGet(s) <-> s
+  def getSet(s: S, a: A): IsEq[A] = getWhatYouSet(s)(a) <-> a
+  def setSet(s: S, a: A): IsEq[S] = indexedLens.set(a)(indexedLens.set(a)(s)) <-> indexedLens.set(a)(s)
+  def overIdentity(s: S): IsEq[S] = indexedLens.over(_._2)(s) <-> s
+  def composeOver(s: S)(f: (I, A) => A)(g: (I, A) => A): IsEq[S] =
+    indexedLens.over(g.tupled)(indexedLens.over(f.tupled)(s)) <->
+      indexedLens.over { case (i, a) => g(i, f(i, a)) }(s)
+
+  def composeSourceLens(s: S): IsEq[S] = (setWhatYouGet _ compose setWhatYouGet)(s) <-> s
+  def composeFocusLens(s: S, a: A): IsEq[A] = (getWhatYouSet(s) _ compose getWhatYouSet(s))(a) <-> a
+}
---
+++ test/shared/src/test/scala/proptics/specs/IndexedLensSpec.scala
@@ -0,0 +1,107 @@
+package proptics.specs
+
+import cats.Id
+import cats.syntax.option._
+import cats.instances.option._
+import cats.instances.int._
+import cats.data.{NonEmptyList, State}
+import cats.kernel.Eq
+import org.scalacheck.{Arbitrary, Gen}
+import proptics.IndexedLens
+import proptics.law.{IndexedLensRules, LensRules}
+
+class IndexedLensSpec extends PropticsSuite {
+  def oneToNine: ((Int, Int)) => Int = _._2 + 8
+  val nel: NonEmptyList[Int] = NonEmptyList.fromListUnsafe(List(1, 2, 3))
+  implicit val eqPairOfIns: Eq[(Int, Int)] = Eq.fromUniversalEquals[(Int, Int)]
+  implicit val state: State[NonEmptyList[Int], Int] = State.pure[NonEmptyList[Int], Int](1)
+  implicit val arbNel: Arbitrary[NonEmptyList[Int]] = Arbitrary[NonEmptyList[Int]] {
+    for {
+      first <- Arbitrary.arbInt.arbitrary
+      rest <- Gen.nonEmptyListOf(Arbitrary.arbInt.arbitrary)
+    } yield NonEmptyList(first, rest)
+  }
+
+  val wholeIndexedLens: IndexedLens[Int, Whole, Int] =
+    IndexedLens[Int, Whole, Int](w => (w.part, w.part))(w => p => w.copy(part = p))
+
+  val indexedLens: IndexedLens[Int, NonEmptyList[Int], Int] =
+    IndexedLens[Int, NonEmptyList[Int], Int](ls => (0, ls.head))(nel => i => nel.copy(head = i))
+
+  checkAll("IndexedLens apply", IndexedLensRules(indexedLens))
+  checkAll("IndexedLens unindex", LensRules(wholeIndexedLens.unindex))
+
+  test("view") {
+    indexedLens.view(nel) shouldEqual ((0, 1))
+  }
+
+  test("set") {
+    indexedLens.set(9)(nel) shouldEqual nel.copy(head = 9)
+  }
+
+  test("over") {
+    indexedLens.over(oneToNine)(nel) shouldEqual nel.copy(head = 9)
+  }
+
+  test("traverse") {
+    val result = indexedLens.traverse[Id](nel)(oneToNine)
+
+    result shouldEqual nel.copy(head = 9)
+    indexedLens.overF[Id](oneToNine)(nel) shouldEqual result
+  }
+
+  test("exists") {
+    indexedLens.exists(_ === ((0, 1)))(nel)
+  }
+
+  test("notExists") {
+    indexedLens.notExists(_ === ((0, 1)))(nel) shouldEqual false
+    indexedLens.notExists(_ === ((1, 1)))(nel) shouldEqual true
+    indexedLens.notExists(_ === ((0, 2)))(nel) shouldEqual true
+    indexedLens.notExists(_ === ((0, 1)))(nel) shouldEqual !indexedLens.exists(_ == ((0, 1)))(nel)
+  }
+
+  test("contains") {
+    indexedLens.contains(nel)((0, 1)) shouldEqual true
+    indexedLens.contains(nel)((1, 1)) shouldEqual false
+  }
+
+  test("notContains") {
+    indexedLens.notContains(nel)((0, 1)) shouldEqual false
+    indexedLens.notContains(nel)((1, 1)) shouldEqual true
+    indexedLens.notContains(nel)((1, 1)) shouldEqual !indexedLens.contains(nel)((1, 1))
+  }
+
+  test("find") {
+    indexedLens.find { case (i, a) => i === 0 && a === 1 }(nel) shouldEqual 1.some
+    indexedLens.find(_ === ((0, 1)))(nel) shouldEqual 1.some
+    indexedLens.find(_._2 === 0)(nel) shouldEqual None
+    indexedLens.find(_ === ((1, 1)))(nel) shouldEqual None
+  }
+
+  test("use") {
+    indexedLens.use.runA(nel).value shouldEqual ((0, 1))
+  }
+
+  test("failover") {
+    val res = indexedLens.failover[Option](nel)(_._2)(strongStarTupleOfDisj, catsStdInstancesForOption)
+    val negativeRes = indexedLens.failover[Option](nel)(_._2)(strongStarTupleOfNegativeDisj, catsStdInstancesForOption)
+
+    res shouldEqual Some(nel)
+    negativeRes shouldEqual None
+  }
+
+  test("zipWith") {
+    val secondNel = NonEmptyList.fromListUnsafe(List(8, 9, 10))
+    val result = NonEmptyList.fromListUnsafe(List(9, 2, 3))
+
+    indexedLens.zipWith(nel, secondNel) { case ((_, a1), (_, a2)) => a1 + a2 } shouldEqual result
+  }
+
+  test("cotraverse") {
+    val cotraversedNel = indexedLens.cotraverse[Id](nel)(_._2)
+
+    cotraversedNel shouldEqual nel
+    indexedLens.zipWithF[Id](_._2)(nel) shouldEqual cotraversedNel
+  }
+}
