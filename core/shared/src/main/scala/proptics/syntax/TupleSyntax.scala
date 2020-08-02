fff58a08e154b37278b7f8bd33ab2564365aa70c
sagifogel <sagi.fogel@gmail.com>
2020-08-02 23:40:57 +0300

    addition of _1

--- core/shared/src/main/scala/proptics/syntax/TupleSyntax.scala
+++ core/shared/src/main/scala/proptics/syntax/TupleSyntax.scala
@@ -5,5 +5,7 @@ trait TupleSyntax {
 }
 
 final case class Tuple2TypeOps(private val tupleType: Tuple2.type) extends AnyVal {
+  def _1[A, B](pair: (A, B)): A = pair._1
+
   def _2[A, B](pair: (A, B)): B = pair._2
 }
