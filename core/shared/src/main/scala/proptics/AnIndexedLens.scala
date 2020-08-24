42733221e898f73b5546e6ca94a5b0f83e78d48e
sagifogel <sagifogel@hotmail.com>
2020-08-24 20:39:11 +0300

    changed circular reference to self set method

--- core/shared/src/main/scala/proptics/AnIndexedLens.scala
+++ core/shared/src/main/scala/proptics/AnIndexedLens.scala
@@ -1,11 +1,11 @@
 package proptics
 
 import cats.arrow.Strong
-import cats.mtl.MonadState
-import cats.syntax.eq._
+import cats.data.State
 import cats.instances.function._
-import cats.syntax.option._
 import cats.syntax.apply._
+import cats.syntax.eq._
+import cats.syntax.option._
 import cats.{Applicative, Eq, Id, Monoid}
 import proptics.IndexedLens_.liftIndexedOptic
 import proptics.internal._
@@ -39,8 +39,10 @@ abstract class AnIndexedLens_[I, S, T, A, B] { self =>
   def overF[F[_]: Applicative](f: ((I, A)) => F[B])(s: S): F[T] = traverse(s)(f)
 
   /** modify the focus type of an [[AnIndexedLens_]] using a [[cats.Functor]], resulting in a change of type to the full structure  */
-  def traverse[F[_]](s: S)(f: ((I, A)) => F[B])(implicit ev: Applicative[F]): F[T] =
-    ev.map(f(applyShop.get(s)))(set(_)(s))
+  def traverse[F[_]](s: S)(f: ((I, A)) => F[B])(implicit ev: Applicative[F]): F[T] = {
+    val shop = applyShop
+    ev.map(f(shop.get(s)))(shop.set(s)(_))
+  }
 
   /** test whether a predicate holds for the focus of an [[AnIndexedLens_]] */
   def exists(f: ((I, A)) => Boolean): S => Boolean = f compose view
