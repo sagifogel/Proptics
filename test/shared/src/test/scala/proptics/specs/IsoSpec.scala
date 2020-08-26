8052de33c7740219cab1300bde2767c7e61d9c19
sagifogel <sagifogel@hotmail.com>
2020-08-26 10:51:07 +0300

    addition of property based/unit test of compose for Lens

--- test/shared/src/test/scala/proptics/specs/IsoSpec.scala
+++ test/shared/src/test/scala/proptics/specs/IsoSpec.scala
@@ -11,7 +11,7 @@ import proptics.specs.Compose._
 import proptics.{Iso, Iso_}
 
 class IsoSpec extends PropticsSuite {
-  val iso: Iso[Whole, Int] = Iso.iso[Whole, Int](_.part)(Whole.apply)
+  val wholeIso: Iso[Whole, Int] = Iso.iso[Whole, Int](_.part)(Whole.apply)
   def ruleSetApply(iso: Iso[Whole, Int]): Laws#RuleSet = IsoRules(iso)
   val identityIso: Iso[Int, Int] = Iso[Int, Int](identity[Int] _)(identity)
   val ruleSetIdentityIso: Laws#RuleSet = IsoRules(identityIso)
@@ -20,72 +20,72 @@ class IsoSpec extends PropticsSuite {
   val curried: Iso_[(Whole, Whole) => Int, (Whole, Whole) => Int, Whole => Whole => Int, Whole => Whole => Int] = Iso_.curried
   val uncurried: Iso_[Whole => Whole => Int, Whole => Whole => Int, (Whole, Whole) => Int, (Whole, Whole) => Int] = Iso_.uncurried
 
-  checkAll("Iso apply", ruleSetApply(iso))
+  checkAll("Iso apply", ruleSetApply(wholeIso))
   checkAll("Iso identity", ruleSetIdentityIso)
   checkAll("Iso id", IsoRules(Iso.id[Int]))
-  checkAll("Iso reverse twice", ruleSetApply(iso.reverse.reverse))
+  checkAll("Iso reverse twice", ruleSetApply(wholeIso.reverse.reverse))
 
   test("view") {
-    iso.view(whole9) shouldEqual 9
+    wholeIso.view(whole9) shouldEqual 9
   }
 
   test("review") {
-    iso.review(9) shouldEqual whole9
+    wholeIso.review(9) shouldEqual whole9
   }
 
   test("set") {
-    iso.set(9)(Whole(1)) shouldEqual whole9
+    wholeIso.set(9)(Whole(1)) shouldEqual whole9
   }
 
   test("over") {
-    iso.over(_ + 1)(Whole(8)) shouldEqual whole9
+    wholeIso.over(_ + 1)(Whole(8)) shouldEqual whole9
   }
 
   test("traverse") {
-    iso.traverse(whole9)(_.some) shouldEqual whole9.some
-    iso.traverse(whole9)(_.some) shouldEqual iso.overF(_.some)(whole9)
+    wholeIso.traverse(whole9)(_.some) shouldEqual whole9.some
+    wholeIso.traverse(whole9)(_.some) shouldEqual wholeIso.overF(_.some)(whole9)
   }
 
   test("exists") {
-    iso.exists(greaterThan5)(whole9) shouldEqual true
-    iso.exists(greaterThan10)(whole9) shouldEqual false
+    wholeIso.exists(greaterThan5)(whole9) shouldEqual true
+    wholeIso.exists(greaterThan10)(whole9) shouldEqual false
   }
 
   test("notExists") {
-    iso.notExists(greaterThan10)(whole9) shouldEqual true
-    iso.notExists(greaterThan5)(whole9) shouldEqual false
-    iso.notExists(greaterThan5)(whole9) shouldEqual (!iso.exists(greaterThan5)(whole9))
+    wholeIso.notExists(greaterThan10)(whole9) shouldEqual true
+    wholeIso.notExists(greaterThan5)(whole9) shouldEqual false
+    wholeIso.notExists(greaterThan5)(whole9) shouldEqual (!wholeIso.exists(greaterThan5)(whole9))
   }
 
   test("contains") {
-    iso.contains(whole9)(9) shouldEqual true
-    iso.contains(whole9)(5) shouldEqual false
+    wholeIso.contains(whole9)(9) shouldEqual true
+    wholeIso.contains(whole9)(5) shouldEqual false
   }
 
   test("notContains") {
-    iso.notContains(whole9)(5) shouldEqual true
-    iso.notContains(whole9)(9) shouldEqual false
-    iso.notContains(whole9)(9) shouldEqual (!iso.contains(whole9)(9))
+    wholeIso.notContains(whole9)(5) shouldEqual true
+    wholeIso.notContains(whole9)(9) shouldEqual false
+    wholeIso.notContains(whole9)(9) shouldEqual (!wholeIso.contains(whole9)(9))
   }
 
   test("find") {
-    iso.find(greaterThan5)(whole9) shouldEqual Some(9)
-    iso.find(greaterThan10)(whole9) shouldEqual None
+    wholeIso.find(greaterThan5)(whole9) shouldEqual Some(9)
+    wholeIso.find(greaterThan10)(whole9) shouldEqual None
   }
 
   test("use") {
-    iso.use.runA(whole9).value shouldEqual 9
+    wholeIso.use.runA(whole9).value shouldEqual 9
   }
 
   test("zipWith") {
-    iso.zipWith(Whole(8), Whole(1))(_ + _) shouldEqual whole9
+    wholeIso.zipWith(Whole(8), Whole(1))(_ + _) shouldEqual whole9
   }
 
   test("cotraverse") {
-    val cotraversedWhole = iso.cotraverse[Id](whole9)(identity)
+    val cotraversedWhole = wholeIso.cotraverse[Id](whole9)(identity)
 
     cotraversedWhole shouldEqual whole9
-    iso.zipWithF[Id](identity)(whole9) shouldEqual cotraversedWhole
+    wholeIso.zipWithF[Id](identity)(whole9) shouldEqual cotraversedWhole
   }
 
   test("curried") {
@@ -102,26 +102,26 @@ class IsoSpec extends PropticsSuite {
     flipped.view(w => w.part + _)(9)(whole9) shouldEqual 18
   }
 
-  checkAll("compose with Iso", IsoRules(identityIso compose identityIso))
-  checkAll("compose with Lens", LensRules(identityIso compose lens))
-  checkAll("compose with ALens", ALensRules(identityIso compose aLens))
-  checkAll("compose with Prism", PrismRules(identityIso compose prism))
-  checkAll("compose with AffineTraversal", AffineTraversalRules(identityIso compose affineTraversal))
-  checkAll("compose with AnAffineTraversal", AnAffineTraversalRules(identityIso compose anAffineTraversal))
-  checkAll("compose with Traversal", TraversalRules(identityIso compose traversal))
-  checkAll("compose with ATraversal", ATraversalRules(identityIso compose aTraversal))
-  checkAll("compose with Setter", SetterRules(identityIso compose setter))
-  checkAll("compose with Grate", GrateRules(identityIso compose grate))
+  checkAll("compose with Iso", IsoRules(iso compose iso))
+  checkAll("compose with Lens", LensRules(iso compose lens))
+  checkAll("compose with ALens", ALensRules(iso compose aLens))
+  checkAll("compose with Prism", PrismRules(iso compose prism))
+  checkAll("compose with AffineTraversal", AffineTraversalRules(iso compose affineTraversal))
+  checkAll("compose with AnAffineTraversal", AnAffineTraversalRules(iso compose anAffineTraversal))
+  checkAll("compose with Traversal", TraversalRules(iso compose traversal))
+  checkAll("compose with ATraversal", ATraversalRules(iso compose aTraversal))
+  checkAll("compose with Setter", SetterRules(iso compose setter))
+  checkAll("compose with Grate", GrateRules(iso compose grate))
 
   test("compose with Getter") {
-    (identityIso compose getter).view(9) shouldEqual 9
+    (iso compose getter).view(9) shouldEqual 9
   }
 
   test("compose with Fold") {
-    (identityIso compose fold).fold(9) shouldEqual 9
+    (iso compose fold).fold(9) shouldEqual 9
   }
 
   test("compose with review") {
-    (identityIso compose review).review(9) shouldEqual 9
+    (iso compose review).review(9) shouldEqual 9
   }
 }
--- test/shared/src/test/scala/proptics/specs/LensSpec.scala
+++ test/shared/src/test/scala/proptics/specs/LensSpec.scala
@@ -1,87 +1,106 @@
 package proptics.specs
 
 import cats.Id
-import cats.syntax.option._
-import cats.instances.option._
 import cats.instances.int._
+import cats.instances.option._
+import cats.syntax.option._
 import org.scalacheck.Arbitrary._
 import org.typelevel.discipline.Laws
 import proptics.Lens
-import proptics.law.LensRules
+import proptics.law.{ALensRules, ATraversalRules, AffineTraversalRules, AnAffineTraversalRules, LensRules, SetterRules, TraversalRules}
+import proptics.specs.Compose._
 
 import scala.Function.const
 
 class LensSpec extends PropticsSuite {
-  val lens: Lens[Whole, Int] = Lens[Whole, Int](_.part)(w => i => w.copy(part = i))
+  val wholeLens: Lens[Whole, Int] = Lens[Whole, Int](_.part)(w => i => w.copy(part = i))
   val ruleSetIdentityLens: Laws#RuleSet = LensRules(Lens[Int, Int](identity)(const(identity)))
   def ruleSetApply(lens: Lens[Whole, Int]): Laws#RuleSet = LensRules(lens)
 
-  checkAll("Lens apply", ruleSetApply(lens))
+  checkAll("Lens apply", ruleSetApply(wholeLens))
   checkAll("Lens identity", ruleSetIdentityLens)
 
   test("view") {
-    lens.view(whole9) shouldEqual 9
+    wholeLens.view(whole9) shouldEqual 9
   }
 
   test("set") {
-    lens.set(9)(Whole(1)) shouldEqual whole9
+    wholeLens.set(9)(Whole(1)) shouldEqual whole9
   }
 
   test("over") {
-    lens.over(_ + 1)(Whole(8)) shouldEqual whole9
+    wholeLens.over(_ + 1)(Whole(8)) shouldEqual whole9
   }
   test("traverse") {
-    lens.traverse(whole9)(_.some) shouldEqual Some(whole9)
-    lens.traverse(whole9)(_.some) shouldEqual lens.overF(_.some)(whole9)
+    wholeLens.traverse(whole9)(_.some) shouldEqual Some(whole9)
+    wholeLens.traverse(whole9)(_.some) shouldEqual wholeLens.overF(_.some)(whole9)
   }
 
   test("find") {
-    lens.find(greaterThan5)(whole9) shouldEqual Some(9)
-    lens.find(greaterThan10)(whole9) shouldEqual None
+    wholeLens.find(greaterThan5)(whole9) shouldEqual Some(9)
+    wholeLens.find(greaterThan10)(whole9) shouldEqual None
   }
 
   test("exists") {
-    lens.exists(greaterThan5)(whole9) shouldEqual true
-    lens.exists(greaterThan10)(whole9) shouldEqual false
+    wholeLens.exists(greaterThan5)(whole9) shouldEqual true
+    wholeLens.exists(greaterThan10)(whole9) shouldEqual false
   }
 
   test("notExists") {
-    lens.notExists(greaterThan10)(whole9) shouldEqual true
-    lens.notExists(greaterThan5)(whole9) shouldEqual false
-    lens.notExists(greaterThan5)(whole9) shouldEqual (!lens.exists(greaterThan5)(whole9))
+    wholeLens.notExists(greaterThan10)(whole9) shouldEqual true
+    wholeLens.notExists(greaterThan5)(whole9) shouldEqual false
+    wholeLens.notExists(greaterThan5)(whole9) shouldEqual (!wholeLens.exists(greaterThan5)(whole9))
   }
 
   test("contains") {
-    lens.contains(whole9)(9) shouldEqual true
-    lens.contains(whole9)(5) shouldEqual false
+    wholeLens.contains(whole9)(9) shouldEqual true
+    wholeLens.contains(whole9)(5) shouldEqual false
   }
 
   test("notContains") {
-    lens.notContains(whole9)(5) shouldEqual true
-    lens.notContains(whole9)(9) shouldEqual false
-    lens.notContains(whole9)(9) shouldEqual (!lens.contains(whole9)(9))
+    wholeLens.notContains(whole9)(5) shouldEqual true
+    wholeLens.notContains(whole9)(9) shouldEqual false
+    wholeLens.notContains(whole9)(9) shouldEqual (!wholeLens.contains(whole9)(9))
   }
 
   test("use") {
-    lens.use.runA(whole9).value shouldEqual 9
+    wholeLens.use.runA(whole9).value shouldEqual 9
   }
 
   test("failover") {
-    val res = lens.failover[Option](identity)(whole9)(strongStarTupleOfDisj, catsStdInstancesForOption)
-    val negativeRes = lens.failover[Option](identity)(whole9)(strongStarTupleOfNegativeDisj, catsStdInstancesForOption)
+    val res = wholeLens.failover[Option](identity)(whole9)(strongStarTupleOfDisj, catsStdInstancesForOption)
+    val negativeRes = wholeLens.failover[Option](identity)(whole9)(strongStarTupleOfNegativeDisj, catsStdInstancesForOption)
 
     res shouldEqual Some(whole9)
     negativeRes shouldEqual None
   }
 
   test("zipWith") {
-    lens.zipWith(Whole(8), Whole(1))(_ + _) shouldEqual whole9
+    wholeLens.zipWith(Whole(8), Whole(1))(_ + _) shouldEqual whole9
   }
 
   test("cotraverse") {
-    val cotraversedWhole = lens.cotraverse[Id](whole9)(identity)
+    val cotraversedWhole = wholeLens.cotraverse[Id](whole9)(identity)
 
     cotraversedWhole shouldEqual whole9
-    lens.zipWithF[Id](identity)(whole9) shouldEqual cotraversedWhole
+    wholeLens.zipWithF[Id](identity)(whole9) shouldEqual cotraversedWhole
+  }
+
+  checkAll("compose with Iso", LensRules(lens compose iso))
+  checkAll("compose with Lens", LensRules(lens compose lens))
+  checkAll("compose with ALens", ALensRules(lens compose aLens))
+  checkAll("compose with Prism", TraversalRules(lens compose prism))
+  checkAll("compose with AffineTraversal", AffineTraversalRules(lens compose affineTraversal))
+  checkAll("compose with AnAffineTraversal", AnAffineTraversalRules(lens compose anAffineTraversal))
+  checkAll("compose with Traversal", TraversalRules(lens compose traversal))
+  checkAll("compose with ATraversal", ATraversalRules(lens compose aTraversal))
+  checkAll("compose with Setter", SetterRules(lens compose setter))
+
+  test("compose with Getter") {
+    (iso compose getter).view(9) shouldEqual 9
+  }
+
+  test("compose with Fold") {
+    (iso compose fold).fold(9) shouldEqual 9
   }
 }
