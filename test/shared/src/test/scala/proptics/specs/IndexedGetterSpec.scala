032e089b106d2df93e2fa701dec8dcb13d4d3612
sagifogel <sagifogel@hotmail.com>
2020-08-28 01:38:37 +0300

    addition of unit tests of compose for IndexedSetter

--- test/shared/src/test/scala/proptics/specs/IndexedGetterSpec.scala
+++ test/shared/src/test/scala/proptics/specs/IndexedGetterSpec.scala
@@ -1,51 +1,72 @@
 package proptics.specs
 
+import cats.instances.int._
 import cats.syntax.option._
 import cats.data.NonEmptyList
 import proptics.IndexedGetter
+import proptics.specs.Compose._
 
 class IndexedGetterSpec extends PropticsSuite {
-  val indexedGetter: IndexedGetter[Int, NonEmptyList[Int], Int] =
+  val nelIndexedGetter: IndexedGetter[Int, NonEmptyList[Int], Int] =
     IndexedGetter[Int, NonEmptyList[Int], Int](nel => (0, nel.head))
 
   test("view") {
-    indexedGetter.view(nel) shouldEqual ((0, 1))
+    nelIndexedGetter.view(nel) shouldEqual ((0, 1))
   }
 
   test("exists") {
-    indexedGetter.exists(_ === ((0, 1)))(nel) shouldEqual true
+    nelIndexedGetter.exists(_ === ((0, 1)))(nel) shouldEqual true
   }
 
   test("notExists") {
-    indexedGetter.notExists(_ === ((0, 1)))(nel) shouldEqual false
-    indexedGetter.notExists(_ === ((1, 1)))(nel) shouldEqual true
-    indexedGetter.notExists(_ === ((0, 2)))(nel) shouldEqual true
-    indexedGetter.notExists(_ === ((0, 1)))(nel) shouldEqual !indexedGetter.exists(_ == ((0, 1)))(nel)
+    nelIndexedGetter.notExists(_ === ((0, 1)))(nel) shouldEqual false
+    nelIndexedGetter.notExists(_ === ((1, 1)))(nel) shouldEqual true
+    nelIndexedGetter.notExists(_ === ((0, 2)))(nel) shouldEqual true
+    nelIndexedGetter.notExists(_ === ((0, 1)))(nel) shouldEqual !nelIndexedGetter.exists(_ == ((0, 1)))(nel)
   }
 
   test("contains") {
-    indexedGetter.contains(nel)((0, 1)) shouldEqual true
-    indexedGetter.contains(nel)((1, 1)) shouldEqual false
+    nelIndexedGetter.contains(nel)((0, 1)) shouldEqual true
+    nelIndexedGetter.contains(nel)((1, 1)) shouldEqual false
   }
 
   test("notContains") {
-    indexedGetter.notContains(nel)((0, 1)) shouldEqual false
-    indexedGetter.notContains(nel)((1, 1)) shouldEqual true
-    indexedGetter.notContains(nel)((1, 1)) shouldEqual !indexedGetter.contains(nel)((1, 1))
+    nelIndexedGetter.notContains(nel)((0, 1)) shouldEqual false
+    nelIndexedGetter.notContains(nel)((1, 1)) shouldEqual true
+    nelIndexedGetter.notContains(nel)((1, 1)) shouldEqual !nelIndexedGetter.contains(nel)((1, 1))
   }
 
   test("find") {
-    indexedGetter.find { case (i, a) => i === 0 && a === 1 }(nel) shouldEqual 1.some
-    indexedGetter.find(_ === ((0, 1)))(nel) shouldEqual 1.some
-    indexedGetter.find(_._2 === 0)(nel) shouldEqual None
-    indexedGetter.find(_ === ((1, 1)))(nel) shouldEqual None
+    nelIndexedGetter.find { case (i, a) => i === 0 && a === 1 }(nel) shouldEqual 1.some
+    nelIndexedGetter.find(_ === ((0, 1)))(nel) shouldEqual 1.some
+    nelIndexedGetter.find(_._2 === 0)(nel) shouldEqual None
+    nelIndexedGetter.find(_ === ((1, 1)))(nel) shouldEqual None
   }
 
   test("asGetter") {
-    indexedGetter.asGetter.view(nel) shouldEqual 1
+    nelIndexedGetter.asGetter.view(nel) shouldEqual 1
   }
 
   test("asIndexedFold") {
-    indexedGetter.asIndexedFold.preview(nel) shouldEqual (0, 1).some
+    nelIndexedGetter.asIndexedFold.preview(nel) shouldEqual (0, 1).some
+  }
+
+  test("compose with IndexedLens") {
+    (indexedGetter compose indexedLens).view(9) shouldEqual ((0, 9))
+  }
+  test("compose with AnIndexedLens") {
+    (indexedGetter compose anIndexedLens).view(9) shouldEqual ((0, 9))
+  }
+
+  test("compose with IndexedTraversal") {
+    (indexedGetter compose indexedTraversal).foldMap(9)(_._2) shouldEqual 9
+  }
+
+  test("compose with IndexedGetter") {
+    (indexedGetter compose indexedGetter).view(9) shouldEqual ((0, 9))
+  }
+
+  test("compose with IndexedFold") {
+    (indexedGetter compose indexedFold).foldMap(9)(_._2) shouldEqual 9
   }
 }
