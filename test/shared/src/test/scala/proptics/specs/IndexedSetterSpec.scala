eb9d01ea8f8bab6803000d99c0b2daac3615584c
sagifogel <sagifogel@hotmail.com>
2020-08-28 01:38:21 +0300

    addition of property based/unit tests of compose for IndexedSetter

--- test/shared/src/test/scala/proptics/specs/IndexedSetterSpec.scala
+++ test/shared/src/test/scala/proptics/specs/IndexedSetterSpec.scala
@@ -4,18 +4,23 @@ import cats.instances.int._
 import proptics.specs.Whole._
 import proptics.IndexedSetter
 import proptics.law.{IndexedSetterRules, SetterRules}
+import proptics.specs.Compose._
 
 class IndexedSetterSpec extends PropticsSuite {
-  val indexedSetter: IndexedSetter[Int, Whole, Int] = IndexedSetter[Int, Whole, Int](fromPair => w => w.copy(part = fromPair(0, w.part)))
+  val wholeIndexedSetter: IndexedSetter[Int, Whole, Int] = IndexedSetter[Int, Whole, Int](fromPair => w => w.copy(part = fromPair(0, w.part)))
 
-  checkAll("IndexedSetter apply", IndexedSetterRules(indexedSetter))
-  checkAll("IndexedSetter asSetter", SetterRules(indexedSetter.asSetter))
+  checkAll("IndexedSetter apply", IndexedSetterRules(wholeIndexedSetter))
+  checkAll("IndexedSetter asSetter", SetterRules(wholeIndexedSetter.asSetter))
+  checkAll("compose with IndexedLens", IndexedSetterRules(indexedSetter compose indexedLens))
+  checkAll("compose with AnIndexedLens", IndexedSetterRules(indexedSetter compose anIndexedLens))
+  checkAll("compose with IndexedTraversal", IndexedSetterRules(indexedSetter compose indexedTraversal))
+  checkAll("compose with IndexedSetter", IndexedSetterRules(indexedSetter compose indexedSetter))
 
   test("set") {
-    indexedSetter.set(9)(Whole(1)) shouldEqual whole9
+    wholeIndexedSetter.set(9)(Whole(1)) shouldEqual whole9
   }
 
   test("over") {
-    indexedSetter.over(_._2 + 1)(Whole(8)) shouldEqual whole9
+    wholeIndexedSetter.over(_._2 + 1)(Whole(8)) shouldEqual whole9
   }
 }
