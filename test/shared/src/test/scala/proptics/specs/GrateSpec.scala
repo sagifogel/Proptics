b6669a4faab57b5e0b4f7a1552283f1a5c66aecd
sagifogel <sagifogel@hotmail.com>
2020-08-27 16:46:05 +0300

    addition of property based/unit test of compose for Grate

--- test/shared/src/test/scala/proptics/specs/GrateSpec.scala
+++ test/shared/src/test/scala/proptics/specs/GrateSpec.scala
@@ -4,41 +4,50 @@ import cats.instances.int._
 import cats.{Applicative, Id}
 import cats.instances.function._
 import proptics.Grate
-import proptics.law.GrateRules
+import proptics.law.{GrateRules, SetterRules}
+import proptics.specs.Compose._
 
 class GrateSpec extends PropticsSuite {
-  val grate: Grate[Whole, Int] = Grate[Whole, Int](w2i => Whole(w2i(_.part)))
+  val wholeGrate: Grate[Whole, Int] = Grate[Whole, Int](w2i => Whole(w2i(_.part)))
   val fromDistributive: Grate[Whole => Int, Int] = Grate.fromDistributive[Whole => *, Int]
 
-  checkAll("Grate apply", GrateRules(grate))
+  checkAll("Grate apply", GrateRules(wholeGrate))
+  checkAll("compose with Iso", GrateRules(grate compose iso))
+  checkAll("compose with AnIso", GrateRules(grate compose anIso))
+  checkAll("compose with Setter", SetterRules(grate compose setter))
+  checkAll("compose with Grate", GrateRules(grate compose grate))
 
   test("review") {
-    grate.review(9) shouldEqual whole9
+    wholeGrate.review(9) shouldEqual whole9
     fromDistributive.review(9)(Whole(1)) shouldEqual 9
   }
 
   test("set") {
-    grate.set(9)(Whole(1)) shouldEqual whole9
+    wholeGrate.set(9)(Whole(1)) shouldEqual whole9
     fromDistributive.set(9)(_.part + 1)(Whole(1)) shouldEqual 9
   }
 
   test("over") {
-    grate.over(_ + 1)(Whole(8)) shouldEqual whole9
+    wholeGrate.over(_ + 1)(Whole(8)) shouldEqual whole9
     fromDistributive.over(_ + 1)(_.part)(Whole(8)) shouldEqual 9
   }
 
   test("zipWith") {
-    grate.zipWith(Whole(8), Whole(1))(_ + _) shouldEqual whole9
+    wholeGrate.zipWith(Whole(8), Whole(1))(_ + _) shouldEqual whole9
     fromDistributive.zipWith(_.part, _.part)(_ + _ + 1)(Whole(4)) shouldEqual 9
   }
   test("cotraverse") {
-    val cotraversedWhole = grate.cotraverse[Id](whole9)(identity)
+    val cotraversedWhole = wholeGrate.cotraverse[Id](whole9)(identity)
     val fromDistributiveCotraverse = fromDistributive.cotraverse[Id](_.part)(identity)
 
     cotraversedWhole shouldEqual whole9
     fromDistributiveCotraverse(whole9) shouldEqual 9
-    grate.zipWithF[Id](identity)(whole9) shouldEqual cotraversedWhole
+    wholeGrate.zipWithF[Id](identity)(whole9) shouldEqual cotraversedWhole
     fromDistributive.zipWithF[Id](identity)(_.part)(Applicative[Id])(whole9) shouldEqual
       fromDistributiveCotraverse(whole9)
+  }
+
+  test("compose with Review") {
+    (grate compose review).review(9) shouldEqual 9
   }
 }
