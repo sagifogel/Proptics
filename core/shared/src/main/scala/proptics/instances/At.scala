2a3175ec4da7d514f66e93763f18bac47356b742
sagifogel <sagifogel@hotmail.com>
2020-08-29 00:05:12 +0300

    moving apply method to companion object

--- core/shared/src/main/scala/proptics/instances/At.scala
+++ core/shared/src/main/scala/proptics/instances/At.scala
@@ -18,13 +18,6 @@ trait At[S, I, A] extends Index[S, I, A] {
 trait AtInstances {
   def at[S, I, A](i: I)(implicit ev: At[S, I, A]): Lens[S, Option[A]] = ev.at(i)
 
-  def apply[S, I, A](get: I => S => Option[A])(set: I => S => Option[A] => S): At[S, I, A] = new At[S, I, A] {
-    override def at(i: I): Lens[S, Option[A]] = Lens[S, Option[A]](get(i))(set(i))
-
-    override def ix(i: I): AffineTraversal[S, A] =
-      at(i) compose (some[A] compose AffineTraversal.id[A])
-  }
-
   implicit final def atIdentity[A]: At[Id[A], Unit, A] = new At[Id[A], Unit, A] {
     override def at(i: Unit): Lens[Id[A], Option[A]] = Lens { id: Id[A] => id.some }(a => _.getOrElse(a))
 
@@ -55,5 +48,14 @@ trait AtInstances {
       Lens { map: Map[K, V] => map.get(i) }(map => _.fold(map - i)(map.updated(i, _)))
 
     override def ix(i: K): AffineTraversal[Map[K, V], V] = indexMap[K, V].ix(i)
+  }
+}
+
+object At {
+  def apply[S, I, A](get: I => S => Option[A])(set: I => S => Option[A] => S): At[S, I, A] = new At[S, I, A] {
+    override def at(i: I): Lens[S, Option[A]] = Lens[S, Option[A]](get(i))(set(i))
+
+    override def ix(i: I): AffineTraversal[S, A] =
+      at(i) compose (some[A] compose AffineTraversal.id[A])
   }
 }
