b4ee2ef39cd682a57c361dd3b7f3097e1114553c
sagifogel <sagifogel@hotmail.com>
2020-08-28 22:52:27 +0300

    refactoring LensRules to support AtSpec

--- law/src/main/scala/proptics/law/LensRules.scala
+++ law/src/main/scala/proptics/law/LensRules.scala
@@ -5,20 +5,24 @@ import org.scalacheck.Arbitrary
 import org.scalacheck.Prop.forAll
 import org.typelevel.discipline.Laws
 import proptics.Lens
+import Function.const
 
 object LensRules extends Laws {
-  def apply[S: Arbitrary: Eq, A: Arbitrary: Eq](lens: Lens[S, A])(implicit ev: Arbitrary[A => A]): RuleSet = {
-    val laws = LensLaws(lens)
+  def apply[S: Arbitrary: Eq, A: Arbitrary: Eq](lens: Lens[S, A])(implicit ev: Arbitrary[A => A]): RuleSet =
+    apply[S, A, Unit](const(lens))
+
+  def apply[S: Arbitrary: Eq, A: Arbitrary: Eq, I: Arbitrary](f: I => Lens[S, A])(implicit ev: Arbitrary[A => A]): RuleSet = {
+    def laws(i: I): LensLaws[S, A] = LensLaws(f(i))
 
     new SimpleRuleSet(
       "Lens",
-      "setView" -> forAll(laws.setGet _),
-      "viewSet" -> forAll((s: S, a: A) => laws.getSet(s, a)),
-      "setSet" -> forAll((s: S, a: A) => laws.setSet(s, a)),
-      "overIdentity" -> forAll(laws.overIdentity _),
-      "composeOver" -> forAll((s: S, f: A => A, g: A => A) => laws.composeOver(s)(f)(g)),
-      "composeSourceLens" -> forAll(laws.composeSourceLens _),
-      "composeFocusLens" -> forAll((s: S, a: A) => laws.composeFocusLens(s, a))
+      "setView" -> forAll((i: I, s: S) => laws(i).setGet(s)),
+      "viewSet" -> forAll((i: I, s: S, a: A) => laws(i).getSet(s, a)),
+      "setSet" -> forAll((i: I, s: S, a: A) => laws(i).setSet(s, a)),
+      "overIdentity" -> forAll((i: I, s: S) => laws(i).overIdentity(s)),
+      "composeOver" -> forAll((i: I, s: S, f: A => A, g: A => A) => laws(i).composeOver(s)(f)(g)),
+      "composeSourceLens" -> forAll((i: I, s: S) => laws(i).composeSourceLens(s)),
+      "composeFocusLens" -> forAll((i: I, s: S, a: A) => laws(i).composeFocusLens(s, a))
     )
   }
 }
