e20080bead6f119f199e84e632ecd57797aa2757
sagifogel <sagifogel@hotmail.com>
2020-08-29 00:07:30 +0300

    refactoring AffineTraversalRules to support IndexSpec

--- law/src/main/scala/proptics/law/AffineTraversalRules.scala
+++ law/src/main/scala/proptics/law/AffineTraversalRules.scala
@@ -6,20 +6,24 @@ import org.scalacheck.Arbitrary
 import org.scalacheck.Prop.forAll
 import org.typelevel.discipline._
 import proptics.AffineTraversal
+import Function.const
 
 object AffineTraversalRules extends Laws {
-  def apply[S: Arbitrary: Eq, A: Arbitrary: Eq](affineTraversal: AffineTraversal[S, A])(implicit ev: Arbitrary[A => A]): RuleSet = {
-    val laws = AffineTraversalLaws(affineTraversal)
+  def apply[S: Arbitrary: Eq, A: Arbitrary: Eq](affineTraversal: AffineTraversal[S, A])(implicit ev: Arbitrary[A => A]): RuleSet =
+    AffineTraversalRules[S, Unit, A](const(affineTraversal))
+
+  def apply[S: Arbitrary: Eq, I: Arbitrary, A: Arbitrary: Eq](f: I => AffineTraversal[S, A])(implicit ev: Arbitrary[A => A]): RuleSet = {
+    def laws(i: I): AffineTraversalLaws[S, A] = AffineTraversalLaws(f(i))
 
     new SimpleRuleSet(
       "AffineTraversal",
-      "respectPurity" -> forAll(laws.respectPurity[Option] _),
-      "consistentFoci" -> forAll((s: S, f: A => A, g: A => A) => laws.consistentFoci(s, f, g)),
-      "viewOrModifySet" -> forAll(laws.getSet _),
-      "previewSet" -> forAll((s: S, a: A) => laws.previewSet(s, a)),
-      "setSet" -> forAll((s: S, a: A) => laws.setSet(s, a)),
-      "overIdentity" -> forAll(laws.overIdentity _),
-      "composeOver" -> forAll((s: S, f: A => A, g: A => A) => laws.composeOver(s)(f)(g))
+      "respectPurity" -> forAll((i: I, s: S) => laws(i).respectPurity[Option](s)),
+      "consistentFoci" -> forAll((i: I, s: S, f: A => A, g: A => A) => laws(i).consistentFoci(s, f, g)),
+      "viewOrModifySet" -> forAll((i: I, s: S) => laws(i).getSet(s)),
+      "previewSet" -> forAll((i: I, s: S, a: A) => laws(i).previewSet(s, a)),
+      "setSet" -> forAll((i: I, s: S, a: A) => laws(i).setSet(s, a)),
+      "overIdentity" -> forAll((i: I, s: S) => laws(i).overIdentity(s)),
+      "composeOver" -> forAll((i: I, s: S, f: A => A, g: A => A) => laws(i).composeOver(s)(f)(g))
     )
   }
 }
