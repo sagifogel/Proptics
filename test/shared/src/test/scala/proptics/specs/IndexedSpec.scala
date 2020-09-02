5a18167942f02b5b99c7b839bdf402876dfc295b
sagifogel <sagifogel@hotmail.com>
2020-09-02 22:44:52 +0300

    addition of property based test for Indexed

---
+++ test/shared/src/test/scala/proptics/specs/IndexedSpec.scala
@@ -0,0 +1,123 @@
+package proptics.specs
+
+import cats.Eq
+import cats.arrow.{Profunctor, Strong}
+import cats.syntax.either._
+import cats.laws.discipline.{ExhaustiveCheck, MiniInt, ProfunctorTests, StrongTests}
+import org.scalacheck.Arbitrary._
+import org.scalacheck.Cogen._
+import proptics.internal.Wander._
+import org.scalacheck.ScalacheckShapeless._
+import proptics.internal.{Indexed, Wander}
+import proptics.law.{ChoiceTests, WanderTests}
+import proptics.profunctor.Choice
+
+class IndexedSpec extends PropticsSuite {
+  implicit val wanderIndexed: Wander[Indexed[* => *, Int, *, *]] = Indexed.wanderIndexed[* => *, Int](wanderFunction)
+  implicit val choiceIndexed: Choice[Indexed[* => *, Int, *, *]] = Indexed.choiceIndexed[* => *, Int](Choice.choiceFunction)
+  implicit val strongIndexed: Strong[Indexed[* => *, Int, *, *]] = Indexed.strongIndexed[* => *, Int](Profunctor.catsStrongForFunction1)
+  implicit val profunctorIndexed: Profunctor[Indexed[* => *, Int, *, *]] = Indexed.profunctorIndexed[* => *, Int](Profunctor.catsStrongForFunction1)
+  implicit def eqForget0(implicit ev: ExhaustiveCheck[MiniInt]): Eq[Indexed[* => *, Int, Int, Int]] = Eq.instance[Indexed[* => *, Int, Int, Int]] {
+    (indexed1, indexed2) =>
+      ev.allValues.forall { miniInt =>
+        val int = miniInt.toInt
+
+        indexed1.runIndex((int, int)) === indexed2.runIndex((int, int))
+      }
+  }
+
+  implicit def eqForget1(implicit ev: ExhaustiveCheck[MiniInt]): Eq[Indexed[* => *, Int, (Int, Int), Int]] = Eq.instance[Indexed[* => *, Int, (Int, Int), Int]] {
+    (indexed1, indexed2) =>
+      ev.allValues.forall { miniInt =>
+        val int = miniInt.toInt
+
+        indexed1.runIndex((int, (int, int))) === indexed2.runIndex((int, (int, int)))
+      }
+  }
+
+  implicit def eqForget2(implicit ev: ExhaustiveCheck[MiniInt]): Eq[Indexed[* => *, Int, (Int, Int), (Int, Int)]] =
+    Eq.instance[Indexed[* => *, Int, (Int, Int), (Int, Int)]] { (indexed1, indexed2) =>
+      ev.allValues.forall { miniInt =>
+        val int = miniInt.toInt
+
+        indexed1.runIndex((int, (int, int))) === indexed2.runIndex((int, (int, int)))
+      }
+    }
+
+  implicit def eqForget3(implicit ev: ExhaustiveCheck[MiniInt]): Eq[Indexed[* => *, Int, ((Int, Int), Int), (Int, Int)]] =
+    Eq.instance[Indexed[* => *, Int, ((Int, Int), Int), (Int, Int)]] { (indexed1, indexed2) =>
+      ev.allValues.forall { miniInt =>
+        val int = miniInt.toInt
+
+        indexed1.runIndex((int, ((int, int), int))) === indexed2.runIndex((int, ((int, int), int)))
+      }
+    }
+
+  implicit def eqForget4(implicit ev: ExhaustiveCheck[MiniInt]): Eq[Indexed[* => *, Int, (Int, (Int, Int)), (Int, (Int, Int))]] =
+    Eq.instance[Indexed[* => *, Int, (Int, (Int, Int)), (Int, (Int, Int))]] { (indexed1, indexed2) =>
+      ev.allValues.forall { miniInt =>
+        val int = miniInt.toInt
+
+        indexed1.runIndex((int, (int, (int, int)))) === indexed2.runIndex((int, (int, (int, int))))
+      }
+    }
+
+  implicit def eqForget5(implicit ev: ExhaustiveCheck[MiniInt]): Eq[Indexed[* => *, Int, ((Int, Int), Int), ((Int, Int), Int)]] =
+    Eq.instance[Indexed[* => *, Int, ((Int, Int), Int), ((Int, Int), Int)]] { (indexed1, indexed2) =>
+      ev.allValues.forall { miniInt =>
+        val int = miniInt.toInt
+
+        indexed1.runIndex((int, ((int, int), int))) === indexed2.runIndex((int, ((int, int), int)))
+      }
+    }
+
+  implicit def eqForget6(implicit ev: ExhaustiveCheck[MiniInt]): Eq[Indexed[* => *, Int, Int, Either[Int, Int]]] =
+    Eq.instance[Indexed[* => *, Int, Int, Either[Int, Int]]] { (indexed1, indexed2) =>
+      ev.allValues.forall { miniInt =>
+        val int = miniInt.toInt
+
+        indexed1.runIndex((int, int)) === indexed2.runIndex((int, int))
+      }
+    }
+
+  implicit def eqForget7(implicit ev: ExhaustiveCheck[MiniInt]): Eq[Indexed[* => *, Int, Either[Int, Int], Either[Int, Int]]] =
+    Eq.instance[Indexed[* => *, Int, Either[Int, Int], Either[Int, Int]]] { (indexed1, indexed2) =>
+      ev.allValues.forall { miniInt =>
+        val int = miniInt.toInt
+
+        indexed1.runIndex((int, int.asRight[Int])) === indexed2.runIndex((int, int.asRight[Int]))
+      }
+    }
+
+  implicit def eqForget8(implicit ev: ExhaustiveCheck[MiniInt]): Eq[Indexed[* => *, Int, Either[Either[Int, Int], Int], Int]] =
+    Eq.instance[Indexed[* => *, Int, Either[Either[Int, Int], Int], Int]] { (indexed1, indexed2) =>
+      ev.allValues.forall { miniInt =>
+        val int = miniInt.toInt
+
+        indexed1.runIndex((int, int.asRight[Either[Int, Int]])) === indexed2.runIndex((int, int.asRight[Either[Int, Int]]))
+      }
+    }
+
+  implicit def eqForget9(implicit ev: ExhaustiveCheck[MiniInt]): Eq[Indexed[* => *, Int, Either[Either[Int, Int], Int], Either[Either[Int, Int], Int]]] =
+    Eq.instance[Indexed[* => *, Int, Either[Either[Int, Int], Int], Either[Either[Int, Int], Int]]] { (indexed1, indexed2) =>
+      ev.allValues.forall { miniInt =>
+        val int = miniInt.toInt
+
+        indexed1.runIndex((int, int.asRight[Either[Int, Int]])) === indexed2.runIndex((int, int.asRight[Either[Int, Int]]))
+      }
+    }
+
+  implicit def eqForget10(implicit ev: ExhaustiveCheck[MiniInt]): Eq[Indexed[* => *, Int, Either[Int, Either[Int, Int]], Either[Int, Either[Int, Int]]]] =
+    Eq.instance[Indexed[* => *, Int, Either[Int, Either[Int, Int]], Either[Int, Either[Int, Int]]]] { (indexed1, indexed2) =>
+      ev.allValues.forall { miniInt =>
+        val int = miniInt.toInt
+
+        indexed1.runIndex((int, int.asLeft[Either[Int, Int]])) === indexed2.runIndex((int, int.asLeft[Either[Int, Int]]))
+      }
+    }
+
+  checkAll("Profunctor Indexed[* => *, Int, Int, Int]", ProfunctorTests[Indexed[* => *, Int, *, *]].profunctor[Int, Int, Int, Int, Int, Int])
+  checkAll("Profunctor Indexed[* => *, Int, Int, Int]", StrongTests[Indexed[* => *, Int, *, *]].strong[Int, Int, Int, Int, Int, Int])
+  checkAll("Choice Indexed[* => *, Int, Int, Int]", ChoiceTests[Indexed[* => *, Int, *, *]].choice[Int, Int, Int, Int, Int, Int])
+  checkAll("Wander Indexed[* => *, Int, Int, Int]", WanderTests[Indexed[* => *, Int, *, *]].wander[Int, Int, Int, Int, Int, Int])
+}
