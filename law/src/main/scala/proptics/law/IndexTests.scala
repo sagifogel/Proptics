2ce8681761a9b68e5eb8db5db10569ca33f6d8df
sagifogel <sagifogel@hotmail.com>
2020-09-02 17:11:55 +0300

    refactoring to IndexTests

---
+++ law/src/main/scala/proptics/law/IndexTests.scala
@@ -0,0 +1,26 @@
+package proptics.law
+
+import cats.Eq
+import org.scalacheck.Arbitrary
+import org.typelevel.discipline.Laws
+import proptics.instances.Index
+import proptics.instances.index.{index => idx}
+
+trait IndexTests[S, I, A] extends Laws {
+  def index(
+      implicit
+      ev: Index[S, I, A],
+      eqS: Eq[S],
+      eqA: Eq[A],
+      arbS: Arbitrary[S],
+      arbI: Arbitrary[I],
+      arbA: Arbitrary[A],
+      arbAA: Arbitrary[A => A],
+      arbOp: Arbitrary[Option[A] => Option[A]]): RuleSet =
+    new SimpleRuleSet("Index", AffineTraversalTests(idx(_: I)(ev)).affineTraversal.props: _*)
+}
+
+object IndexTests {
+  def apply[S, I, A]: IndexTests[S, I, A] =
+    new IndexTests[S, I, A] {}
+}
--- test/shared/src/test/scala/proptics/specs/IndexSpec.scala
+++ test/shared/src/test/scala/proptics/specs/IndexSpec.scala
@@ -2,9 +2,9 @@ package proptics.specs
 
 import cats.instances.map._
 import cats.instances.int._
-import proptics.law.IndexRules
+import proptics.law.IndexTests
 import proptics.instances.index._
 
 class IndexSpec extends PropticsSuite {
-  checkAll("Index Map[Int, Int]", IndexRules[Map[Int, Int], Int, Int])
+  checkAll("Index Map[Int, Int]", IndexTests[Map[Int, Int], Int, Int].index)
 }
