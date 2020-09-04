fb4a7d8132fc31ab0d0ecca6aeccfb6059c06ac8
sagifogel <sagifogel@hotmail.com>
2020-09-04 17:58:55 +0300

    changed implementation of lmapClosedRmapClosedConsistent of ClosedLaws

--- law/src/main/scala/proptics/law/ClosedLaws.scala
+++ law/src/main/scala/proptics/law/ClosedLaws.scala
@@ -8,16 +8,16 @@ import Function.const
 trait ClosedLaws[F[_, _]] extends ProfunctorLaws[F] {
   implicit def F: Closed[F]
 
-  def lmapClosedRmapClosedConsistent[A, B, C](fab: F[A, B], f: (C => B) => C => B, g: (C => A) => C => A): IsEq[F[C => A, C => B]] =
-    F.closed[A, B, C](fab).rmap[C => B](f) <-> F.closed[A, B, C](fab).lmap[C => A](g)
+  def lmapClosedRmapClosedConsistent[A](fab: F[A, A]): IsEq[F[A => A, A => A]] =
+    F.closed[A, A, A](fab).rmap[A => A](f => a => f(a)) <-> F.closed[A, A, A](fab).lmap[A => A](f => a => f(a))
 
-  def closedComposeClosedDimapConsistent[A, B, C](fab: F[A, B]): IsEq[F[A => A => A, A => A => B]] =
+  def closedComposeClosedDimapConsistent[A, B](fab: F[A, B]): IsEq[F[A => A => A, A => A => B]] =
     F.closed[A => A, A => B, A](F.closed[A, B, A](fab)) <->
       F.closed[A, B, (A, A)](fab).dimap[A => A => A, A => A => B](f => p => f(p._1)(p._2)) { f => a1 => a2 =>
         f((a1, a2))
       }
 
-  def dimapConstIdentityConsistent[A, B, C](fab: F[A, B]): IsEq[F[A, B]] =
+  def dimapConstIdentityConsistent[A, B](fab: F[A, B]): IsEq[F[A, B]] =
     F.closed[A, B, Unit](fab).dimap[A, B](const)(_.apply(())) <-> fab
 }
 
