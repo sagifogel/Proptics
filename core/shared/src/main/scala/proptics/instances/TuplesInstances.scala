e183bb96eed982b02e554b26f208071e6e435440
sagifogel <sagifogel@hotmail.com>
2020-09-14 02:07:12 +0300

    remove unnecessary type parameters

--- core/shared/src/main/scala/proptics/instances/TuplesInstances.scala
+++ core/shared/src/main/scala/proptics/instances/TuplesInstances.scala
@@ -21,9 +21,9 @@ trait TuplesInstances {
 
   final def pfsta[A, B, C]: ALens_[(A, C), (B, C), A, B] = _p1A
 
-  final def _1A[A, B, C]: ALens[(A, B), A] = _p1A[A, A, B]
+  final def _1A[A, B]: ALens[(A, B), A] = _p1A[A, A, B]
 
-  final def fstA[A, B, C]: ALens[(A, B), A] = _1A
+  final def fstA[A, B]: ALens[(A, B), A] = _1A
 
   final def _p2[A, B, C]: Lens_[(C, A), (C, B), A, B] =
     Lens_(new Rank2TypeLensLike[(C, A), (C, B), A, B] {
@@ -34,7 +34,7 @@ trait TuplesInstances {
 
   final def _2[A, B]: Lens[(B, A), A] = _p2[A, A, B]
 
-  final def snd[A, B, C]: Lens[(B, A), A] = _2
+  final def snd[A, B]: Lens[(B, A), A] = _2
 
   final def _p2A[A, B, C]: ALens_[(C, A), (C, B), A, B] =
     ALens_.lens[(C, A), (C, B), A, B](_._2) { case (c, _) => b => (c, b) }
@@ -43,5 +43,5 @@ trait TuplesInstances {
 
   final def _2A[A, B]: ALens[(B, A), A] = _p2A[A, A, B]
 
-  final def sndA[A, B, C]: ALens[(B, A), A] = _2A
+  final def sndA[A, B]: ALens[(B, A), A] = _2A
 }
