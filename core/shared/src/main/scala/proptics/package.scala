475e8ffe00cef955e805461f16dea73c2dd764cd
Sagi Fogel <sagi.fogel@gmail.com>
2020-10-30 21:17:03 +0200

    reorder types

--- core/shared/src/main/scala/proptics/package.scala
+++ core/shared/src/main/scala/proptics/package.scala
@@ -1,31 +1,19 @@
 package object proptics {
 
-  /** [[Lens]] is a specialization of [[Lens_]] */
-  type Lens[S, A] = Lens_[S, S, A, A]
-
-  /** [[Prism]] is a specialization of [[Prism_]] */
-  type Prism[S, A] = Prism_[S, S, A, A]
-
   /** [[Iso]] is a specialization of [[Iso_]] */
   type Iso[S, A] = Iso_[S, S, A, A]
-
-  /** [[Traversal]] is a specialization of [[Traversal_]] */
-  type Traversal[S, A] = Traversal_[S, S, A, A]
-
-  /** [[ATraversal]] is a specialization of [[ATraversal_]] */
-  type ATraversal[S, A] = ATraversal_[S, S, A, A]
 
   /** [[AnIso]] is a specialization of [[AnIso_]] */
   type AnIso[S, A] = AnIso_[S, S, A, A]
 
+  /** [[Lens]] is a specialization of [[Lens_]] */
+  type Lens[S, A] = Lens_[S, S, A, A]
+
   /** [[ALens]] is a specialization of [[ALens_]] */
   type ALens[S, A] = ALens_[S, S, A, A]
 
-  /** [[IndexedLens]] is a specialization of [[IndexedLens_]] */
-  type IndexedLens[I, S, A] = IndexedLens_[I, S, S, A, A]
-
-  /** [[AnIndexedLens]] is a specialization of [[AnIndexedLens_]] */
-  type AnIndexedLens[I, S, A] = AnIndexedLens_[I, S, S, A, A]
+  /** [[Prism]] is a specialization of [[Prism_]] */
+  type Prism[S, A] = Prism_[S, S, A, A]
 
   /** [[APrism]] is a specialization of [[APrism_]] */
   type APrism[S, A] = APrism_[S, S, A, A]
@@ -36,8 +24,14 @@ package object proptics {
   /** [[AffineTraversal]] is a specialization of [[AffineTraversal_]] */
   type AnAffineTraversal[S, A] = AnAffineTraversal_[S, S, A, A]
 
-  /** [[Grate]] is a specialization of [[Grate_]] */
-  type Grate[S, A] = Grate_[S, S, A, A]
+  /** [[Traversal]] is a specialization of [[Traversal_]] */
+  type Traversal[S, A] = Traversal_[S, S, A, A]
+
+  /** [[ATraversal]] is a specialization of [[ATraversal_]] */
+  type ATraversal[S, A] = ATraversal_[S, S, A, A]
+
+  /** [[Fold]] is a specialization of [[Fold_]] */
+  type Fold[S, A] = Fold_[S, S, A, A]
 
   /** [[Getter]] is a specialization of [[Getter_]] */
   type Getter[S, A] = Getter_[S, S, A, A]
@@ -48,8 +42,14 @@ package object proptics {
   /** [[Review]] is a specialization of [[Review_]] */
   type Review[S, A] = Review_[S, S, A, A]
 
-  /** [[Fold]] is a specialization of [[Fold_]] */
-  type Fold[S, A] = Fold_[S, S, A, A]
+  /** [[Grate]] is a specialization of [[Grate_]] */
+  type Grate[S, A] = Grate_[S, S, A, A]
+
+  /** [[IndexedLens]] is a specialization of [[IndexedLens_]] */
+  type IndexedLens[I, S, A] = IndexedLens_[I, S, S, A, A]
+
+  /** [[AnIndexedLens]] is a specialization of [[AnIndexedLens_]] */
+  type AnIndexedLens[I, S, A] = AnIndexedLens_[I, S, S, A, A]
 
   /** [[IndexedTraversal]] is a specialization of [[IndexedTraversal_]] */
   type IndexedTraversal[I, S, A] = IndexedTraversal_[I, S, S, A, A]
