53769314701c5765e841db66c2d9c8df0c3d9f90
Sagi Fogel <sagi.fogel@gmail.com>
2020-11-20 01:13:24 +0200

    addition of unit tests for SetterSyntax

--- core/shared/src/main/scala/proptics/syntax/SetterSyntax.scala
+++ core/shared/src/main/scala/proptics/syntax/SetterSyntax.scala
@@ -13,21 +13,28 @@ trait SetterSyntax {
 }
 
 final case class SetterSTAAOps[S, T, A](private val setter: Setter_[S, T, A, A]) extends AnyVal {
-  def addOver(a: A)(implicit ev0: Semiring[A]): S => T = setter.over(ev0.additive.combine(_, a))
+  /** modify the focus type of a [[Setter_]] using a Semiring additive */
+  def addOver(s: S)(a: A)(implicit ev0: Semiring[A]): T = setter.over(ev0.additive.combine(_, a))(s)
 
-  def mulOver(a: A)(implicit ev0: Semiring[A]): S => T = setter.over(ev0.multiplicative.combine(_, a))
+  /** modify the focus type of a [[Setter_]] using a Semiring multiplicative */
+  def mulOver(s: S)(a: A)(implicit ev0: Semiring[A]): T = setter.over(ev0.multiplicative.combine(_, a))(s)
 
-  def subOver(a: A)(implicit ev0: Ring[A]): S => T = setter.over(ev0.minus(_, a))
+  /** modify the focus type of a [[Setter_]] using a Ring minus */
+  def subOver(s: S)(a: A)(implicit ev0: Ring[A]): T = setter.over(ev0.minus(_, a))(s: S)
 
-  def divOver(a: A)(implicit ev0: Field[A]): S => T = setter.over(ev0.div(_, a))
+  /** modify the focus type of a [[Setter_]] using a Field div */
+  def divOver(s: S)(a: A)(implicit ev0: Field[A]): T = setter.over(ev0.div(_, a))(s)
 
-  def disjOver(a: A)(implicit ev0: Heyting[A]): S => T = setter.over(ev0.or(_, a))
+  /** modify the focus type of a [[Setter_]] using a Heyting or */
+  def disjOver(s: S)(a: A)(implicit ev0: Heyting[A]): T = setter.over(ev0.or(_, a))(s)
 
-  def conjOver(a: A)(implicit ev0: Heyting[A]): S => T = setter.over(ev0.and(_, a))
+  /** modify the focus type of a [[Setter_]] using a Heyting and */
+  def conjOver(s: S)(a: A)(implicit ev0: Heyting[A]): T = setter.over(ev0.and(_, a))(s)
 
-  def appendOver(a: A)(implicit ev0: Semigroup[A]): S => T = setter.over(ev0.combine(_, a))
+  /** modify the focus type of a [[Setter_]] using a Semigroup combine */
+  def appendOver(s: S)(a: A)(implicit ev0: Semigroup[A]): T = setter.over(ev0.combine(_, a))(s)
 }
 
 final case class SetterSTAOptionB[S, T, A, B](private val setter: Setter_[S, T, A, Option[B]]) extends AnyVal {
-  def setJust(b: B): S => T = setter.set(Some(b))
+  def setJust(s: S)(b: B): T = setter.set(Some(b))(s)
 }
