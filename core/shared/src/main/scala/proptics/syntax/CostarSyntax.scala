37eceb9dc89ffd7e7f0979252b762a2c89e9c476
Sagi Fogel <sagi.fogel@gmail.com>
2020-11-20 01:08:47 +0200

    addition of documentation for hoist

--- core/shared/src/main/scala/proptics/syntax/CostarSyntax.scala
+++ core/shared/src/main/scala/proptics/syntax/CostarSyntax.scala
@@ -9,7 +9,9 @@ trait CostarSyntax {
 }
 
 final case class CostarOps[F[_], A, B](private val costar: Costar[F, A, B]) extends AnyVal {
+  /** synonym to Cokleisli run */
   def runCostar(fa: F[A]): B = costar.run(fa)
 
-  def hoist[G[_]](f: G ~> F): Costar[G, A, B] = Costar.hoistCostar(f)(costar)
+  /** apply a natural transformation from a Functor of G to a Functor of F */
+  def hoist[G[_]](f: G ~> F): Costar[G, A, B] = Costar.hoist(f)(costar)
 }
