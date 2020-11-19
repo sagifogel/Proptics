af6908989639240a66bfaa7815ff32d844f022de
Sagi Fogel <sagi.fogel@gmail.com>
2020-11-20 01:11:07 +0200

    addition of documentation for FunctionSyntax

--- core/shared/src/main/scala/proptics/syntax/FunctionSyntax.scala
+++ core/shared/src/main/scala/proptics/syntax/FunctionSyntax.scala
@@ -3,23 +3,22 @@ package proptics.syntax
 trait FunctionSyntax {
   implicit final def function2FlipOps[A, B, C](f: A => B => C): Function2FlipOps[A, B, C] = Function2FlipOps(f)
 
-  implicit final def function2Domain[A, B, C](f: B => B => C): Function2Domain[A, B, C] = Function2Domain(f)
-
   implicit final def functionFlippedApply[A](a: A): FunctionFlippedApply[A] = FunctionFlippedApply(a)
 }
 
 final case class Function2FlipOps[A, B, C](private val f: A => B => C) extends AnyVal {
+  /** flip the order of the arguments to a function of two arguments */
   def flip: B => A => C = b => a => f(a)(b)
 
+  /** transforms a unary function returning another unary function into a function of arity 2 */
   def uncurried: (A, B) => C = Function.uncurried(f)
 }
 
-final case class Function2Domain[A, B, C](private val f: B => B => C) extends AnyVal {
-  def on(g: A => B): A => A => C = x => y => f(g(x))(g(y))
-}
-
 final case class FunctionFlippedApply[A](private val a: A) extends AnyVal {
+
+  /** applies an argument to a function */
   def applyFlipped[B](f: A => B): B = f(a)
 
+  /** synonym to [[applyFlipped]] */
   def `#`[B](f: A => B): B = applyFlipped(f)
 }
