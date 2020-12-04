f3f73ebe3af67e19a33c4b09d43974bc2eb5e8b4
Sagi Fogel <sagi.fogel@gmail.com>
2020-12-04 23:34:05 +0200

    rename method names for TupleOptics

--- core/shared/src/main/scala/proptics/std/TuplesOptics.scala
+++ core/shared/src/main/scala/proptics/std/TuplesOptics.scala
@@ -8,45 +8,45 @@ import proptics.{ALens, ALens_, Lens, Lens_}
 trait TuplesOptics extends LensTupleOptics with ALensTupleOptics
 
 private[std] trait LensTupleOptics {
-  final def _p1[A, B, C]: Lens_[(A, C), (B, C), A, B] =
+  final def _1P[A, B, C]: Lens_[(A, C), (B, C), A, B] =
     Lens_(new Rank2TypeLensLike[(A, C), (B, C), A, B] {
       override def apply[P[_, _]](pab: P[A, B])(implicit ev: Strong[P]): P[(A, C), (B, C)] = ev.first(pab)
     })
 
-  final def pfst[A, B, C]: Lens_[(A, C), (B, C), A, B] = _p1
+  final def fstP[A, B, C]: Lens_[(A, C), (B, C), A, B] = _1P
 
-  final def _1[A, B]: Lens[(A, B), A] = _p1[A, A, B]
+  final def _1[A, B]: Lens[(A, B), A] = _1P[A, A, B]
 
   final def fst[A, B]: Lens[(A, B), A] = _1
 
-  final def _p2[A, B, C]: Lens_[(C, A), (C, B), A, B] =
+  final def _2P[A, B, C]: Lens_[(C, A), (C, B), A, B] =
     Lens_(new Rank2TypeLensLike[(C, A), (C, B), A, B] {
       override def apply[P[_, _]](pab: P[A, B])(implicit ev: Strong[P]): P[(C, A), (C, B)] = ev.second(pab)
     })
 
-  final def psnd[A, B, C]: Lens_[(C, A), (C, B), A, B] = _p2
+  final def sndP[A, B, C]: Lens_[(C, A), (C, B), A, B] = _2P
 
-  final def _2[A, B]: Lens[(A, B), B] = _p2[B, B, A]
+  final def _2[A, B]: Lens[(A, B), B] = _2P[B, B, A]
 
   final def snd[A, B]: Lens[(A, B), B] = _2
 }
 
 private[std] trait ALensTupleOptics {
-  final def _p1A[A, B, C]: ALens_[(A, C), (B, C), A, B] =
+  final def _1PA[A, B, C]: ALens_[(A, C), (B, C), A, B] =
     ALens_.lens[(A, C), (B, C), A, B](_._1) { case (_, c) => b => (b, c) }
 
-  final def pfstA[A, B, C]: ALens_[(A, C), (B, C), A, B] = _p1A
+  final def fstPA[A, B, C]: ALens_[(A, C), (B, C), A, B] = _1PA
 
-  final def _1A[A, B]: ALens[(A, B), A] = _p1A[A, A, B]
+  final def _1A[A, B]: ALens[(A, B), A] = _1PA[A, A, B]
 
   final def fstA[A, B]: ALens[(A, B), A] = _1A
 
-  final def _p2A[A, B, C]: ALens_[(C, A), (C, B), A, B] =
+  final def _2PA[A, B, C]: ALens_[(C, A), (C, B), A, B] =
     ALens_.lens[(C, A), (C, B), A, B](_._2) { case (c, _) => b => (c, b) }
 
-  final def psndA[A, B, C]: ALens_[(C, A), (C, B), A, B] = _p2A
+  final def sndPA[A, B, C]: ALens_[(C, A), (C, B), A, B] = _2PA
 
-  final def _2A[A, B]: ALens[(A, B), B] = _p2A[B, B, A]
+  final def _2A[A, B]: ALens[(A, B), B] = _2PA[B, B, A]
 
   final def sndA[A, B]: ALens[(B, A), A] = _2A
 }
