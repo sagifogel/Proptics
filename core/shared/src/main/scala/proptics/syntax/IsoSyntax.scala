f77c17a39522c0fcb13cadd83e609757226b55d1
Sagi Fogel <sagi.fogel@gmail.com>
2020-12-10 22:25:16 +0200

    addition of syntax for map, bimap, contramap

--- core/shared/src/main/scala/proptics/syntax/IsoSyntax.scala
+++ core/shared/src/main/scala/proptics/syntax/IsoSyntax.scala
@@ -1,11 +1,21 @@
 package proptics.syntax
 
-import cats.Applicative
+import cats.{Applicative, Bifunctor, Contravariant, Functor}
 
-import proptics.Iso_
+import proptics.{Iso, Iso_}
 
 trait IsoSyntax {
   implicit def isoSequenceOps[F[_], S, T, A](iso: Iso_[S, T, F[A], A]): IsoSequenceOps[F, S, T, A] = IsoSequenceOps(iso)
+
+  implicit def isoFunctorOps[S, A](iso: Iso[S, A]): IsoFunctorOps[S, A] = IsoFunctorOps(iso)
+}
+
+final case class IsoFunctorOps[S, A](iso: Iso[S, A]) extends AnyVal {
+  def map[F[_]: Functor]: Iso[F[S], F[A]] = Iso.map[F](iso)
+
+  def bimap[F[_, _]: Bifunctor]: Iso[F[S, S], F[A, A]] = Iso.bimap[F](iso)
+
+  def contramap[F[_]: Contravariant]: Iso[F[A], F[S]] = Iso.contramap[F](iso)
 }
 
 final case class IsoSequenceOps[F[_], S, T, A](private val iso: Iso_[S, T, F[A], A]) extends AnyVal {
