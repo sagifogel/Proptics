65e2e05f36a29ae3a4894f6f0fe632f2bee19335
Sagi Fogel <sagi.fogel@gmail.com>
2020-12-10 22:22:23 +0200

    addition of swapEither

--- core/shared/src/main/scala/proptics/std/EitherOptics.scala
+++ core/shared/src/main/scala/proptics/std/EitherOptics.scala
@@ -2,7 +2,7 @@ package proptics.std
 
 import cats.syntax.either._
 
-import proptics.Prism_
+import proptics.{Iso, Prism_}
 
 trait EitherOptics {
   final def left[A, B, C]: Prism_[Either[A, C], Either[B, C], A, B] =
@@ -10,4 +10,7 @@ trait EitherOptics {
 
   final def right[A, B, C]: Prism_[Either[C, A], Either[C, B], A, B] =
     Prism_ { either: Either[C, A] => either.fold(_.asLeft[B].asLeft[A], _.asRight[Either[C, B]]) }(_.asRight[C])
+
+  /** swap the elements of an Either */
+  final def swapEither[A, B]: Iso[Either[A, B], Either[B, A]] = Iso.iso[Either[A, B], Either[B, A]](_.swap)(_.swap)
 }
