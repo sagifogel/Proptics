9eeb212a4daa760649a954ab43fdc7af4f8fa5ea
Sagi Fogel <sagi.fogel@gmail.com>
2020-12-10 22:22:59 +0200

    addition of reverse method

--- core/shared/src/main/scala/proptics/std/ListOptics.scala
+++ core/shared/src/main/scala/proptics/std/ListOptics.scala
@@ -5,9 +5,15 @@ import scala.reflect.ClassTag
 import proptics.Iso
 
 trait ListOptics {
+  /** a monomorphic [[Iso]] from a list to an array */
   final def listToArray[A: ClassTag]: Iso[List[A], Array[A]] = Iso.iso[List[A], Array[A]](_.toArray)(_.toList)
 
+  /** a monomorphic [[Iso]] from a list to a vector */
   final def listToVector[A]: Iso[List[A], Vector[A]] = Iso.iso[List[A], Vector[A]](_.toVector)(_.toList)
 
+  /** a monomorphic [[Iso]] from a list of chars to a string */
   final val charsToString: Iso[List[Char], String] = Iso.iso[List[Char], String](_.mkString)(_.toList)
+
+  /** a monomorphic [[Iso]] for reversing a list */
+  final def reverse[A]: Iso[List[A], List[A]] = Iso.involuted[List[A]](_.reverse)
 }
