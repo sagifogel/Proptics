061d3edc3103eee95bf643c830de681e96e3f536
Sagi Fogel <sagi.fogel@gmail.com>
2020-12-21 16:20:58 +0200

    addition of isEmpty, head, prefix, suffix List optics

--- core/shared/src/main/scala/proptics/std/ListOptics.scala
+++ core/shared/src/main/scala/proptics/std/ListOptics.scala
@@ -1,8 +1,18 @@
 package proptics.std
 
+import scala.Function.const
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
 import scala.reflect.ClassTag
 
-import proptics.Iso
+import cats.instances.list._
+import cats.instances.option._
+import cats.syntax.eq._
+import cats.syntax.option._
+import cats.{Alternative, Eq}
+import spire.tailrec
+
+import proptics.{AffineTraversal, Cons, Iso, Prism}
 
 trait ListOptics {
   /** a monomorphic [[Iso]] from a list to an array */
@@ -16,4 +26,57 @@ trait ListOptics {
 
   /** a monomorphic [[Iso]] for reversing a list */
   final def reverse[A]: Iso[List[A], List[A]] = Iso.involuted[List[A]](_.reverse)
+
+  /** a monomorphic [[Prism]] to test whether a list is empty */
+  def isEmpty[A]: Prism[List[A], Unit] = Prism.nearly(List.empty[A])(_.isEmpty)
+
+  /** a monomorphic [[Prism]] to select optionally the first element of a list */
+  def head[A](implicit ev: Cons[List[A], A]): AffineTraversal[List[A], A] = ev.headOption
+
+  /** a monomorphic [[Prism]] stripping a prefix from a list when used as a [[proptics.Traversal]], or prepending that prefix when run backwards */
+  def prefix[A: Eq](list: List[A]): Prism[List[A], List[A]] =
+    Prism.fromPreview[List[A], List[A]](stripPrefix(list))(list ++ _)
+
+  /** a monomorphic [[Prism]] stripping a suffix from a list when used as a [[proptics.Traversal]], or appending that suffix when run backwards */
+  def suffix[A: Eq](list: List[A]): Prism[List[A], List[A]] =
+    Prism.fromPreview[List[A], List[A]](stripSuffix(list))(_ ++ list)
+
+  @tailrec
+  private def stripPrefix[A: Eq](ls1: List[A])(ls2: List[A]): Option[List[A]] = (ls1, ls2) match {
+    case (Nil, ys) => ys.some
+    case (x :: xs, y :: ys) if x === y => stripPrefix(xs)(ys)
+    case _ => None
+  }
+
+  private def stripSuffix[A: Eq](qs: List[A])(xs0: List[A]): Option[List[A]] = {
+    @tailrec
+    def drop(ls1: List[A], ls2: List[A]): List[A] = (ls1, ls2) match {
+      case (_ :: ps, _ :: xs) => drop(ps, xs)
+      case (Nil, xs) => xs
+      case (_, Nil) => Nil
+    }
+
+    def zipWith[A, B, C](f: A => B => C)(listA: List[A], listB: List[B]): List[C] = {
+      @tailrec
+      def go(ls1: List[A], ls2: List[B], result: ListBuffer[C]): ListBuffer[C] = (ls1, ls2) match {
+        case (Nil, _) => result
+        case (_, Nil) => result
+        case (x :: xs, y :: ys) => go(xs, ys, result += f(x)(y))
+      }
+
+      go(listA, listB, new mutable.ListBuffer[C]()).toList
+    }
+
+    @tailrec
+    def go(ls1: List[A], ls2: List[A], zs: List[A]): Option[List[A]] = (ls1, ls2) match {
+      case (_ :: xs, _ :: ys) => go(xs, ys, zs)
+      case (xs, Nil) =>
+        Alternative[Option].guard(xs === qs) map const(zipWith(const[A, A])(xs0, zs))
+      case (Nil, _) => None
+    }
+
+    val dropped = drop(qs, xs0)
+
+    go(xs0, dropped, dropped)
+  }
 }
