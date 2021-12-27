2c1b88470f33e6b578f53091ea6a21499b901a9b
Sagi Fogel <sagi.f@syte.ai>
2021-12-27 16:59:31 +0200

    addition of composition methods for AppliedAffineTraversal

--- core/shared/src/main/scala/proptics/applied/AppliedAffineTraversal.scala
+++ core/shared/src/main/scala/proptics/applied/AppliedAffineTraversal.scala
@@ -1,11 +1,53 @@
 package proptics.applied
 
 import proptics.applied.internal.AppliedAffineTraversal0
-import proptics.{AffineTraversal, AffineTraversal_, AppliedAffineTraversal}
+import proptics._
 
 trait AppliedAffineTraversal_[S, T, A, B] extends AppliedAffineTraversal0[S, T, A, B] {
   val value: S
   val optic: AffineTraversal_[S, T, A, B]
+
+  /** compose this [[AffineTraversal_]] with a function lifted to a [[Getter_]], having this [[AffineTraversal_]] applied first */
+  final def focus[C, D](f: A => C): AppliedFold_[S, T, C, D] = AppliedFold_(value, optic.andThen(Getter_[A, B, C, D](f)))
+
+  /** compose this [[AffineTraversal_]] with an [[Iso_]], having this [[AffineTraversal_]] applied first */
+  final def andThen[C, D](other: Iso_[A, B, C, D]): AppliedAffineTraversal_[S, T, C, D] = AppliedAffineTraversal_(value, optic.andThen(other))
+
+  /** compose this [[AffineTraversal_]] with an [[AnIso_]], having this [[AffineTraversal_]] applied first */
+  final def andThen[C, D](other: AnIso_[A, B, C, D]): AppliedAffineTraversal_[S, T, C, D] = AppliedAffineTraversal_(value, optic.andThen(other))
+
+  /** compose this [[AffineTraversal_]] with a [[Lens_]], having this [[AffineTraversal_]] applied first */
+  final def andThen[C, D](other: Lens_[A, B, C, D]): AppliedAffineTraversal_[S, T, C, D] = AppliedAffineTraversal_(value, optic.andThen(other))
+
+  /** compose this [[AffineTraversal_]] with an [[ALens_]], having this [[AffineTraversal_]] applied first */
+  final def andThen[C, D](other: ALens_[A, B, C, D]): AppliedAffineTraversal_[S, T, C, D] = AppliedAffineTraversal_(value, optic.andThen(other))
+
+  /** compose this [[AffineTraversal_]] with a [[Prism_]], having this [[AffineTraversal_]] applied first */
+  final def andThen[C, D](other: Prism_[A, B, C, D]): AppliedAffineTraversal_[S, T, C, D] = AppliedAffineTraversal_(value, optic.andThen(other))
+
+  /** compose this [[AffineTraversal_]] with an [[APrism_]], having this [[AffineTraversal_]] applied first */
+  final def andThen[C, D](other: APrism_[A, B, C, D]): AppliedAffineTraversal_[S, T, C, D] = AppliedAffineTraversal_(value, optic.andThen(other))
+
+  /** compose this [[AffineTraversal_]] with an [[AffineTraversal_]], having this [[AffineTraversal_]] applied first */
+  final def andThen[C, D](other: AffineTraversal_[A, B, C, D]): AppliedAffineTraversal_[S, T, C, D] = AppliedAffineTraversal_(value, optic.andThen(other))
+
+  /** compose this [[AffineTraversal_]] with an [[AnAffineTraversal_]], having this [[AffineTraversal_]] applied first */
+  final def andThen[C, D](other: AnAffineTraversal_[A, B, C, D]): AppliedAffineTraversal_[S, T, C, D] = AppliedAffineTraversal_(value, optic.andThen(other))
+
+  /** compose this [[AffineTraversal_]] with a [[Traversal_]], having this [[AffineTraversal_]] applied first */
+  final def andThen[C, D](other: Traversal_[A, B, C, D]): AppliedTraversal_[S, T, C, D] = AppliedTraversal_(value, optic.andThen(other))
+
+  /** compose this [[AffineTraversal_]] with an [[ATraversal_]], having this [[AffineTraversal_]] applied first */
+  final def andThen[C, D](other: ATraversal_[A, B, C, D]): AppliedATraversal_[S, T, C, D] = AppliedATraversal_(value, optic.andThen(other))
+
+  /** compose this [[AffineTraversal_]] with a [[Setter_]], having this [[AffineTraversal_]] applied first */
+  final def andThen[C, D](other: Setter_[A, B, C, D]): AppliedSetter_[S, T, C, D] = AppliedSetter_(value, optic.andThen(other))
+
+  /** compose this [[AffineTraversal_]] with a [[Getter_]], having this [[AffineTraversal_]] applied first */
+  final def andThen[C, D](other: Getter_[A, B, C, D]): AppliedFold_[S, T, C, D] = AppliedFold_(value, optic.andThen(other))
+
+  /** compose this [[AffineTraversal_]] with a [[Fold_]], having this [[AffineTraversal_]] applied first */
+  final def andThen[C, D](other: Fold_[A, B, C, D]): AppliedFold_[S, T, C, D] = AppliedFold_(value, optic.andThen(other))
 }
 
 object AppliedAffineTraversal_ {
