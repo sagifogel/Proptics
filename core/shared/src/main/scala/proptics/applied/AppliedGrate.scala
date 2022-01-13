47b23093e8709c00cb6716591a68533483f71a29
Sagi Fogel <sagi.f@syte.ai>
2022-01-14 01:26:29 +0200

    addition of compose methods to AppliedGrate

--- core/shared/src/main/scala/proptics/applied/AppliedGrate.scala
+++ core/shared/src/main/scala/proptics/applied/AppliedGrate.scala
@@ -1,10 +1,25 @@
 package proptics.applied
 
-import proptics.{AppliedGrate, Grate, Grate_}
+import proptics._
 
 trait AppliedGrate_[S, T, A, B] extends Serializable {
   val value: S
   val optic: Grate_[S, T, A, B]
+
+  /** compose this [[Grate_]] with an [[Iso_]], having this [[Grate_]] applied first */
+  final def andThen[C, D](other: Iso_[A, B, C, D]): AppliedGrate_[S, T, C, D] = AppliedGrate_(value, optic.andThen(other))
+
+  /** compose this [[Grate_]] with an [[AnIso_]], having this [[Grate_]] applied first */
+  final def andThen[C, D](other: AnIso_[A, B, C, D]): AppliedGrate_[S, T, C, D] = AppliedGrate_(value, optic.andThen(other))
+
+  /** compose this [[Grate_]] with a [[Setter_]], having this [[Grate_]] applied first */
+  final def andThen[C, D](other: Setter_[A, B, C, D]): AppliedSetter_[S, T, C, D] = AppliedSetter_(value, optic.andThen(other))
+
+  /** compose this [[Grate_]] with a [[Grate_]], having this [[Grate_]] applied first */
+  final def andThen[C, D](other: Grate_[A, B, C, D]): AppliedGrate_[S, T, C, D] = AppliedGrate_(value, optic.andThen(other))
+
+  /** compose this [[Grate_]] with a [[Review_]], having this [[Grate_]] applied first */
+  final def andThen[C, D](other: Review_[A, B, C, D]): AppliedReview_[S, T, C, D] = AppliedReview_(value, optic.andThen(other))
 }
 
 object AppliedGrate_ {
