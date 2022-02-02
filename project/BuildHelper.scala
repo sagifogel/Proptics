20dbe314916fdb94b06fae036ca364627f6fe97a
Scala Steward <me@scala-steward.org>
2022-02-02 02:30:19 +0100

    Update scala3-library, ... to 3.1.1

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -30,7 +30,7 @@ object BuildHelper {
 
   val Scala213 = "2.13.8"
   val Scala212 = "2.12.14"
-  val ScalaDotty = "3.1.0"
+  val ScalaDotty = "3.1.1"
   val scalaDottyVersions = Seq(ScalaDotty)
   val latestVersion: SettingKey[String] = settingKey[String]("Latest stable released version")
   private val sonatypeRepo = s"https://${Sonatype.sonatype01}/service/local"
