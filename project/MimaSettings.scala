1e0216531887ec70e9df9122f53a0a37d87052b8
Sagi Fogel <sagi.f@syte.ai>
2022-03-23 22:56:23 +0200

    change mima artifact to 0.4.0

--- project/MimaSettings.scala
+++ project/MimaSettings.scala
@@ -3,7 +3,7 @@ import sbt.Keys.{moduleName, organization}
 import sbt._
 
 object MimaSettings {
-  lazy val previousArtifactsToCompare = "0.3.2"
+  lazy val previousArtifactsToCompare = "0.4.0"
   def mimaSettings(failOnProblem: Boolean) = Seq(
     mimaFailOnProblem := failOnProblem,
     mimaPreviousArtifacts := Set(organization.value %% moduleName.value % previousArtifactsToCompare)
