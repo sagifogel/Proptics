6087abb899b52af16d1f94d85f0dfd98e534e54d
Sagi Fogel <sagi.f@syte.ai>
2022-04-04 23:34:42 +0300

    bump version to 0.4.2

--- project/MimaSettings.scala
+++ project/MimaSettings.scala
@@ -3,7 +3,7 @@ import sbt.Keys.{moduleName, organization}
 import sbt._
 
 object MimaSettings {
-  lazy val previousArtifactsToCompare = "0.4.0"
+  lazy val previousArtifactsToCompare = "0.4.1"
   def mimaSettings(failOnProblem: Boolean) = Seq(
     mimaFailOnProblem := failOnProblem,
     mimaPreviousArtifacts := Set(organization.value %% moduleName.value % previousArtifactsToCompare)
