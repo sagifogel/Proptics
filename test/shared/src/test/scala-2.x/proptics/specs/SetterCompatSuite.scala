855869e36faffe650a763864ce78485f5d0269a9
Sagi Fogel <sagi.f@syte.ai>
2022-07-07 12:12:21 +0300

    add all spire tests to SetterSpec

--- test/shared/src/test/scala-2.x/proptics/specs/SetterCompatSuite.scala
+++ test/shared/src/test/scala-2.x/proptics/specs/SetterCompatSuite.scala
@@ -1,13 +1,6 @@
 package proptics.specs
 
-import scala.Function.const
-
-import cats.syntax.option._
 import spire.algebra.Field
-import spire.std.boolean._
-
-import proptics.syntax.setter._
-import proptics.{Setter, Setter_}
 
 trait SetterCompatSuite extends PropticsSuite {
   implicit val intField: Field[Int] with Field.WithDefaultGCD[Int] = new Field[Int] with Field.WithDefaultGCD[Int] {
@@ -22,47 +15,5 @@ trait SetterCompatSuite extends PropticsSuite {
     override def plus(x: Int, y: Int): Int = x + y
 
     override def div(x: Int, y: Int): Int = x / y
-  }
-
-  val wholeSetter: Setter[Whole, Int]
-  val fromFunctor: Setter[List[Int], Int]
-  val boolSetter: Setter[Boolean, Boolean]
-  val setterOption: Setter_[List[Int], List[Option[Int]], Int, Option[Int]]
-
-  test("addOver") {
-    wholeSetter.addOver(Whole(8))(1) shouldEqual whole9
-  }
-
-  test("mulOver") {
-    wholeSetter.mulOver(Whole(5))(2) shouldEqual Whole(10)
-  }
-
-  test("subOver") {
-    wholeSetter.subOver(Whole(10))(1) shouldEqual whole9
-  }
-
-  test("divOver") {
-    wholeSetter.divOver(Whole(8))(2) shouldEqual Whole(4)
-  }
-
-  test("disjOver") {
-    boolSetter.disjOver(true)(false) shouldEqual true
-    boolSetter.disjOver(false)(true) shouldEqual true
-    boolSetter.disjOver(false)(false) shouldEqual false
-  }
-
-  test("conjOver") {
-    boolSetter.conjOver(true)(false) shouldEqual false
-    boolSetter.conjOver(false)(true) shouldEqual false
-    boolSetter.conjOver(false)(false) shouldEqual false
-    boolSetter.conjOver(true)(true) shouldEqual true
-  }
-
-  test("appendOver") {
-    wholeSetter.appendOver(Whole(8))(2) shouldEqual Whole(10)
-  }
-
-  test("setJust") {
-    setterOption.setJust(list)(9) shouldEqual list.map(const(9.some))
   }
 }
--- test/shared/src/test/scala/proptics/specs/SetterSpec.scala
+++ test/shared/src/test/scala/proptics/specs/SetterSpec.scala
@@ -1,10 +1,15 @@
 package proptics.specs
 
+import scala.Function.const
+
 import cats.Show
+import cats.syntax.option._
+import spire.std.boolean._
 
 import proptics.law.discipline._
 import proptics.specs.Whole._
 import proptics.specs.compose._
+import proptics.syntax.setter._
 import proptics.{Setter, Setter_}
 
 class SetterSpec extends SetterCompatSuite {
@@ -62,5 +67,42 @@ class SetterSpec extends SetterCompatSuite {
     fromFunctor.over(_ + 1)(List.fill(1)(8)) shouldEqual List(9)
     fromContravariant.over(_.length)(Show.fromToString[Int]).show(list) shouldEqual list.length.toString
     wholeSetter.over(_ + 1)(Whole(8)) shouldEqual whole9
+  }
+
+  test("addOver") {
+    wholeSetter.addOver(Whole(8))(1) shouldEqual whole9
+  }
+
+  test("mulOver") {
+    wholeSetter.mulOver(Whole(5))(2) shouldEqual Whole(10)
+  }
+
+  test("subOver") {
+    wholeSetter.subOver(Whole(10))(1) shouldEqual whole9
+  }
+
+  test("divOver") {
+    wholeSetter.divOver(Whole(8))(2) shouldEqual Whole(4)
+  }
+
+  test("disjOver") {
+    boolSetter.disjOver(true)(false) shouldEqual true
+    boolSetter.disjOver(false)(true) shouldEqual true
+    boolSetter.disjOver(false)(false) shouldEqual false
+  }
+
+  test("conjOver") {
+    boolSetter.conjOver(true)(false) shouldEqual false
+    boolSetter.conjOver(false)(true) shouldEqual false
+    boolSetter.conjOver(false)(false) shouldEqual false
+    boolSetter.conjOver(true)(true) shouldEqual true
+  }
+
+  test("appendOver") {
+    wholeSetter.appendOver(Whole(8))(2) shouldEqual Whole(10)
+  }
+
+  test("setJust") {
+    setterOption.setJust(list)(9) shouldEqual list.map(const(9.some))
   }
 }
