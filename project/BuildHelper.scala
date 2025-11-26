42e76743f9e87264185c6c2b91987dcc6b7b9c9b
Scala Steward <scala_steward@virtuslab.com>
2025-11-26 22:29:35 +0000

    Update scala-compiler, scala-library, ... to 2.13.18

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -28,7 +28,7 @@ object Dependencies {
 object BuildHelper {
   import Dependencies._
 
-  val Scala213 = "2.13.12"
+  val Scala213 = "2.13.18"
   val Scala212 = "2.12.16"
   val ScalaDotty = "3.4.3"
   val scalaDottyVersions: Seq[String] = Seq(ScalaDotty)
